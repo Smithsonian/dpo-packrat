cb656265b11100f535af274652f4065b37665310
Karan Pratap Singh <synester.gates4@gmail.com>
2020-05-29 12:43:55 +0530

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    camelcase: 'error',
-    'prefer-const': 'error',
-    'no-var': 'error',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/semi': ['error', 'always'],
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+        camelcase: 'error',
+        'prefer-const': 'error',
+        'no-var': 'error',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/semi': ['error', 'always'],
+        '@typescript-eslint/indent': ['error', 4],
+        '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    },
 };
