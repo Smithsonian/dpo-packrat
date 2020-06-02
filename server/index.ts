eb88f6b66e5f0c130215507b5c687300dfb79f32
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-02 13:58:55 +0530

    added cors to server

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,19 @@
  * Entry file for server
  */
 
-import * as express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
-app.get('/', (_, res) => res.send('hello from the server'));
+const PORT = 4000;
 
-app.listen(4000, () => {
-    console.log('server is running on port 4000');
+app.get('/', (request: Request, response: Response) => {
+    const { hostname } = request;
+    response.send(`Hello from Packrat server: ${hostname}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 });
