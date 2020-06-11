142d01d1246045f95ac3f1c6ad06a7c804db5449
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-11 23:46:40 +0530

    added capture data types, resolvers

--- server/graphql/schema/index.ts
+++ server/graphql/schema/index.ts
@@ -1,13 +1,35 @@
 import { mergeResolvers, mergeTypeDefs, makeExecutableSchema } from 'graphql-tools';
 
+import { assetTypes, assetResolvers } from './asset';
+import { captureDataTypes, captureDataResolvers } from './capturedata';
+import { licenceTypes, licenceResolvers } from './licence';
+import { unitTypes, unitResolvers } from './unit';
+import { modelTypes, modelResolvers } from './model';
+import { sceneTypes, sceneResolvers } from './scene';
 import { userTypes, userQueries, userResolvers } from './user';
+import { vocabularyTypes, vocabularyResolvers } from './vocabulary';
+
 import { GraphQLSchema } from 'graphql';
 
 const resolvers = mergeResolvers([
+    assetResolvers,
+    captureDataResolvers,
+    licenceResolvers,
+    unitResolvers,
+    modelResolvers,
+    sceneResolvers,
     userResolvers,
+    vocabularyResolvers,
 ]);
 
 const typeDefs = mergeTypeDefs([
+    assetTypes,
+    captureDataTypes,
+    licenceTypes,
+    unitTypes,
+    modelTypes,
+    sceneTypes,
+    vocabularyTypes,
     userTypes,
     userQueries
 ]);
