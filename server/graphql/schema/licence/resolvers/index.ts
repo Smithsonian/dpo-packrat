c710e66f0b53762e9e3810caee2354d45ebabc44
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-11 23:46:13 +0530

    added Licence types, resolvers

---
+++ server/graphql/schema/licence/resolvers/index.ts
@@ -0,0 +1,9 @@
+import Licence from './types/Licence';
+import LicenceAssignment from './types/LicenceAssignment';
+
+const resolvers = {
+    Licence,
+    LicenceAssignment,
+};
+
+export default resolvers;
