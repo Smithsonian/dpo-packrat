15d8dfa0bcfb7831ab2741e14fb2ad3c56c9e6f8
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-12 13:28:28 +0530

    remove depricated utils

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -3,15 +3,3 @@
  * These are utilities for the packrat server
  */
 
-import { GraphQLSchema } from 'graphql';
-import { loadSchemaSync, GraphQLFileLoader } from 'graphql-tools';
-import { join } from 'path';
-
-export function importGraphQL(directory: string, path: string): GraphQLSchema {
-    const resolvedPath = join(directory, path);
-    const options = {
-        loaders: [new GraphQLFileLoader()]
-    };
-
-    return loadSchemaSync(resolvedPath, options);
-}
