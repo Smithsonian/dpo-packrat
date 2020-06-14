6770334d6be840967dae872aa0c55f847e01c2d5
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-14 15:01:02 +0530

    added prettier, test new rules for windows systems

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 180,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    jsxBracketSameLine: true,
+    'function-paren-newline': ['error', { minItems: 4 }],
+};
