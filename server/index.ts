983b96b14047471f9161d63673f7488a901f4570
Jon Tyson <jon.tyson@gmail.com>
2020-06-17 18:28:03 -0700

    Jest Integration: * Added jest, specifying the version that our client's React install seems to be using (24.9.0).  Note that if we specify the latest version, we get a build error when building the client.  A separate fix is to create a .env file in the client folder with "SKIP_PREFLIGHT_CHECK=true".  This will force the client build to ignore the fact that the jest version is more recent than expected.  I suspect this approach is OK, too, but it may have side effects that we don't understand. * Added "test" script, which launches jest; execute via "yarn test" * Avoid copying **/tests to the build output directory.  This causes jest to find and run tests twice, once in the original source folder, and a second time in the build folder * Removed test code from server's webserver route /dbtest * Migrated test code originally in dbtest.ts into dbcreation.test.ts, using jest framework

--- server/index.ts
+++ server/index.ts
@@ -4,16 +4,11 @@
  * Entry file for server
  */
 
-import express, { Request, Response } from 'express';
+import express /*, { Request, Response } */ from 'express';
 import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
-
-import { PrismaClient } from '@prisma/client';
-import * as TEST from './tests';
-
 import schema from './graphql/schema';
 
-const prisma = new PrismaClient();
 const app = express();
 app.use(cors());
 
@@ -24,10 +19,4 @@ server.applyMiddleware({ app });
 
 app.listen(PORT, () => {
     console.log('GraphQL Server is running');
-});
-
-app.get('/dbtest', async (request: Request, response: Response) => {
-    const { hostname } = request;
-    const TR: TEST.TestResult = await TEST.testCreate(prisma);
-    response.send(`Hello from Packrat server @ ${hostname}:</br>` + TR.Message);
 });
