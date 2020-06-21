370d4c0a68c32c52adb9be897fde2e01b06c1697
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-21 13:23:55 +0530

    added user type resolvers

--- server/graphql/schema/user/resolvers/types/User.ts
+++ server/graphql/schema/user/resolvers/types/User.ts
@@ -1,7 +1,49 @@
 /**
  * Type resolver for User
  */
+import { User, UserPersonalizationSystemObject, UserPersonalizationUrl } from '../../../../../types/graphql';
+import { Parent, Args, Context } from '../../../../../types/resolvers';
+import { resolveUserPersonalizationSystemObjectsByUserID } from './UserPersonalizationSystemObject';
+import { resolveUserPersonalizationUrlsByUserID } from './UserPersonalizationUrl';
+import { fetchUser } from '../../../../../db';
+import { PrismaClient } from '@prisma/client';
 
-const User = {};
+const User = {
+    userPersonalizationSystemObjects: async (parent: Parent, _: Args, context: Context): Promise<UserPersonalizationSystemObject[] | null> => {
+        const { id } = parent;
+        const { prisma } = context;
+
+        return resolveUserPersonalizationSystemObjectsByUserID(prisma, Number.parseInt(id));
+    },
+    userPersonalizationUrls: async (parent: Parent, _: Args, context: Context): Promise<UserPersonalizationUrl[] | null> => {
+        const { id } = parent;
+        const { prisma } = context;
+
+        return resolveUserPersonalizationUrlsByUserID(prisma, Number.parseInt(id));
+    }
+};
+
+export async function resolveUserByID(prisma: PrismaClient, userId: number): Promise<User | null> {
+    const foundUser = await fetchUser(prisma, userId);
+    let user;
+    if (foundUser) {
+        const { idUser, Name, EmailAddress, SecurityID, Active, DateActivated, DateDisabled, WorkflowNotificationTime, EmailSettings } = foundUser;
+        user = {
+            id: idUser,
+            name: Name,
+            emailAddress: EmailAddress,
+            securityId: SecurityID,
+            active: Boolean(Active),
+            dateActivated: DateActivated,
+            dateDisabled: DateDisabled,
+            workflowNotificationTime: WorkflowNotificationTime,
+            emailSettings: EmailSettings
+        };
+
+        return user;
+    }
+
+    return user;
+}
 
 export default User;
