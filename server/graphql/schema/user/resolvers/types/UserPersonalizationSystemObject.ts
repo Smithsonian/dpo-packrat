fe829be3e4a4dea2778acfdc48a23d6d96d5fcc8
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-21 13:23:55 +0530

    added user type resolvers

--- server/graphql/schema/user/resolvers/types/UserPersonalizationSystemObject.ts
+++ server/graphql/schema/user/resolvers/types/UserPersonalizationSystemObject.ts
@@ -1,7 +1,38 @@
 /**
  * Type resolver for UserPersonalizationSystemObject
  */
+import { User, UserPersonalizationSystemObject } from '../../../../../types/graphql';
+import { Parent, Args, Context } from '../../../../../types/resolvers';
+import { fetchUserPersonalizationSystemObjectsForUserID } from '../../../../../db';
+import { PrismaClient } from '@prisma/client';
+import { resolveUserByID } from './User';
 
-const UserPersonalizationSystemObject = {};
+const UserPersonalizationSystemObject = {
+    user: async (parent: Parent, _: Args, context: Context): Promise<User | null> => {
+        const { user } = parent;
+        const { prisma } = context;
+
+        return resolveUserByID(prisma, Number.parseInt(user));
+    }
+};
+
+export async function resolveUserPersonalizationSystemObjectsByUserID(prisma: PrismaClient, userId: number): Promise<UserPersonalizationSystemObject[] | null> {
+    const foundUserPersonalizationSystemObjects = await fetchUserPersonalizationSystemObjectsForUserID(prisma, userId);
+
+    let userPersonalizationSystemObjects;
+    if (foundUserPersonalizationSystemObjects) {
+        userPersonalizationSystemObjects = foundUserPersonalizationSystemObjects.map(personalizationSystemObject => {
+            const { idUser, idUserPersonalizationSystemObject, Personalization } = personalizationSystemObject;
+
+            return {
+                id: idUserPersonalizationSystemObject,
+                personalization: Personalization,
+                user: idUser
+            };
+        });
+    }
+
+    return userPersonalizationSystemObjects;
+}
 
 export default UserPersonalizationSystemObject;
--- server/graphql/schema/user/resolvers/types/UserPersonalizationUrl.ts
+++ server/graphql/schema/user/resolvers/types/UserPersonalizationUrl.ts
@@ -1,7 +1,38 @@
 /**
  * Type resolver for UserPersonalizationUrl
  */
+import { User, UserPersonalizationUrl } from '../../../../../types/graphql';
+import { Parent, Args, Context } from '../../../../../types/resolvers';
+import { fetchUserPersonalizationUrlsForUserID } from '../../../../../db';
+import { PrismaClient } from '@prisma/client';
+import { resolveUserByID } from './User';
 
-const UserPersonalizationUrl = {};
+const UserPersonalizationUrl = {
+    user: async (parent: Parent, _: Args, context: Context): Promise<User | null> => {
+        const { user } = parent;
+        const { prisma } = context;
+
+        return resolveUserByID(prisma, Number.parseInt(user));
+    }
+};
+
+export async function resolveUserPersonalizationUrlsByUserID(prisma: PrismaClient, userId: number): Promise<UserPersonalizationUrl[] | null> {
+    const foundUserPersonalizationUrls = await fetchUserPersonalizationUrlsForUserID(prisma, userId);
+
+    let userPersonalizationUrls;
+    if (foundUserPersonalizationUrls) {
+        userPersonalizationUrls = foundUserPersonalizationUrls.map(userPersonalizationUrl => {
+            const { idUser, idUserPersonalizationUrl, URL, Personalization } = userPersonalizationUrl;
+            return {
+                id: idUserPersonalizationUrl,
+                url: URL,
+                personalization: Personalization,
+                user: idUser
+            };
+        });
+    }
+
+    return userPersonalizationUrls;
+}
 
 export default UserPersonalizationUrl;
