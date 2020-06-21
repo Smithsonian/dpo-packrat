b0462dd6277a86baa5254b38c85bb11272d1111f
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-21 13:24:41 +0530

    finish up getUser query resolver

--- server/graphql/schema/user/resolvers/queries/getUser.ts
+++ server/graphql/schema/user/resolvers/queries/getUser.ts
@@ -1,17 +1,20 @@
-import { User, GetUserResult, GetUserInput } from '../../../../../types/graphql';
+import { GetUserResult, GetUserInput } from '../../../../../types/graphql';
+import { Parent, Context } from '../../../../../types/resolvers';
+
+import { resolveUserByID } from '../types/User';
 
 type Args = { input: GetUserInput };
 
-export default function getUser(_: unknown, args: Args): GetUserResult {
-    const { input: { id } } = args;
-    const user: User = {
-        id,
-        name: 'Packrat user',
-        emailAddress: 'email@packrat.com',
-        securityId: 'securityId',
-        active: false,
-        dateActivated: new Date(),
-    };
+export default async function getUser(_: Parent, args: Args, context: Context): Promise<GetUserResult> {
+    const { input } = args;
+    const { id } = input;
+    const { prisma } = context;
 
-    return { user };
+    const user = await resolveUserByID(prisma, Number.parseInt(id));
+
+    if (user) {
+        return { user };
+    }
+
+    return { user: null };
 }
