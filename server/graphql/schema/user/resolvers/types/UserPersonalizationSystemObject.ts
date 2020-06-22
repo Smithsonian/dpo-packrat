b2e047b635865d66c1f2b5b95daf3617943356d9
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-22 12:11:02 +0530

    updated user resolver relationship modeling

--- server/graphql/schema/user/resolvers/types/UserPersonalizationSystemObject.ts
+++ server/graphql/schema/user/resolvers/types/UserPersonalizationSystemObject.ts
@@ -3,22 +3,33 @@
  */
 import { User, UserPersonalizationSystemObject } from '../../../../../types/graphql';
 import { Parent, Args, Context } from '../../../../../types/resolvers';
-import { fetchUserPersonalizationSystemObjectsForUserID } from '../../../../../db';
+import { fetchUserPersonalizationSystemObjectsForUserID, fetchUserForUserPersonalizationSystemObjectID } from '../../../../../db';
+import * as DB from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
-import { resolveUserByID } from './User';
+import { parseUser } from './User';
 
 const UserPersonalizationSystemObject = {
     user: async (parent: Parent, _: Args, context: Context): Promise<User | null> => {
-        const { user } = parent;
+        const { id } = parent;
         const { prisma } = context;
 
-        return resolveUserByID(prisma, Number.parseInt(user));
+        return resolveUserForUserPersonalizationSystemObjectID(prisma, Number.parseInt(id));
     }
 };
+
+export async function resolveUserForUserPersonalizationSystemObjectID(prisma: PrismaClient, userPersonalizationSystemObjectId: number): Promise<User | null> {
+    const foundUser = await fetchUserForUserPersonalizationSystemObjectID(prisma, userPersonalizationSystemObjectId);
+
+    return parseUser(foundUser);
+}
 
 export async function resolveUserPersonalizationSystemObjectsByUserID(prisma: PrismaClient, userId: number): Promise<UserPersonalizationSystemObject[] | null> {
     const foundUserPersonalizationSystemObjects = await fetchUserPersonalizationSystemObjectsForUserID(prisma, userId);
 
+    return parseUserPersonalizationSystemObjects(foundUserPersonalizationSystemObjects);
+}
+
+export async function parseUserPersonalizationSystemObjects(foundUserPersonalizationSystemObjects: DB.UserPersonalizationSystemObject[] | null): Promise<UserPersonalizationSystemObject[] | null> {
     let userPersonalizationSystemObjects;
     if (foundUserPersonalizationSystemObjects) {
         userPersonalizationSystemObjects = foundUserPersonalizationSystemObjects.map(personalizationSystemObject => {
--- server/graphql/schema/user/resolvers/types/UserPersonalizationUrl.ts
+++ server/graphql/schema/user/resolvers/types/UserPersonalizationUrl.ts
@@ -3,31 +3,41 @@
  */
 import { User, UserPersonalizationUrl } from '../../../../../types/graphql';
 import { Parent, Args, Context } from '../../../../../types/resolvers';
-import { fetchUserPersonalizationUrlsForUserID } from '../../../../../db';
+import { fetchUserPersonalizationUrlsForUserID, fetchUserForUserPersonalizationUrlID } from '../../../../../db';
 import { PrismaClient } from '@prisma/client';
-import { resolveUserByID } from './User';
+import * as DB from '@prisma/client';
+import { parseUser } from './User';
 
 const UserPersonalizationUrl = {
     user: async (parent: Parent, _: Args, context: Context): Promise<User | null> => {
-        const { user } = parent;
+        const { id } = parent;
         const { prisma } = context;
 
-        return resolveUserByID(prisma, Number.parseInt(user));
+        return resolveUserForUserPersonalizationUrlID(prisma, Number.parseInt(id));
     }
 };
+
+export async function resolveUserForUserPersonalizationUrlID(prisma: PrismaClient, userPersonalizationUrlId: number): Promise<User | null> {
+    const foundUser = await fetchUserForUserPersonalizationUrlID(prisma, userPersonalizationUrlId);
+
+    return parseUser(foundUser);
+}
 
 export async function resolveUserPersonalizationUrlsByUserID(prisma: PrismaClient, userId: number): Promise<UserPersonalizationUrl[] | null> {
     const foundUserPersonalizationUrls = await fetchUserPersonalizationUrlsForUserID(prisma, userId);
 
+    return parseUserPersonalizationUrls(foundUserPersonalizationUrls);
+}
+
+export async function parseUserPersonalizationUrls(foundUserPersonalizationUrls: DB.UserPersonalizationUrl[] | null): Promise<UserPersonalizationUrl[] | null> {
     let userPersonalizationUrls;
     if (foundUserPersonalizationUrls) {
         userPersonalizationUrls = foundUserPersonalizationUrls.map(userPersonalizationUrl => {
-            const { idUser, idUserPersonalizationUrl, URL, Personalization } = userPersonalizationUrl;
+            const { idUserPersonalizationUrl, URL, Personalization } = userPersonalizationUrl;
             return {
                 id: idUserPersonalizationUrl,
                 url: URL,
-                personalization: Personalization,
-                user: idUser
+                personalization: Personalization
             };
         });
     }
