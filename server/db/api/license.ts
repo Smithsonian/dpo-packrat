aea678be3b64d076f2139feae868917c327c0530
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-22 13:11:12 +0530

    added license type resolvers

--- server/db/api/license.ts
+++ server/db/api/license.ts
@@ -1,5 +1,22 @@
 /* eslint-disable camelcase */
-import { PrismaClient, License, LicenseAssignment } from '@prisma/client';
+import { PrismaClient, License, LicenseAssignment, User } from '@prisma/client';
+
+export async function fetchLicense(prisma: PrismaClient, idLicense: number): Promise<License | null> {
+    return prisma.license.findOne({ where: { idLicense } });
+}
+
+export async function fetchLicenseAssignmentsForLicenseID(prisma: PrismaClient, idLicense: number): Promise<LicenseAssignment[] | null> {
+    return prisma.license.findOne({ where: { idLicense } }).LicenseAssignment();
+}
+
+export async function fetchLicenseForLicenseAssignmentID(prisma: PrismaClient, idLicenseAssignment: number): Promise<License | null> {
+    return prisma.licenseAssignment.findOne({ where: { idLicenseAssignment } }).License();
+}
+
+export async function fetchUserForLicenseAssignmentID(prisma: PrismaClient, idLicenseAssignment: number): Promise<User | null> {
+    return prisma.licenseAssignment.findOne({ where: { idLicenseAssignment } }).User();
+}
+
 
 export async function createLicense(prisma: PrismaClient, license: License): Promise<License> {
     const { Name, Description } = license;
--- server/graphql/schema/license/resolvers/types/LicenseAssignment.ts
+++ server/graphql/schema/license/resolvers/types/LicenseAssignment.ts
@@ -1,7 +1,58 @@
 /**
  * Type resolver for LicenseAssignment
  */
+import { License, LicenseAssignment, User } from '../../../../../types/graphql';
+import { Parent, Args, Context } from '../../../../../types/resolvers';
+import { PrismaClient } from '@prisma/client';
+import * as DB from '@prisma/client';
+import { fetchUserForLicenseAssignmentID, fetchLicenseForLicenseAssignmentID } from '../../../../../db';
+import { parseLicense } from './License';
+import { parseUser } from '../../../user/resolvers/types/User';
 
-const LicenseAssignment = {};
+const LicenseAssignment = {
+    license: async (parent: Parent, _: Args, context: Context): Promise<License | null> => {
+        const { id } = parent;
+        const { prisma } = context;
+
+        return resolveLicenseByLicenseAssignmentID(prisma, Number.parseInt(id));
+    },
+    userCreator: async (parent: Parent, _: Args, context: Context): Promise<User | null> => {
+        const { id } = parent;
+        const { prisma } = context;
+
+        return resolveUserCreatorByLicenseAssignmentID(prisma, Number.parseInt(id));
+    }
+};
+
+export async function resolveLicenseByLicenseAssignmentID(prisma: PrismaClient, idLicenseAssignment: number): Promise<License | null> {
+    const foundLicense = await fetchLicenseForLicenseAssignmentID(prisma, idLicenseAssignment);
+
+    return parseLicense(foundLicense);
+}
+
+export async function resolveUserCreatorByLicenseAssignmentID(prisma: PrismaClient, idLicenseAssignment: number): Promise<User | null> {
+    const foundUser = await fetchUserForLicenseAssignmentID(prisma, idLicenseAssignment);
+
+    return parseUser(foundUser);
+}
+
+export function parseLicenseAssignments(foundLicenseAssignments: DB.LicenseAssignment[] | null): LicenseAssignment[] | null {
+    let licenseAssignments;
+    if (foundLicenseAssignments) {
+        licenseAssignments = foundLicenseAssignments.map(licenseAssignment => {
+            const { idLicense, DateStart, DateEnd } = licenseAssignment;
+
+            return {
+                id: idLicense,
+                dateStart: DateStart,
+                dateEnd: DateEnd
+            };
+        });
+
+        return licenseAssignments;
+    }
+
+    return licenseAssignments;
+}
 
 export default LicenseAssignment;
