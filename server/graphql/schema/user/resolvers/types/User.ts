805420a625fad72a5eae97f332dfbbb9e11fe16b
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-22 13:26:18 +0530

    added user <-> license assignment resolver

--- server/graphql/schema/user/resolvers/types/User.ts
+++ server/graphql/schema/user/resolvers/types/User.ts
@@ -1,26 +1,33 @@
 /**
  * Type resolver for User
  */
-import { User, UserPersonalizationSystemObject, UserPersonalizationUrl } from '../../../../../types/graphql';
+import { User, UserPersonalizationSystemObject, UserPersonalizationUrl, LicenseAssignment } from '../../../../../types/graphql';
 import { Parent, Args, Context } from '../../../../../types/resolvers';
 import { resolveUserPersonalizationSystemObjectsByUserID } from './UserPersonalizationSystemObject';
 import { resolveUserPersonalizationUrlsByUserID } from './UserPersonalizationUrl';
 import { fetchUser } from '../../../../../db';
 import { PrismaClient } from '@prisma/client';
 import * as DB from '@prisma/client';
+import { resolveLicenseAssignmentsByUserID } from '../../../license/resolvers/types/LicenseAssignment';
 
 const User = {
     userPersonalizationSystemObjects: async (parent: Parent, _: Args, context: Context): Promise<UserPersonalizationSystemObject[] | null> => {
         const { id } = parent;
         const { prisma } = context;
 
-        return resolveUserPersonalizationSystemObjectsByUserID(prisma, Number.parseInt(id));
+        return resolveUserPersonalizationSystemObjectsByUserID(prisma, id);
     },
     userPersonalizationUrls: async (parent: Parent, _: Args, context: Context): Promise<UserPersonalizationUrl[] | null> => {
         const { id } = parent;
         const { prisma } = context;
 
         return resolveUserPersonalizationUrlsByUserID(prisma, Number.parseInt(id));
+    },
+    licenseAssignments: async (parent: Parent, _: Args, context: Context): Promise<LicenseAssignment[] | null> => {
+        const { id } = parent;
+        const { prisma } = context;
+
+        return resolveLicenseAssignmentsByUserID(prisma, Number.parseInt(id));
     }
 };
 
