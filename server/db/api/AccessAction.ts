a45019d4129b55b7c074e0e1990f5e23003cc300
jahjedtieson <jon.tyson@gmail.com>
2020-06-25 14:15:46 -0700

    DB API: * Add methods for fetching objects connected via Xref tables.  For each pair of tables joined by an Xref, we create a pair of APIs: given the primary key ID of one table, return an array of objects from the connected table (by using the Xref). * SystemObjectXref connects SystemObject to SystemObject in a "master-derived" relationship.  As such, we have two methods added to SystemObject.ts, one to fetch the derived objects and the other to fetch the master objects. * Because SystemObjects are always paired with another record in the DB, we've also added a pair of methods that return Derived and Master pairs of <SystemObject, OtherObjectType> * Added test logic

--- server/db/api/AccessAction.ts
+++ server/db/api/AccessAction.ts
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import { PrismaClient, AccessAction } from '@prisma/client';
+import { PrismaClient, AccessAction, AccessRole } from '@prisma/client';
 import * as LOG from '../../utils/logger';
 
 export async function createAccessAction(prisma: PrismaClient, accessAction: AccessAction): Promise<AccessAction | null> {
@@ -25,6 +25,21 @@ export async function fetchAccessAction(prisma: PrismaClient, idAccessAction: nu
         return await prisma.accessAction.findOne({ where: { idAccessAction, }, });
     } catch (error) {
         LOG.logger.error('DBAPI.fetchAccessAction', error);
+        return null;
+    }
+}
+
+export async function fetchAccessRoleFromXref(prisma: PrismaClient, idAccessAction: number): Promise<AccessRole[] | null> {
+    try {
+        return await prisma.accessRole.findMany({
+            where: {
+                AccessRoleAccessActionXref: {
+                    some: { idAccessAction },
+                },
+            },
+        });
+    } catch (error) {
+        LOG.logger.error('DBAPI.fetchAccessRoleFromXref', error);
         return null;
     }
 }
