f4d25a37a511e9ab4348359818f0b845e00d6c30
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-25 14:09:00 +0530

    added additional entity queries, updated types

--- server/graphql/schema/workflow/resolvers/index.ts
+++ server/graphql/schema/workflow/resolvers/index.ts
@@ -2,8 +2,12 @@ import Workflow from './types/Workflow';
 import WorkflowStep from './types/WorkflowStep';
 import WorkflowStepSystemObjectXref from './types/WorkflowStepSystemObjectXref';
 import WorkflowTemplate from './types/WorkflowTemplate';
+import getWorkflow from './queries/getWorkflow';
 
 const resolvers = {
+    Query: {
+        getWorkflow
+    },
     Workflow,
     WorkflowStep,
     WorkflowStepSystemObjectXref,
