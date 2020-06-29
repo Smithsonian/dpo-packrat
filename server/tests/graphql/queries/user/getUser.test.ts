2405df3140d0d369493ae6b02597ebbbbf077049
Karan Pratap Singh <synester.gates4@gmail.com>
2020-06-29 18:46:07 +0530

    update get user query test

--- server/tests/graphql/queries/user/getUser.test.ts
+++ server/tests/graphql/queries/user/getUser.test.ts
@@ -1,26 +1,44 @@
 import { GetUserInput, GetUserResult } from '../../../../types/graphql';
 import GraphQLApi from '../../../../graphql';
 import TestSuiteUtils from '../../utils';
+import { PrismaClient } from '@prisma/client';
+import * as DBAPI from '../../../../db';
 
 const getUserTest = (utils: TestSuiteUtils): void => {
     let graphQLApi: GraphQLApi;
+    let prisma: PrismaClient;
 
     beforeAll(() => {
         graphQLApi = utils.graphQLApi;
+        prisma = utils.prisma;
     });
 
     describe('Query: getUser', () => {
         test('should work with valid input', async () => {
-            const input: GetUserInput = {
+            const userArgs = {
+                Name: 'Test User',
+                EmailAddress: 'test@si.edu',
+                SecurityID: 'SECURITY_ID',
+                Active: true,
+                DateActivated: new Date(),
+                DateDisabled: null,
+                WorkflowNotificationTime: new Date(),
+                EmailSettings: 0,
                 idUser: 0
             };
 
-            const { User }: GetUserResult = await graphQLApi.getUser(input);
+            const user = await DBAPI.createUser(prisma, userArgs);
 
-            if (User) {
-                expect(User.idUser).toBe(0);
-            } else {
-                expect(User).toBe(null);
+            if (user) {
+                const input: GetUserInput = {
+                    idUser: user.idUser
+                };
+
+                const { User }: GetUserResult = await graphQLApi.getUser(input);
+
+                if (User) {
+                    expect(User.idUser).toBe(user.idUser);
+                }
             }
         });
     });
