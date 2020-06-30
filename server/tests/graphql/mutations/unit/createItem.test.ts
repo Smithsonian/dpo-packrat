b29fdf179bc3403f83e48912c3c971e84cef7165
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-01 00:23:56 +0530

    use utils.inputs in mutation tests

--- server/tests/graphql/mutations/unit/createItem.test.ts
+++ server/tests/graphql/mutations/unit/createItem.test.ts
@@ -4,37 +4,29 @@ import TestSuiteUtils from '../../utils';
 
 const createItemTest = (utils: TestSuiteUtils): void => {
     let graphQLApi: GraphQLApi;
+    let createUnitInput: () => CreateUnitInput;
+    let createSubjectInput: (idUnit: number) => CreateSubjectInput;
+    let createItemInput: (idSubject: number) => CreateItemInput;
 
     beforeAll(() => {
         graphQLApi = utils.graphQLApi;
+        createUnitInput = utils.createUnitInput;
+        createSubjectInput = utils.createSubjectInput;
+        createItemInput = utils.createItemInput;
     });
 
     describe('Mutation: createItem', () => {
         test('should work with valid input', async () => {
-            const unitArgs: CreateUnitInput = {
-                Name: 'Test Name',
-                Abbreviation: 'Test Abbreviation',
-                ARKPrefix: 'Test ARKPrefix'
-            };
-
+            const unitArgs: CreateUnitInput = createUnitInput();
             const { Unit } = await graphQLApi.createUnit(unitArgs);
 
             if (Unit) {
-                const subjectArgs: CreateSubjectInput = {
-                    idUnit: Unit.idUnit,
-                    Name: 'Test Subject'
-                };
-
+                const subjectArgs: CreateSubjectInput = createSubjectInput(Unit.idUnit);
                 const { Subject } = await graphQLApi.createSubject(subjectArgs);
                 expect(Subject).toBeTruthy();
 
                 if (Subject) {
-                    const itemArgs: CreateItemInput = {
-                        idSubject: Subject.idSubject,
-                        Name: 'Test Item',
-                        EntireSubject: true
-                    };
-
+                    const itemArgs: CreateItemInput = createItemInput(Subject.idSubject);
                     const { Item } = await graphQLApi.createItem(itemArgs);
                     expect(Item).toBeTruthy();
                 }
