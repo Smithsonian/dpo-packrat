1a720174519065581270aa6c7212226faccd4356
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-02 20:03:18 +0530

    added a sample theme

---
+++ client/src/theme/index.tsx
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+    primary: {
+        main: '#1893CF',
+    },
+    secondary: {
+        main: '#F8D00D',
+    },
+};
+
+const theme = createMuiTheme({ palette });
+
+export default theme;
