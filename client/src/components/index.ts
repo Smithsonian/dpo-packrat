86325537388db52e115176d2662facfc40643f30
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-02 20:03:36 +0530

    added sample components

---
+++ client/src/components/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Components
+ * All reusable components are exported from here
+ */
+import Header from './Header';
+
+export { Header };
---
+++ client/src/config/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Config
+ *
+ * Organize and export client config here by extending from .env
+ */
