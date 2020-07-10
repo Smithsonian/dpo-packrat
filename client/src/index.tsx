6f4821433a8d34300bbb80b2cb10f28cc6d7c38a
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-10 13:00:24 +0530

    integrated react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './global/root.css';
-import { LoginPage } from './pages';
+import { Home, Login, About } from './pages';
+import { Header } from './components';
 import { ThemeProvider } from '@material-ui/core';
 import theme from './theme';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Routes } from './constants';
 
 function App(): React.ReactElement {
     return (
         <ThemeProvider theme={theme}>
-            <LoginPage />
+            <Router>
+                <Route path={[Routes.HOME, Routes.LOGIN, Routes.ABOUT]} component={Header} />
+                <Switch>
+                    <Route exact path={Routes.HOME} component={Home} />
+                    <Route exact path={Routes.LOGIN} component={Login} />
+                    <Route exact path={Routes.ABOUT} component={About} />
+                </Switch>
+            </Router>
         </ThemeProvider>
     );
 }
