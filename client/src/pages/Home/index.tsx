81dfd899ade6c321dc894258aaeea9b20f184d0e
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-11 15:49:49 +0530

    added sample apollo client usage with react hooks

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Check, Warning } from '@material-ui/icons';
+import userUser from './hooks/userUser';
 
 const useStyles = makeStyles(() => ({
     container: {
         display: 'flex',
         flex: 1,
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -13,10 +16,32 @@ const useStyles = makeStyles(() => ({
 
 function Home(): React.ReactElement {
     const classes = useStyles();
+    const { userLoading, userError } = userUser(0);
+
+    let queryProgress = <CircularProgress color='primary' size={20} />
+
+    if (!userLoading && !userError) {
+        queryProgress = (
+            <>
+                <Typography color='textPrimary' variant='body1'>Server reachable</Typography>
+                <Check color='primary' />
+            </>
+        )
+    }
+
+    if (userError) {
+        queryProgress = (
+            <>
+                <Typography color='textPrimary' variant='body1'>Server unreachable</Typography>
+                <Warning htmlColor='red' />
+            </>
+        )
+    }
 
     return (
         <Box className={classes.container}>
             <Typography color='textPrimary' variant='h4'>Home</Typography>
+            {queryProgress}
         </Box>
     );
 }
