fd5f1227cf07f4271cb7e9e87d807da40974300c
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-20 19:10:09 +0530

    minor update

--- server/graphql/schema/user/resolvers/queries/getUser.ts
+++ server/graphql/schema/user/resolvers/queries/getUser.ts
@@ -1,10 +1,8 @@
-import { GetUserResult, GetUserInput } from '../../../../../types/graphql';
+import { GetUserResult, QueryGetUserArgs } from '../../../../../types/graphql';
 import { Parent } from '../../../../../types/resolvers';
 import * as DBAPI from '../../../../../db';
 
-type Args = { input: GetUserInput };
-
-export default async function getUser(_: Parent, args: Args): Promise<GetUserResult> {
+export default async function getUser(_: Parent, args: QueryGetUserArgs): Promise<GetUserResult> {
     const { input } = args;
     const { idUser } = input;
 
