bdeb402df45d10a9d59b4587ab966f3073f7705a
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-21 14:35:00 +0530

    added public/private routing

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,5 +3,7 @@
  * All reusable components are exported from here
  */
 import Header from './Header';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
-export { Header };
+export { Header, PrivateRoute, PublicRoute };
