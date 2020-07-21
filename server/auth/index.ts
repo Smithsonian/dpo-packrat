acdfcac6a3bf2a9747c9cb65df01d99a4bd850b0
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-21 13:56:30 +0530

    updated session cookie age

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -8,16 +8,23 @@ const authCorsConfig = {
     credentials: true
 };
 
-const sessionAge = 24 * 60 * 60;
+/**
+ * To persist sessions we'll require to store them in our DB
+ * https://www.npmjs.com/package/express-mysql-session
+ */
+
+const maxAge = Date.now() + 24 * 60 * 60 * 1000;
+const checkPeriod = 24 * 60 * 60;
+
 const Store = MemoryStore(session);
 
 const sessionConfig = {
-    cookie: { maxAge: sessionAge }, // 24hrs expiration time
+    cookie: { maxAge, expires: new Date(maxAge) }, // 24hrs expiration time
     secret: 'test', // TODO: turn this into env
     resave: true,
     saveUninitialized: true,
     store: new Store({
-        checkPeriod: sessionAge // prune expired entries every 24h
+        checkPeriod // prune expired entries every 24h
     })
 };
 
