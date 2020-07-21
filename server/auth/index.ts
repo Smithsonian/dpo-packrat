ca1fa96ad584ec403de274f560ace3951ae37204
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-21 11:37:25 +0530

    added memory store to prevent session leak

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -1,15 +1,26 @@
 import passport from './passport';
 import AuthRouter from './router';
+import session from 'express-session';
+import MemoryStore from 'memorystore';
 
 const authCorsConfig = {
     origin: 'http://localhost:3000', // TODO: turn this into env
     credentials: true
 };
 
+const sessionAge = 24 * 60 * 60;
+const Store = MemoryStore(session);
+
 const sessionConfig = {
+    cookie: { maxAge: sessionAge }, // 24hrs expiration time
     secret: 'test', // TODO: turn this into env
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: new Store({
+        checkPeriod: sessionAge // prune expired entries every 24h
+    })
 };
 
-export { passport, authCorsConfig, sessionConfig, AuthRouter };
+const authSession = session(sessionConfig);
+
+export { passport, authCorsConfig, authSession, AuthRouter };
