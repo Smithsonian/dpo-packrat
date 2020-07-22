da5f15759fe762b21a481623c0ddd663abdabd41
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-22 16:26:53 +0530

    refactor auth into interfaces

--- server/auth/impl/local.ts
+++ server/auth/impl/local.ts
@@ -1,29 +1,58 @@
 import { Strategy } from 'passport';
 import PassportLocal from 'passport-local';
 import * as DBAPI from '../../db';
+import { Auth, VerifiedUser } from '../interface';
 
-const options = {
-    usernameField: 'email'
-};
+class LocalAuth implements Auth {
+    setup(): Strategy {
+        const options = {
+            usernameField: 'email'
+        };
 
-const verifyFunction = async (email: string, password: string, done) => {
-    const user = await DBAPI.User.fetchByEmail(email);
+        const verifyFunction = async (email: string, password: string, done) => {
+            const { user, error } = await this.verifyUser(email, password);
 
-    if (!user) {
-        return done('User does not exist', null);
+            if (error) {
+                done(error, null);
+            } else {
+                done(null, user);
+            }
+        };
+
+        const strategy = new PassportLocal.Strategy(options, verifyFunction);
+
+        return strategy;
     }
 
-    if (!user.Active) {
-        return done('User is not active', null);
+    async verifyUser(email: string, password: string): Promise<VerifiedUser> {
+        const user = await DBAPI.User.fetchByEmail(email);
+
+        if (!user) {
+            return {
+                user,
+                error: 'User does not exist'
+            };
+        }
+
+        if (!user.Active) {
+            return {
+                user,
+                error: 'User is not active'
+            };
+        }
+        // TODO: password is same as email (to be removed/updated)
+        if (password !== user.EmailAddress) {
+            return {
+                user,
+                error: 'Invalid password for user'
+            };
+        }
+
+        return {
+            user,
+            error: null
+        };
     }
-    // TODO: password is same as email (to be removed/updated)
-    if (password !== user.EmailAddress) {
-        return done('Invalid password for user', null);
-    }
+}
 
-    done(null, user);
-};
-
-const LocalStrategy: Strategy = new PassportLocal.Strategy(options, verifyFunction);
-
-export default LocalStrategy;
+export default LocalAuth;
