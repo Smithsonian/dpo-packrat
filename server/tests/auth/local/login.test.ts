dd47975f4932d7c1c0d517ec35d7c6c3f7e8439f
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-22 12:30:03 +0530

    added local login  and logout tests

---
+++ server/tests/auth/local/login.test.ts
@@ -0,0 +1,78 @@
+import { app } from '../../../index';
+import request from 'supertest';
+import * as DBAPI from '../../../db';
+
+describe('Auth implementation: local login', () => {
+    test('should work with correct user credentials', async done => {
+        const userInput = {
+            Name: 'Test User',
+            EmailAddress: 'test@si.edu',
+            SecurityID: 'SECURITY_ID',
+            Active: true,
+            DateActivated: new Date(),
+            DateDisabled: null,
+            WorkflowNotificationTime: new Date(),
+            EmailSettings: 0,
+            idUser: 0
+        };
+
+        const user = new DBAPI.User(userInput);
+        expect(user).toBeTruthy();
+
+        if (user) {
+            expect(await user.create()).toBeTruthy();
+            expect(user.idUser).toBeGreaterThan(0);
+        }
+
+        const authBody = {
+            email: userInput.EmailAddress,
+            password: userInput.EmailAddress
+        };
+
+        const { body } = await request(app).post('/auth/login').send(authBody).expect(200);
+        expect(body.success).toBe(true);
+        done();
+    });
+
+    test('should fail with incorrect credentials', async done => {
+        const userInput = {
+            Name: 'Test User',
+            EmailAddress: 'test@si.edu',
+            SecurityID: 'SECURITY_ID',
+            Active: true,
+            DateActivated: new Date(),
+            DateDisabled: null,
+            WorkflowNotificationTime: new Date(),
+            EmailSettings: 0,
+            idUser: 0
+        };
+
+        const user = new DBAPI.User(userInput);
+        expect(user).toBeTruthy();
+        if (user) {
+            expect(await user.create()).toBeTruthy();
+            expect(user.idUser).toBeGreaterThan(0);
+        }
+
+        const authBody = {
+            email: userInput.EmailAddress,
+            password: 'incorrect-password'
+        };
+
+        const { body } = await request(app).post('/auth/login').send(authBody).expect(200);
+        expect(body.success).toBe(false);
+        expect(body.message).toBeTruthy();
+        done();
+    });
+
+    test('should fail with invalid credentials', async done => {
+        const authBody = {
+            email: null,
+            password: null
+        };
+
+        const response = await request(app).post('/auth/login').send(authBody).expect(500);
+        expect(response.accepted).toBe(false);
+        done();
+    });
+});
