38af09fed7351d16758e2a7ceb8679b681abb55a
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-23 18:00:34 +0530

    added formik integration

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,13 +1,15 @@
-import React, { useState, useContext } from 'react';
-import { Box, Typography, Container, TextField, Button, Icon } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Box, Typography, Container, Button, Icon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
 import { Routes } from '../../constants';
 import API from '../../api';
 import { getAuthenticatedUser } from '../../utils/auth';
 import { AppContext } from '../../context';
 import { toast } from 'react-toastify';
-import { requiredFieldsValidator } from '../../utils/validation';
+import { TextField } from 'formik-material-ui';
 
 const useStyles = makeStyles(({ palette, typography, spacing }) => ({
     container: {
@@ -15,7 +17,7 @@ const useStyles = makeStyles(({ palette, typography, spacing }) => ({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        background: palette.text.secondary,
+        background: palette.background.paper,
     },
     loginTitle: {
         fontWeight: typography.fontWeightLight
@@ -23,6 +25,10 @@ const useStyles = makeStyles(({ palette, typography, spacing }) => ({
     loginSubtitle: {
         color: palette.grey[500],
         fontWeight: typography.fontWeightLight
+    },
+    loginForm: {
+        display: 'flex',
+        flexDirection: 'column'
     },
     textFields: {
         marginTop: spacing(2)
@@ -38,13 +44,20 @@ const useStyles = makeStyles(({ palette, typography, spacing }) => ({
     }
 }));
 
+const validationSchema = () => Yup.object({
+    email: Yup.string().required('Required'),
+    password: Yup.string().required('Required')
+});
+
 function Login(): React.ReactElement {
     const { updateUser } = useContext(AppContext);
     const classes = useStyles();
     const history = useHistory();
 
-    const [email, setEmail] = useState('test@si.edu');
-    const [password, setPassword] = useState('test@si.edu');
+    const initialValues = {
+        email: 'test@si.edu',
+        password: 'test@si.edu'
+    };
 
     const InputProps = {
         classes: {
@@ -53,14 +66,8 @@ function Login(): React.ReactElement {
         disableUnderline: true
     };
 
-    const onLogin = async (): Promise<void> => {
-        const fields = { email, password };
-        const { isValid, invalidField } = requiredFieldsValidator(fields);
-
-        if (!isValid) {
-            toast.warn(`${invalidField} is required`);
-            return;
-        }
+    const onLogin = async (values, { setSubmitting }): Promise<void> => {
+        const { email, password } = values;
 
         try {
             const { success, message } = await API.login(email, password);
@@ -76,46 +83,57 @@ function Login(): React.ReactElement {
         } catch ({ message }) {
             toast.error(message);
         }
+
+        setSubmitting(false);
     };
 
     return (
         <Box className={classes.container}>
             <Container maxWidth='xs'>
-                <Typography className={classes.loginTitle} variant='h4' color='textPrimary'>Login</Typography>
+                <Typography className={classes.loginTitle} variant='h4' color='primary'>Login</Typography>
                 <Typography className={classes.loginSubtitle} variant='subtitle1'>Welcome to packrat</Typography>
-                <Box display='flex' flexDirection='column'>
-                    <TextField
-                        value={email}
-                        onChange={({ target }) => setEmail(target.value)}
-                        className={classes.textFields}
-                        required
-                        focused
-                        label='Email'
-                        type='email'
-                        placeholder='example@si.edu'
-                        InputProps={InputProps}
-                    />
-                    <TextField
-                        value={password}
-                        onChange={({ target }) => setPassword(target.value)}
-                        className={classes.textFields}
-                        required
-                        focused
-                        label='Password'
-                        type='password'
-                        placeholder='password'
-                        InputProps={InputProps}
-                    />
-                    <Button
-                        onClick={onLogin}
-                        className={classes.loginButton}
-                        variant='outlined'
-                        color='primary'
-                        endIcon={<Icon>login</Icon>}
-                    >
-                        Login
-                    </Button>
-                </Box>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onLogin}
+                >
+                    {({ handleSubmit, handleChange, values }) => (
+                        <form className={classes.loginForm} onSubmit={handleSubmit}>
+                            <Field
+                                value={values.email}
+                                onChange={handleChange}
+                                className={classes.textFields}
+                                name='email'
+                                label='Email'
+                                type='email'
+                                placeholder='example@si.edu'
+                                InputProps={InputProps}
+                                component={TextField}
+                            />
+                            <Field
+                                value={values.password}
+                                onChange={handleChange}
+                                className={classes.textFields}
+                                name='password'
+                                label='Password'
+                                type='password'
+                                disabled
+                                placeholder='password'
+                                InputProps={InputProps}
+                                component={TextField}
+                            />
+                            <Button
+                                type='submit'
+                                className={classes.loginButton}
+                                variant='outlined'
+                                color='primary'
+                                endIcon={<Icon>login</Icon>}
+                            >
+                                Login
+                            </Button>
+                        </form>
+                    )}
+                </Formik>
             </Container>
         </Box>
     );
--- client/src/theme/index.tsx
+++ client/src/theme/index.tsx
@@ -1,15 +1,12 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+// https://material-ui.com/customization/palette/
 const palette = {
     primary: {
         main: '#1893CF'
     },
     secondary: {
         main: '#F8D00D',
-    },
-    text: {
-        primary: '#1893CF',
-        secondary: '#fff'
     }
 };
 
