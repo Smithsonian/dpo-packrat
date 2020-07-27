0e57d2e2440ac4b721bc56884bb8a02842799e76
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-27 14:44:58 +0530

    updated routes constants

---
+++ client/src/constants/routes.ts
@@ -0,0 +1,19 @@
+export enum Routes {
+    HOME = '/:type',
+    LOGIN = '/login',
+    ABOUT = '/about',
+    DASHBOARD = '/dashboard'
+}
+
+export enum DASHBOARD_TYPES {
+    DASHBOARD = 'dashboard',
+    REPOSITORY = 'repository',
+    INGESTION = 'ingestion',
+    WORKFLOW = 'workflow',
+    REPORTING = 'reporting',
+    ADMIN = 'admin'
+}
+
+export function getRoute(route: Routes | DASHBOARD_TYPES): string {
+    return `/${route}`;
+}
