d78b89a6e7b9ff100014d55ff93c957a7c1cda57
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-28 15:47:18 +0530

    added ingestion steps component

---
+++ client/src/pages/Ingestion/components/SubjectItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { HOME_ROUTES, resolveSubRoute, INGESTION_ROUTE } from '../../../../constants';
+import { SidebarBottomNavigator } from '../../../../components';
+
+const useStyles = makeStyles(() => ({
+    container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column'
+    },
+    content: {
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+}));
+
+function SubjectItem(): React.ReactElement {
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.container}>
+            <Box className={classes.content}>
+                <Typography variant='subtitle1'>Subject and Item</Typography>
+            </Box>
+            <SidebarBottomNavigator
+                leftLabel='Previous'
+                leftRoute={resolveSubRoute(HOME_ROUTES.INGESTION, INGESTION_ROUTE.ROUTES.FILES)}
+                rightLabel='Next'
+                rightRoute={resolveSubRoute(HOME_ROUTES.INGESTION, INGESTION_ROUTE.ROUTES.METADATA)}
+            />
+        </Box>
+    );
+}
+
+export default SubjectItem;
