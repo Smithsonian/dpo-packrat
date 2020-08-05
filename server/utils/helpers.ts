43ecf29c7bef6c5a31c99e7b70bce34a47275769
jahjedtieson <jon.tyson@gmail.com>
2020-08-05 12:01:50 -0700

    Per review feedback, use Lodash for array equivalence testing

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -1,18 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import * as L from 'lodash';
+
 export class Helpers {
     static arraysEqual(input1: any, input2: any): boolean {
         if (!Array.isArray(input1) || ! Array.isArray(input2) || input1.length !== input2.length)
             return false;
-
-        const SortedArray1 = input1.concat().sort();
-        const SortedArray2 = input2.concat().sort();
-
-        for (let arrayElement = 0; arrayElement < SortedArray1.length; arrayElement++) {
-            if (SortedArray1[arrayElement] !== SortedArray2[arrayElement])
-                return false;
-        }
-
-        return true;
+        return L.isEqual(input1.sort(), input2.sort());
     }
 }
