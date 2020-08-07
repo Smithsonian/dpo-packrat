83f52603415565b675f3290584c275583c7af99c
jahjedtieson <jon.tyson@gmail.com>
2020-08-06 20:59:01 -0700

    Maybe this is the magic that CI needs -- correct server endpint and appid !!

--- server/tests/setEnvVars.ts
+++ server/tests/setEnvVars.ts
@@ -4,6 +4,6 @@ process.env.SESSION_SECRET = 'test-secret';
 if (!process.env.EDAN_AUTH_KEY)
     process.env.EDAN_AUTH_KEY = 'testing1234';
 if (!process.env.EDAN_SERVER)
-    process.env.EDAN_SERVER = 'http://edantest.si.edu/';
+    process.env.EDAN_SERVER = 'http://edan.si.edu/';
 if (!process.env.EDAN_APPID)
-    process.env.EDAN_APPID = 'QUOTIENTTEST';
+    process.env.EDAN_APPID = 'OCIO3D';
