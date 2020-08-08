64980ccf0934b03757228415fed7304246c8e29d
Karan Pratap Singh <synester.gates4@gmail.com>
2020-08-08 17:00:03 +0530

    minor update

--- server/tests/setEnvVars.ts
+++ server/tests/setEnvVars.ts
@@ -1,9 +1,6 @@
 process.env.NODE_ENV = 'test';
 process.env.CLIENT_ENDPOINT = 'http://localhost:3000';
 process.env.SESSION_SECRET = 'test-secret';
-if (!process.env.EDAN_AUTH_KEY)
-    process.env.EDAN_AUTH_KEY = 'testing1234';
-if (!process.env.EDAN_SERVER)
-    process.env.EDAN_SERVER = 'http://edan.si.edu/';
-if (!process.env.EDAN_APPID)
-    process.env.EDAN_APPID = 'OCIO3D';
+if (!process.env.EDAN_AUTH_KEY) process.env.EDAN_AUTH_KEY = 'testing1234';
+if (!process.env.EDAN_SERVER) process.env.EDAN_SERVER = 'http://edan.si.edu/';
+if (!process.env.EDAN_APPID) process.env.EDAN_APPID = 'OCIO3D';
