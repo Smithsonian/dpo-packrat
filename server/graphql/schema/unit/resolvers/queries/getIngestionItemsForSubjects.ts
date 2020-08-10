643859cc541c73fe6516b073319414f8925dfc3d
Karan Pratap Singh <synester.gates4@gmail.com>
2020-08-10 16:56:16 +0530

    updated methods

--- server/graphql/schema/unit/resolvers/queries/getIngestionItemsForSubjects.ts
+++ server/graphql/schema/unit/resolvers/queries/getIngestionItemsForSubjects.ts
@@ -6,7 +6,7 @@ export default async function getIngestionItemsForSubjects(_: Parent, args: Quer
     const { input } = args;
     const { idSubjects } = input;
 
-    const Item = await DBAPI.Item.fetchFromSubjects(idSubjects);
+    const Item = await DBAPI.Item.fetchDerivedFromSubjects(idSubjects);
 
     if (Item) {
         return { Item };
