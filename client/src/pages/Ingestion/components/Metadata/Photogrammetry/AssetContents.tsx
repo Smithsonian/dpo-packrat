d12b42abd0c5912725414cdf25c17c1992f8f3c9
Karan Pratap Singh <synester.gates4@gmail.com>
2020-08-18 21:20:15 +0530

    refactor metadata, fix issues added folder variants integration

---
+++ client/src/pages/Ingestion/components/Metadata/Photogrammetry/AssetContents.tsx
@@ -0,0 +1,74 @@
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { FieldType } from '../../../../../components';
+import SelectField from './SelectField';
+import { StateFolder } from '../../../../../context';
+import { VocabularyOption } from './index';
+
+const useStyles = makeStyles(({ palette }) => ({
+    header: {
+        display: 'flex',
+        flex: 1,
+        borderBottom: `1px solid ${palette.grey[400]}`,
+        paddingBottom: 10
+    },
+    headerTitle: {
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: palette.primary.contrastText
+    },
+    emptyFolders: {
+        marginTop: 10,
+        color: palette.grey[600],
+        textAlign: 'center'
+    }
+}));
+
+interface AssetContentsProps {
+    initialEntry: number | null;
+    folders: StateFolder[];
+    options: VocabularyOption[];
+    onUpdate: (id: number, variantType: number) => void;
+}
+
+function AssetContents(props: AssetContentsProps): React.ReactElement {
+    const { folders, options, initialEntry, onUpdate } = props;
+    const classes = useStyles();
+
+    return (
+        <FieldType required renderLabel={false} marginTop={1.5}>
+            <Box className={classes.header}>
+                <Box className={classes.headerTitle}>
+                    <Typography variant='body1'>Folder Name</Typography>
+                </Box>
+                <Box className={classes.headerTitle}>
+                    <Typography variant='body1'>Variant Type</Typography>
+                </Box>
+            </Box>
+            <Box display='flex' flex={1} flexDirection='column'>
+                {!folders.length && <Typography className={classes.emptyFolders} variant='caption'>No folders detected</Typography>}
+                {folders.map(({ id, name, variantType }) => {
+                    const update = ({ target }) => onUpdate(id, target.value);
+
+                    return (
+                        <SelectField
+                            key={id}
+                            required
+                            label={name}
+                            width='96%'
+                            value={variantType || initialEntry}
+                            name='folders'
+                            onChange={update}
+                            options={options}
+                        />
+                    );
+                })}
+            </Box>
+        </FieldType>
+    );
+}
+
+export default AssetContents;
--- client/src/pages/Ingestion/hooks/useMetadata.ts
+++ client/src/pages/Ingestion/hooks/useMetadata.ts
@@ -1,6 +1,21 @@
 import { useContext } from 'react';
-import { AppContext, FileId, StateMetadata, PhotogrammetryFields, METADATA_ACTIONS, IngestionDispatchAction, AssetType } from '../../../context';
+import {
+    AppContext,
+    FileId,
+    StateMetadata,
+    PhotogrammetryFields,
+    METADATA_ACTIONS,
+    IngestionDispatchAction,
+    AssetType,
+    StateFolder,
+    StateVocabulary,
+    StateIdentifier
+} from '../../../context';
 import lodash from 'lodash';
+import { GetContentsForAssetVersionsDocument } from '../../../types/graphql';
+import { apolloClient } from '../../../graphql';
+import { eVocabularySetID } from '../../../types/server';
+import useVocabularyEntries from './useVocabularyEntries';
 
 type MetadataInfo = {
     metadata: StateMetadata;
@@ -20,6 +35,7 @@ interface UseMetadata {
     getCurrentMetadata: (id: FileId) => StateMetadata | undefined;
     getMetadataInfo: (id: FileId) => MetadataInfo;
     updatePhotogrammetryFields: (metadataIndex: number, values: PhotogrammetryFields) => void;
+    updateMetadataFolders: (vocabularies: StateVocabulary) => Promise<void>;
 }
 
 function useMetadata(): UseMetadata {
@@ -27,6 +43,11 @@ function useMetadata(): UseMetadata {
         ingestion: { metadatas },
         ingestionDispatch
     } = useContext(AppContext);
+
+    const { getInitialEntryWithVocabularies } = useVocabularyEntries();
+
+    // TODO: replace index with fileId this afterwards
+    const idAssetVersions: number[] = [...metadatas].map((_, index) => index);
 
     const getFieldErrors = (metadata: StateMetadata): FieldErrors => {
         const errors: FieldErrors = {
@@ -84,11 +105,76 @@ function useMetadata(): UseMetadata {
         ingestionDispatch(updateMetadataFieldsAction);
     };
 
+    const updateMetadataFolders = async (vocabularies: StateVocabulary): Promise<void> => {
+        const getInitialVocabularyEntry = (eVocabularySetID: eVocabularySetID) => getInitialEntryWithVocabularies(vocabularies, eVocabularySetID);
+
+        const defaultIdentifier: StateIdentifier = {
+            id: 0,
+            identifier: '',
+            identifierType: getInitialVocabularyEntry(eVocabularySetID.eIdentifierIdentifierType),
+            selected: false
+        };
+
+        const defaultVocabularyFields = {
+            datasetType: getInitialVocabularyEntry(eVocabularySetID.eCaptureDataDatasetType),
+            identifiers: [defaultIdentifier]
+        };
+
+        const variables = {
+            input: {
+                idAssetVersions
+            }
+        };
+
+        const { data } = await apolloClient.query({
+            query: GetContentsForAssetVersionsDocument,
+            variables
+        });
+
+        const { getContentsForAssetVersions } = data;
+        const { AssetVersionContent } = getContentsForAssetVersions;
+
+        let updatedMetadatas = metadatas.slice();
+
+        AssetVersionContent.forEach(({ idAssetVersion, folders }, index) => {
+            const stateFolders: StateFolder[] = folders.map((folder, index) => ({
+                id: index,
+                name: folder,
+                variantType: getInitialVocabularyEntry(eVocabularySetID.eCaptureDataFileVariantType)
+            }));
+
+            updatedMetadatas = updatedMetadatas.map(metadata => {
+                const { photogrammetry } = metadata;
+                // TODO: replace index with fileId this afterwards
+                if (index === idAssetVersion) {
+                    return {
+                        ...metadata,
+                        photogrammetry: {
+                            ...photogrammetry,
+                            ...defaultVocabularyFields,
+                            folders: stateFolders
+                        }
+                    };
+                }
+
+                return metadata;
+            });
+        });
+
+        const updateMetadataFieldsAction: IngestionDispatchAction = {
+            type: METADATA_ACTIONS.UPDATE_METADATA_FIELDS,
+            metadatas: updatedMetadatas
+        };
+
+        ingestionDispatch(updateMetadataFieldsAction);
+    };
+
     return {
         getFieldErrors,
         getCurrentMetadata,
         getMetadataInfo,
-        updatePhotogrammetryFields
+        updatePhotogrammetryFields,
+        updateMetadataFolders
     };
 }
 
