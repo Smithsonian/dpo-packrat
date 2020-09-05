0c6896cb5f111dcac224a8f79e0991335e89c5fe
Jon Tyson <jon.tyson@gmail.com>
2020-09-05 11:12:57 -0700

    Parser: * moved to server/utils/parser * added support for capture_data CSV information in bulk ingest files

--- server/tests/utils/parser/parser.test.ts
+++ server/tests/utils/parser/parser.test.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import { join } from 'path';
-import { Parser, CSVTypes, SubjectsCSVFields, ItemsCSVFields, ModelsCSVFields } from '../../parser';
+import { Parser, CSVTypes, SubjectsCSVFields, ItemsCSVFields, ModelsCSVFields, CaptureDataCSVFields } from '../../../utils/parser';
 
-const mockPath = (type: CSVTypes) => join(__dirname, `../mock/parser/mock.${type}.csv`);
+const mockPath = (type: CSVTypes) => join(__dirname, `../../mock/parser/mock.${type}.csv`);
 
 describe('Parser: CSV parser', () => {
     test('Parser.parseCSV type subjects', async () => {
@@ -42,6 +42,14 @@ describe('Parser: CSV parser', () => {
         const fileStream = fs.createReadStream(path);
 
         const result = await Parser.parseCSV<ModelsCSVFields>(fileStream, CSVTypes.models);
+        expect(result).toBeTruthy();
+    });
+
+    test('Parser.parseCSV type capture data', async () => {
+        const path = mockPath(CSVTypes.captureData);
+        const fileStream = fs.createReadStream(path);
+
+        const result = await Parser.parseCSV<CaptureDataCSVFields>(fileStream, CSVTypes.captureData);
         expect(result).toBeTruthy();
     });
 });
