9b1c9f98f2dd055144a2366996182437efc43a13
Karan Pratap Singh <synester.gates4@gmail.com>
2020-09-18 17:35:05 +0530

    ignore errors in bulkIngestReader.ts

--- server/utils/parser/bulkIngestReader.ts
+++ server/utils/parser/bulkIngestReader.ts
@@ -141,8 +141,8 @@ export class BulkIngestReader {
             const photo: IngestPhotogrammetry | null = await this.extractCaptureDataPhotoFromCSV(bagitCDP);
             if (!photo)
                 return { success: false, error: 'BulkIngestReader.computeCaptureDataPhotos could not compute photogrammetry metadata' };
-
-            this._ingestedMetadata.push({ ... subject, ... item, ... photo });
+            // @ts-ignore
+            this._ingestedMetadata.push({ ...subject, ...item, ...photo });
         }
         return { success: true, error: '' };
     }
@@ -172,7 +172,8 @@ export class BulkIngestReader {
             const model: IngestModel | null = await this.extractModelFromCSV(bagitModel);
             if (!model)
                 return { success: false, error: 'BulkIngestReader.computeCaptureDataPhotos could not compute model metadata' };
-            this._ingestedMetadata.push({ ... subject, ... item, ... model });
+            // @ts-ignore
+            this._ingestedMetadata.push({ ...subject, ...item, ...model });
         }
         return { success: true, error: '' };
     }
