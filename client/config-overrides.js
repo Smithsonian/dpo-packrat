259d97b519c90e47e854b69b1255a93e833a2ae9
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-12 14:58:46 +0530

    added react fast refresh webpack plugin

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,3 +1,4 @@
 const { override, addExternalBabelPlugin } = require('customize-cra');
+const { addReactRefresh } = require('customize-cra-react-refresh');
 
-module.exports = override(addExternalBabelPlugin('react-activation/babel'));
+module.exports = override(addExternalBabelPlugin('react-activation/babel'), addReactRefresh());
