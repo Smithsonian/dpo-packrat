d95c21d7aff8be35e1c1be5b9702605a7ca3a290
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-20 12:51:57 +0530

    added test selectors in client

---
+++ client/src/config/test.selectors.ts
@@ -0,0 +1,10 @@
+const Selectors = {
+    AUTH: {
+        EMAIL_FIELD: 'auth-email-field',
+        PASSWORD_FIELD: 'auth-password-field',
+        LOGIN_BUTTON: 'auth-login-button',
+        LOGOUT_BUTTON: 'auth-logout-button'
+    }
+};
+
+export { Selectors };
