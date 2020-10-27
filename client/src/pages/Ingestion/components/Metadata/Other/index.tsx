73e5745a40fc1fccbabfe4e6a5021bec751c27da
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-27 13:43:37 +0530

    - Add metadata containers for model, scenes and other

---
+++ client/src/pages/Ingestion/components/Metadata/Other/index.tsx
@@ -0,0 +1,25 @@
+/**
+ * Metadata - Other
+ *
+ * This component renders the metadata fields for other asset types.
+ */
+import { Box, Typography } from '@material-ui/core';
+import React from 'react';
+import { useMetadataStore } from '../../../../../store';
+
+interface OtherProps {
+    metadataIndex: number;
+}
+
+function Other(props: OtherProps): React.ReactElement {
+    const { metadataIndex } = props;
+    const metadata = useMetadataStore(state => state.metadatas[metadataIndex]);
+
+    return (
+        <Box display='flex' flex={1} alignItems='center' justifyContent='center'>
+            <Typography variant='subtitle1' color='primary'>Metadata Not yet implemented for {metadata.file.name}</Typography>
+        </Box>
+    );
+}
+
+export default Other;
