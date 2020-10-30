5633118a49f97100607acf214d81098aa3356ee0
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-30 16:38:29 +0530

    identifier list fixes

--- client/src/pages/Ingestion/components/SubjectItem/index.tsx
+++ client/src/pages/Ingestion/components/SubjectItem/index.tsx
@@ -124,7 +124,7 @@ function SubjectItem(): React.ReactElement {
         const { file: { id, type } } = metadatas[0];
         const { isLast } = getMetadataInfo(id);
         const nextRoute = resolveSubRoute(HOME_ROUTES.INGESTION, `${INGESTION_ROUTE.ROUTES.METADATA}?fileId=${id}&type=${type}&last=${isLast}`);
-
+        toast.dismiss();
         history.push(nextRoute);
     };
 
