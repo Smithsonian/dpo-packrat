ca057382d824cb7d512719e38d5024ddab568e63
Karan Pratap Singh <synester.gates4@gmail.com>
2020-11-02 16:17:07 +0530

    updated field type component

--- client/src/components/shared/FieldType.tsx
+++ client/src/components/shared/FieldType.tsx
@@ -3,7 +3,7 @@
  *
  * This component wraps content and highlights it as required field or not.
  */
-import { Box, BoxProps, PropTypes, Typography } from '@material-ui/core';
+import { Box, BoxProps, PropTypes, Typography, TypographyProps } from '@material-ui/core';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Progress from './Progress';
@@ -35,18 +35,19 @@ interface FieldTypeProps {
     width?: string;
     direction?: string;
     containerProps?: BoxProps;
+    labelProps?: TypographyProps;
     align?: PropTypes.Alignment;
     marginTop?: number;
     error?: boolean;
     loading?: boolean;
-    children: React.ReactNode
+    children?: React.ReactNode;
 }
 
 function FieldType(props: FieldTypeProps): React.ReactElement {
-    const { label, renderLabel, children, align = 'left', direction, containerProps, loading } = props;
+    const { label, renderLabel, children, align = 'left', direction, containerProps, labelProps, loading } = props;
     const classes = useStyles(props);
 
-    let content: React.ReactNode = <Typography align={align} className={classes.label} variant='caption'>{label}</Typography>;
+    let content: React.ReactNode = <Typography align={align} className={classes.label} variant='caption' {...labelProps}>{label}</Typography>;
 
     if (renderLabel === false) {
         content = null;
