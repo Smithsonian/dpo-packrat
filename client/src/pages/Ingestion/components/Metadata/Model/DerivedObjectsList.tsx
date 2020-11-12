2c27cb8443e67ac8dc17efe591a3fbae04be2eb9
Karan Pratap Singh <synester.gates4@gmail.com>
2020-11-12 15:59:42 +0530

    added derived object list component

---
+++ client/src/pages/Ingestion/components/Metadata/Model/DerivedObjectsList.tsx
@@ -0,0 +1,103 @@
+/**
+ * DerivedObjectsList
+ *
+ * This component renders the derived object list with add capability.
+ */
+import { Box, Button, Typography } from '@material-ui/core';
+import clsx from 'clsx';
+import React from 'react';
+import { MdRemoveCircleOutline } from 'react-icons/md';
+import { NewTabLink } from '../../../../../components';
+import { StateDerivedObject, useVocabularyStore } from '../../../../../store';
+import { eVocabularySetID } from '../../../../../types/server';
+import { getDetailsUrlForObject, getTermForSystemObjectType } from '../../../../../utils/repository';
+import { Header, useStyles } from './SourceObjectsList';
+
+interface DerivedObjectsListProps {
+    derivedObjects: StateDerivedObject[];
+    onAdd: () => void;
+    onRemove?: (id: number) => void;
+    viewMode?: boolean;
+    disabled?: boolean;
+}
+
+function DerivedObjectsList(props: DerivedObjectsListProps): React.ReactElement {
+    const { derivedObjects, onAdd, onRemove, viewMode = false, disabled = false } = props;
+    const classes = useStyles(viewMode);
+
+    const titles = ['Source Object(s)', 'Variant Type', 'Object Type'];
+    const hasDerivedObjects = !!derivedObjects.length;
+
+    return (
+        <Box className={classes.container}>
+            <Header titles={titles} />
+            {hasDerivedObjects && (
+                <Box className={classes.list}>
+                    {derivedObjects.map((derivedObject: StateDerivedObject, index: number) => (
+                        <Item
+                            key={index}
+                            viewMode={viewMode}
+                            derivedObject={derivedObject}
+                            onRemove={onRemove}
+                        />
+                    ))}
+                </Box>
+            )}
+            <Button
+                className={classes.addButton}
+                disableElevation
+                color='primary'
+                variant='contained'
+                onClick={() => onAdd()}
+                disabled={disabled}
+            >
+                Add
+            </Button>
+        </Box>
+    );
+}
+
+interface ItemProps {
+    derivedObject: StateDerivedObject;
+    onRemove?: (id: number) => void;
+    viewMode?: boolean;
+}
+
+function Item(props: ItemProps): React.ReactElement {
+    const { derivedObject, onRemove, viewMode = false } = props;
+    const { idSystemObject, name, variantType, objectType } = derivedObject;
+    const classes = useStyles(viewMode);
+    const getEntries = useVocabularyStore(state => state.getEntries);
+
+    const entries = getEntries(eVocabularySetID.eCaptureDataFileVariantType);
+    const variant = entries.find(entry => entry.idVocabulary === variantType);
+
+    const remove = () => onRemove?.(idSystemObject);
+
+    return (
+        <Box
+            display='flex'
+            flex={1}
+            flexDirection='row'
+            alignItems='center'
+            pb='10px'
+        >
+            <Box display='flex' flex={2}>
+                <NewTabLink to={getDetailsUrlForObject(idSystemObject)}>
+                    <Typography className={clsx(classes.label, classes.labelUnderline)}>{name}</Typography>
+                </NewTabLink>
+            </Box>
+            <Box display='flex' flex={3}>
+                <Typography className={classes.label}>{variant?.Term}</Typography>
+            </Box>
+            <Box display='flex' flex={1}>
+                <Typography className={classes.label}>{getTermForSystemObjectType(objectType)}</Typography>
+            </Box>
+            <Box width='50px'>
+                {!viewMode && <MdRemoveCircleOutline className={classes.removeIcon} onClick={remove} size={24} />}
+            </Box>
+        </Box>
+    );
+}
+
+export default DerivedObjectsList;
--- client/src/pages/Repository/components/DetailsView/index.tsx
+++ client/src/pages/Repository/components/DetailsView/index.tsx
@@ -5,10 +5,16 @@
  */
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
-import { eSystemObjectType } from '../../../../types/server';
+import IdentifierList from '../../../../components/shared/IdentifierList';
+import { useVocabularyStore } from '../../../../store';
+import { eSystemObjectType, eVocabularySetID } from '../../../../types/server';
+import DerivedObjectsList from '../../../Ingestion/components/Metadata/Model/DerivedObjectsList';
+import ObjectSelectModal from '../../../Ingestion/components/Metadata/Model/ObjectSelectModal';
+import SourceObjectsList from '../../../Ingestion/components/Metadata/Model/SourceObjectsList';
 import DetailsHeader from './DetailsHeader';
+import DetailsThumbnail from './DetailsThumbnail';
 import ObjectNotFoundView from './ObjectNotFoundView';
 
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
@@ -16,11 +22,14 @@ const useStyles = makeStyles(({ palette, breakpoints }) => ({
         display: 'flex',
         flex: 1,
         flexDirection: 'column',
+        maxHeight: 'calc(100vh - 160px)',
         padding: 20,
         marginBottom: 20,
         borderRadius: 10,
+        overflowY: 'scroll',
         backgroundColor: palette.primary.light,
         [breakpoints.down('lg')]: {
+            maxHeight: 'calc(100vh - 120px)',
             padding: 10
         }
     }
@@ -41,19 +50,67 @@ const mockData = {
                 name: 'Armstrong Suit',
                 objectType: eSystemObjectType.eProject,
             }, {
-                idSystemObject: 0,
+                idSystemObject: 2,
                 name: 'Armstrong Glove',
                 objectType: eSystemObjectType.eSubject,
             }, {
-                idSystemObject: 0,
+                idSystemObject: 3,
                 name: 'Armstrong Glove Full',
                 objectType: eSystemObjectType.eItem,
             }, {
-                idSystemObject: 0,
+                idSystemObject: 4,
                 name: 'PhotoSet1.zip',
                 objectType: eSystemObjectType.eCaptureData,
             }
-        ]
+        ],
+        identifiers: [
+            {
+                id: 0,
+                identifier: '31958de82-ab13-4049-c979-746e2fbe229e',
+                identifierType: 75,
+                selected: true
+            },
+        ],
+        sourceObjects: [
+            {
+                idSystemObject: 0,
+                name: 'PhotoSetAlpha1.zip',
+                identifier: 'a5cf8642-7466-4896-a0a2-d698f2009cd3',
+                objectType: eSystemObjectType.eModel
+            },
+            {
+                idSystemObject: 0,
+                name: 'PhotoSetAlpha2.zip',
+                identifier: 'a5cf8642-7466-4896-a0a2-d698f2009cd3',
+                objectType: eSystemObjectType.eCaptureData
+            }
+        ],
+        derivedObjects: [
+            {
+                idSystemObject: 0,
+                name: 'Photo1.zip',
+                variantType: 28,
+                objectType: eSystemObjectType.eAsset
+            },
+            {
+                idSystemObject: 1,
+                name: 'Photo2.zip',
+                variantType: 28,
+                objectType: eSystemObjectType.eAsset
+            },
+            {
+                idSystemObject: 2,
+                name: 'Photo3.zip',
+                variantType: 28,
+                objectType: eSystemObjectType.eAsset
+            },
+            {
+                idSystemObject: 3,
+                name: 'Photo4.zip',
+                variantType: 28,
+                objectType: eSystemObjectType.eAsset
+            },
+        ],
     }
 };
 
@@ -64,13 +121,45 @@ type DetailsParams = {
 function DetailsView(): React.ReactElement {
     const classes = useStyles();
     const params = useParams<DetailsParams>();
+    const [modalOpen, setModalOpen] = useState(false);
+
     const data = mockData[params.idSystemObject];
+    const getEntries = useVocabularyStore(state => state.getEntries);
 
     if (!data) {
         return <ObjectNotFoundView loading={false} />;
     }
 
-    const { name, objectType, path, retired } = data;
+    const { name, objectType, path, retired, identifiers, sourceObjects, derivedObjects, thumbnail } = data;
+    const disabled: boolean = false;
+
+    const addIdentifer = () => {
+        alert('TODO: KARAN: add identifier');
+    };
+
+    const removeIdentifier = () => {
+        alert('TODO: KARAN: remove identifier');
+    };
+
+    const updateIdentifierFields = () => {
+        alert('TODO: KARAN: update identifier');
+    };
+
+    const onSelectedObjects = () => {
+        onModalClose();
+    };
+
+    const onModalClose = () => {
+        setModalOpen(false);
+    };
+
+    const onAddSourceObject = () => {
+        setModalOpen(true);
+    };
+
+    const onAddDerivedObject = () => {
+        alert('TODO: KARAN: on add derived object');
+    };
 
     return (
         <Box className={classes.container}>
@@ -79,7 +168,41 @@ function DetailsView(): React.ReactElement {
                 objectType={objectType}
                 path={path}
                 retired={retired}
-                editable
+                disabled={disabled}
+            />
+            <Box display='flex' flex={1}>
+                <Box display='flex' flex={1} flexDirection='column'>
+                    <IdentifierList
+                        disabled={disabled}
+                        identifiers={identifiers}
+                        identifierTypes={getEntries(eVocabularySetID.eIdentifierIdentifierType)}
+                        onAdd={addIdentifer}
+                        onRemove={removeIdentifier}
+                        onUpdate={updateIdentifierFields}
+                    />
+                    <SourceObjectsList
+                        viewMode
+                        disabled={disabled}
+                        sourceObjects={sourceObjects}
+                        onAdd={onAddSourceObject}
+                    />
+                    <DerivedObjectsList
+                        viewMode
+                        disabled={disabled}
+                        derivedObjects={derivedObjects}
+                        onAdd={onAddDerivedObject}
+                    />
+                </Box>
+                <Box display='flex' flex={1} padding={2}>
+                    <DetailsThumbnail thumbnail={thumbnail} />
+                </Box>
+            </Box>
+
+            <ObjectSelectModal
+                open={modalOpen}
+                onSelectedObjects={onSelectedObjects}
+                onModalClose={onModalClose}
+                selectedObjects={sourceObjects}
             />
         </Box>
     );
