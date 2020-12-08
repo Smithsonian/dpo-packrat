29d200233521cec725ea5bde57af03c47eb32154
Karan Pratap Singh <synester.gates4@gmail.com>
2020-12-08 15:32:32 +0530

    updated asset details component

---
+++ client/src/pages/Repository/components/DetailsView/DetailsTab/AssetDetailsTable.tsx
@@ -0,0 +1,125 @@
+/**
+ * AssetDetailsTable
+ *
+ * This component renders asset details table tab for the DetailsTab component.
+ */
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import React from 'react';
+import { NewTabLink, Progress } from '../../../../../components';
+import { StateAssetDetail, useVocabularyStore } from '../../../../../store';
+import { eVocabularySetID } from '../../../../../types/server';
+import { getDetailsUrlForObject } from '../../../../../utils/repository';
+import { formatBytes } from '../../../../../utils/upload';
+import { useObjectAssets } from '../../../hooks/useDetailsView';
+
+export const useStyles = makeStyles(({ palette }) => ({
+    container: {
+        width: '100%',
+        background: palette.secondary.light,
+        padding: 5,
+        borderRadius: 5
+    },
+    header: {
+        fontSize: '0.9em',
+        color: palette.primary.dark
+    },
+    value: {
+        fontSize: '0.8em',
+        color: palette.primary.dark
+    },
+    empty: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        background: palette.secondary.light,
+        padding: 40,
+        borderRadius: 5
+    },
+    link: {
+        textDecoration: 'underline'
+    }
+}));
+
+interface AssetDetailsTableProps {
+    idSystemObject: number;
+}
+
+function AssetDetailsTable(props: AssetDetailsTableProps): React.ReactElement {
+    const classes = useStyles();
+    const { idSystemObject } = props;
+    const { data, loading } = useObjectAssets(idSystemObject);
+    const getVocabularyTerm = useVocabularyStore(state => state.getVocabularyTerm);
+
+    const headers: string[] = [
+        'Name',
+        'Path',
+        'Asset Type',
+        'Version',
+        'Date Created',
+        'Size',
+    ];
+
+    if (!data || loading) {
+        return <EmptyTable />;
+    }
+
+    const { assetDetails } = data.getAssetDetailsForSystemObject;
+
+    return (
+        <table className={classes.container}>
+            <tr>
+                {headers.map((header, index: number) => (
+                    <th key={index} align='left'>
+                        <Typography className={classes.header}>{header}</Typography>
+                    </th>
+                ))}
+            </tr>
+            {assetDetails.map((assetDetail: StateAssetDetail, index: number) => (
+                <tr key={index}>
+                    <td>
+                        <NewTabLink to={getDetailsUrlForObject(assetDetail.idSystemObject)}>
+                            <Typography className={clsx(classes.value, classes.link)}>{assetDetail.name}</Typography>
+                        </NewTabLink>
+                    </td>
+                    <td>
+                        <Typography className={classes.value}>{assetDetail.path}</Typography>
+                    </td>
+                    <td>
+                        <Typography className={classes.value}>{getVocabularyTerm(eVocabularySetID.eAssetAssetType, assetDetail.assetType)}</Typography>
+                    </td>
+                    <td align='left'>
+                        <Typography className={classes.value}>{assetDetail.version}</Typography>
+                    </td>
+                    <td>
+                        <Typography className={classes.value}>{assetDetail.dateCreated}</Typography>
+                    </td>
+                    <td>
+                        <Typography className={classes.value}>{formatBytes(assetDetail.size)}</Typography>
+                    </td>
+                </tr>
+            ))}
+            <td colSpan={6}>
+                {!assetDetails.length && (
+                    <Box my={2}>
+                        <Typography align='center' className={classes.value}>No assets found</Typography>
+                    </Box>
+                )}
+            </td>
+        </table>
+    );
+}
+
+export function EmptyTable(): React.ReactElement {
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.empty}>
+            <Progress size={20} />
+        </Box>
+    );
+}
+
+export default AssetDetailsTable;
