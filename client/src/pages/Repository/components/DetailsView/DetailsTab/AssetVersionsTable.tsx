ed72f111b0b84c28954e31df06228ad06fbeb6c7
Karan Pratap Singh <synester.gates4@gmail.com>
2020-12-10 20:43:39 +0530

    minor fixes to asset detail tables

--- client/src/pages/Repository/components/DetailsView/DetailsTab/AssetVersionsTable.tsx
+++ client/src/pages/Repository/components/DetailsView/DetailsTab/AssetVersionsTable.tsx
@@ -38,43 +38,53 @@ function AssetVersionsTable(props: AssetVersionsTableProps): React.ReactElement 
 
     return (
         <table className={classes.container}>
-            <tr>
-                {headers.map((header, index: number) => (
-                    <th key={index} align='left'>
-                        <Typography className={classes.header}>{header}</Typography>
-                    </th>
-                ))}
-            </tr>
-            {versions.map((version: StateDetailVersion, index: number) => (
-                <tr key={index}>
-                    <td>
-                        <NewTabLink to={getDetailsUrlForObject(version.idSystemObject)}>
-                            <Typography className={clsx(classes.value, classes.link)}>{version.version}</Typography>
-                        </NewTabLink>
-                    </td>
-                    <td>
-                        <NewTabLink to={getDetailsUrlForObject(version.idSystemObject)}>
-                            <Typography className={clsx(classes.value, classes.link)}>{version.name}</Typography>
-                        </NewTabLink>
-                    </td>
-                    <td align='left'>
-                        <Typography className={classes.value}>{version.creator}</Typography>
-                    </td>
-                    <td>
-                        <Typography className={classes.value}>{version.dateCreated}</Typography>
-                    </td>
-                    <td>
-                        <Typography className={classes.value}>{formatBytes(version.size)}</Typography>
-                    </td>
+            <thead>
+                <tr>
+                    {headers.map((header, index: number) => (
+                        <th key={index} align='left'>
+                            <Typography className={classes.header}>{header}</Typography>
+                        </th>
+                    ))}
                 </tr>
-            ))}
-            <td colSpan={5}>
-                {!versions.length && (
-                    <Box my={2}>
-                        <Typography align='center' className={classes.value}>No versions found</Typography>
-                    </Box>
-                )}
-            </td>
+
+            </thead>
+
+            <tbody>
+                {versions.map((version: StateDetailVersion, index: number) => (
+                    <tr key={index}>
+                        <td>
+                            <NewTabLink to={getDetailsUrlForObject(version.idSystemObject)}>
+                                <Typography className={clsx(classes.value, classes.link)}>{version.version}</Typography>
+                            </NewTabLink>
+                        </td>
+                        <td>
+                            <NewTabLink to={getDetailsUrlForObject(version.idSystemObject)}>
+                                <Typography className={clsx(classes.value, classes.link)}>{version.name}</Typography>
+                            </NewTabLink>
+                        </td>
+                        <td align='left'>
+                            <Typography className={classes.value}>{version.creator}</Typography>
+                        </td>
+                        <td>
+                            <Typography className={classes.value}>{version.dateCreated}</Typography>
+                        </td>
+                        <td>
+                            <Typography className={classes.value}>{formatBytes(version.size)}</Typography>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+
+            <tfoot>
+                <td colSpan={5}>
+                    {!versions.length && (
+                        <Box my={2}>
+                            <Typography align='center' className={classes.value}>No versions found</Typography>
+                        </Box>
+                    )}
+                </td>
+            </tfoot>
+
         </table>
     );
 }
