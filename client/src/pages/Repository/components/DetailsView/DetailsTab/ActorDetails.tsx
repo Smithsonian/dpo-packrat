d40d2dbe6eea9576f781a3b5eb15b7b664fa2f10
Karan Pratap Singh <synester.gates4@gmail.com>
2020-12-14 14:28:40 +0530

    added actor details

--- client/src/pages/Repository/components/DetailsView/DetailsTab/ActorDetails.tsx
+++ client/src/pages/Repository/components/DetailsView/DetailsTab/ActorDetails.tsx
@@ -3,22 +3,55 @@
  *
  * This component renders details tab for Actor specific details used in DetailsTab component.
  */
-import React from 'react';
-import { Loader } from '../../../../../components';
+import { Box } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { InputField, Loader } from '../../../../../components';
 import { GetDetailsTabDataForObjectQueryResult } from '../../../../../types/graphql';
 
 interface ActorDetailsProps extends GetDetailsTabDataForObjectQueryResult {
     disabled: boolean;
 }
 
+interface ActorDetailsFields {
+    OrganizationName?: string | null;
+}
+
 function ActorDetails(props: ActorDetailsProps): React.ReactElement {
-    const { data, loading } = props;
+    const { data, loading, disabled, } = props;
+
+    const [details, setDetails] = useState<ActorDetailsFields>({});
+
+    useEffect(() => {
+        if (data && !loading) {
+            const { Actor } = data.getDetailsTabDataForObject;
+            setDetails({
+                OrganizationName: Actor?.OrganizationName,
+            });
+        }
+    }, [data, loading]);
 
     if (!data || loading) {
         return <Loader minHeight='15vh' />;
     }
 
-    return <span>Actor Details</span>;
+    const onSetField = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setDetails(details => ({ ...details, [name]: value }));
+    };
+
+    return (
+        <Box>
+            <InputField
+                viewMode
+                required
+                disabled={disabled}
+                label='OrganizationName'
+                value={details?.OrganizationName}
+                name='OrganizationName'
+                onChange={onSetField}
+            />
+        </Box>
+    );
 }
 
 export default ActorDetails;
