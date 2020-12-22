58cf465cadfb50773f29f681f600d50a2a794fbc
Karan Pratap Singh <synester.gates4@gmail.com>
2020-12-22 20:18:44 +0530

    added details components for asset details

--- client/src/pages/Repository/components/DetailsView/DetailsTab/AssetDetails.tsx
+++ client/src/pages/Repository/components/DetailsView/DetailsTab/AssetDetails.tsx
@@ -3,22 +3,82 @@
  *
  * This component renders details tab for Asset specific details used in DetailsTab component.
  */
-import React from 'react';
-import { Loader } from '../../../../../components';
+import { Box } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { InputField, Loader, SelectField } from '../../../../../components';
+import { useVocabularyStore } from '../../../../../store';
 import { GetDetailsTabDataForObjectQueryResult } from '../../../../../types/graphql';
+import { eVocabularySetID } from '../../../../../types/server';
+import { withDefaultValueNumber } from '../../../../../utils/shared';
 
 interface AssetDetailsProps extends GetDetailsTabDataForObjectQueryResult {
     disabled: boolean;
 }
 
+interface AssetDetailsFields {
+    FilePath?: string | null;
+    AssetType?: number | null;
+}
+
 function AssetDetails(props: AssetDetailsProps): React.ReactElement {
-    const { data, loading } = props;
+    const { data, loading, disabled, } = props;
+
+    const [details, setDetails] = useState<AssetDetailsFields>({});
+    const [getEntries, getInitialEntry] = useVocabularyStore(state => [state.getEntries, state.getInitialEntry]);
+
+    useEffect(() => {
+        if (data && !loading) {
+            const { Asset } = data.getDetailsTabDataForObject;
+            setDetails({
+                FilePath: Asset?.FilePath,
+                AssetType: Asset?.VAssetType?.idVocabulary
+            });
+        }
+    }, [data, loading]);
 
     if (!data || loading) {
         return <Loader minHeight='15vh' />;
     }
 
-    return <span>Asset Details</span>;
+    const onSetField = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setDetails(details => ({ ...details, [name]: value }));
+    };
+
+    const setIdField = ({ target }): void => {
+        const { name, value } = target;
+        let idFieldValue: number | null = null;
+
+        if (value) {
+            idFieldValue = Number.parseInt(value, 10);
+        }
+        console.log({ [name]: idFieldValue });
+        setDetails(details => ({ ...details, [name]: idFieldValue }));
+    };
+
+    return (
+        <Box>
+            <SelectField
+                required
+                viewMode
+                disabled={disabled}
+                label='Asset Type'
+                value={withDefaultValueNumber(details?.AssetType as never, getInitialEntry(eVocabularySetID.eAssetAssetType))}
+                name='AssetType'
+                onChange={setIdField}
+                options={getEntries(eVocabularySetID.eAssetAssetType)}
+            />
+            <InputField
+                viewMode
+                required
+                disabled={disabled}
+                label='FilePath'
+                value={details?.FilePath}
+                name='FilePath'
+                onChange={onSetField}
+            />
+        </Box>
+    );
 }
 
 export default AssetDetails;
