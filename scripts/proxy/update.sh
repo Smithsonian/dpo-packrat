1d9979ab124b42506ce46ca61e378ce33034d133
Karan Pratap Singh <synester.gates4@gmail.com>
2021-01-18 01:58:58 +0530

    update script

--- scripts/proxy/update.sh
+++ scripts/proxy/update.sh
@@ -1,8 +1,11 @@
 # Updates the global nginx.conf on the server
-sudo mv -v ./scripts/proxy/nginx.conf /etc/nginx/nginx.conf
+sudo cp -v ./scripts/proxy/nginx.conf /etc/nginx/nginx.conf
 
-# restart nginx service
+# Restart nginx service
 sudo service nginx restart
+
+# Check if status is active
+sudo service nginx status
 
 # Use the command below to stop if you're getting nginx: [emerg] bind() to 0.0.0.0:80 failed
 # sudo fuser -k 80/tcp
