8d6fb4d735392c45a49bff86bfbbf87f249c7f9a
Karan Pratap Singh <synester.gates4@gmail.com>
2021-01-18 15:39:22 +0530

    added test use

--- server/db/sql/scripts/Packrat.DATA.sql
+++ server/db/sql/scripts/Packrat.DATA.sql
@@ -1114,6 +1114,7 @@ INSERT INTO User (Name, EmailAddress, SecurityID, Active, DateActivated) VALUES 
 INSERT INTO User (Name, EmailAddress, SecurityID, Active, DateActivated) VALUES ('Vince Rossi', 'rossiv@si.edu', 'TBD', 1, NOW());
 INSERT INTO User (Name, EmailAddress, SecurityID, Active, DateActivated) VALUES ('Jamie Cope', 'copeg@si.edu', 'TBD', 1, NOW());
 INSERT INTO User (Name, EmailAddress, SecurityID, Active, DateActivated) VALUES ('Karan Pratap Singh', 'singhk@si.edu', 'TBD', 1, NOW());
+INSERT INTO User (Name, EmailAddress, SecurityID, Active, DateActivated) VALUES ('Test User', 'user@test.com', 'TBD', 1, NOW());
 
 INSERT INTO UnitEdan (idUnit, Abbreviation) VALUES (1, '3D_YT');
 INSERT INTO UnitEdan (idUnit, Abbreviation) VALUES (4, 'AAA');
