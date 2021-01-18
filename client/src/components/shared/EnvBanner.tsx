4524845425696108daf4794460be647bb224d1a5
Karan Pratap Singh <synester.gates4@gmail.com>
2021-01-18 16:34:49 +0530

    added dev banner to client

---
+++ client/src/components/shared/EnvBanner.tsx
@@ -0,0 +1,46 @@
+/**
+ * EnvBanner
+ *
+ * This component renders banner for the environment the app is running.
+ */
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+
+const useStyles = makeStyles(({ palette, typography }) => ({
+    container: {
+        display: 'flex',
+        position: 'absolute',
+        zIndex: 100,
+        backgroundColor: palette.primary.main,
+        padding: 10,
+        right: 0,
+        bottom: 0,
+        borderTopLeftRadius: 5,
+        color: palette.background.paper
+    },
+    content: {
+        fontWeight: typography.fontWeightMedium
+    }
+}));
+
+interface EnvBannerProps {
+    renderFor: string[];
+}
+
+function EnvBanner(props: EnvBannerProps): React.ReactElement | null {
+    const { renderFor } = props;
+    const classes = useStyles();
+
+    const env = process.env.NODE_ENV;
+
+    if (!renderFor.includes(env)) return null;
+
+    return (
+        <Box className={classes.container}>
+            <Typography className={classes.content} color='inherit' variant='caption'>{env.toUpperCase()}</Typography>
+        </Box>
+    );
+}
+
+export default EnvBanner;
