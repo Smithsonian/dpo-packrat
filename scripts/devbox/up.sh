447a2c7c38e99fa6476dc9914ff7c7c0b70a1626
Karan Pratap Singh <synester.gates4@gmail.com>
2021-01-30 13:36:29 +0530

    script update

--- scripts/devbox/up.sh
+++ scripts/devbox/up.sh
@@ -4,3 +4,5 @@ IMAGE=node:12.18.4
 PACKRAT_WORKDIR=/app
 # Run node docker image and map port 3000, 4000 for access to client and server
 docker run --name packrat-devbox -p 3000:3000 -p 4000:4000 -v ${PWD}:${PACKRAT_WORKDIR} --env-file ./.env.dev -itd $IMAGE
+
+echo "Done"
