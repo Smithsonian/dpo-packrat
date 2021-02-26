277e162d3e12186becc1df26367ba0d7eeb00ad7
Hsin tung <hsintung@Hsins-MacBook-Pro-2.local>
2021-02-25 20:31:12 -0800

    completed styling and fixed time rendering issues

--- client/src/pages/Ingestion/components/IngestionSidebar/IngestionSidebarMenuOption.tsx
+++ client/src/pages/Ingestion/components/IngestionSidebar/IngestionSidebarMenuOption.tsx
@@ -23,14 +23,14 @@ const useStyles = makeStyles(({ palette }) => ({
         overflow: 'hidden',
         borderRadius: 5,
         marginTop: 2,
-        color: ({ isSelected }: IngestionSidebarMenuOptionProps) => isSelected ? palette.primary.main : palette.primary.dark,
-        backgroundColor: ({ isSelected }: IngestionSidebarMenuOptionProps) => isSelected ? palette.primary.light : Colors.defaults.white,
+        color: ({ isSelected }: IngestionSidebarMenuOptionProps) => (isSelected ? palette.primary.main : palette.primary.dark),
+        backgroundColor: ({ isSelected }: IngestionSidebarMenuOptionProps) => (isSelected ? palette.primary.light : Colors.defaults.white),
         '&:hover': {
-            cursor: ({ enabled }: IngestionSidebarMenuOptionProps) => enabled ? 'pointer' : undefined,
-            color: ({ enabled }: IngestionSidebarMenuOptionProps) => enabled ? palette.primary.main : undefined,
-            backgroundColor: ({ enabled }: IngestionSidebarMenuOptionProps) => enabled ? palette.primary.light : undefined
-        },
-    },
+            cursor: ({ enabled }: IngestionSidebarMenuOptionProps) => (enabled ? 'pointer' : undefined),
+            color: ({ enabled }: IngestionSidebarMenuOptionProps) => (enabled ? palette.primary.main : undefined),
+            backgroundColor: ({ enabled }: IngestionSidebarMenuOptionProps) => (enabled ? palette.primary.light : undefined)
+        }
+    }
 }));
 
 export type SidebarRouteTypes = INGESTION_ROUTES_TYPE | string;
@@ -50,16 +50,16 @@ function IngestionSidebarMenuOption(props: IngestionSidebarMenuOptionProps): Rea
 
     const subContent: React.ReactNode = (
         <React.Fragment>
-            <Typography color='inherit' variant='body1'>{title}</Typography>
-            <Typography color='textSecondary' variant='caption'>{subtitle}</Typography>
+            <Typography color='inherit' variant='body1'>
+                {title}
+            </Typography>
+            <Typography color='textSecondary' variant='caption'>
+                {subtitle}
+            </Typography>
         </React.Fragment>
     );
 
-    let content = (
-        <Box className={classes.container}>
-            {subContent}
-        </Box>
-    );
+    let content = <Box className={classes.container}>{subContent}</Box>;
 
     if (enabled) {
         content = (
@@ -69,11 +69,7 @@ function IngestionSidebarMenuOption(props: IngestionSidebarMenuOptionProps): Rea
         );
     }
 
-    return (
-        <React.Fragment>
-            {content}
-        </React.Fragment>
-    );
+    return <React.Fragment>{content}</React.Fragment>;
 }
 
 export default memo(IngestionSidebarMenuOption);
