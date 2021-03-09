824efdda0d9f15d6c9ee000bc9a47df1a4a186f8
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-03-08 16:21:53 -0800

    Devbox:  updating with support for solr * Placed variables directly into scripts * Renamed packrat-db to packrat-devbox-db * Added solr.sh script to configure solr container * Added packrat-devbox-solr to packrat-devbox-network

--- conf/scripts/devbox/db.sh
+++ conf/scripts/devbox/db.sh
@@ -1,14 +1,11 @@
 # Creates and seeds the packrat db
 
-IMAGE=mariadb:10.5
-PACKRAT_WORKDIR=/app
-
-docker run --name packrat-db -p 3306:3306 -v ${PWD}/server/db/sql:${PACKRAT_WORKDIR} --env-file ./.env.dev -itd $IMAGE
+docker run --name packrat-devbox-db -p 3306:3306 -v ${PWD}/server/db/sql:/app --env-file ./.env.dev -itd mariadb:10.5
 
 SQL_PASSWORD=$(grep MYSQL_ROOT_PASSWORD .env.dev | cut -d '=' -f2)
 
-docker exec -i packrat-db sh -c "mysql -u root -p$SQL_PASSWORD < /app/scripts/Packrat.SCHEMA.sql"
-docker exec -i packrat-db sh -c "mysql -u root -p$SQL_PASSWORD < /app/scripts/Packrat.PROC.sql"
-docker exec -i packrat-db sh -c "mysql -u root -p$SQL_PASSWORD < /app/scripts/Packrat.DATA.sql"
+docker exec -i packrat-devbox-db sh -c "mysql -u root -p$SQL_PASSWORD < /app/scripts/Packrat.SCHEMA.sql"
+docker exec -i packrat-devbox-db sh -c "mysql -u root -p$SQL_PASSWORD < /app/scripts/Packrat.PROC.sql"
+docker exec -i packrat-devbox-db sh -c "mysql -u root -p$SQL_PASSWORD < /app/scripts/Packrat.DATA.sql"
 
 echo "Done"
---
+++ conf/scripts/devbox/solr.sh
@@ -0,0 +1,7 @@
+# Devbox for Packrat
+
+# Run node docker image and map port 8983 for access to solr
+docker run --name packrat-devbox-solr -p 8983:8983 -v ${PWD}/server/config/solr/data/packrat/:/var/solr/data/packrat/ --env-file ./.env.dev -itd solr:8
+
+echo "Done"
+## COPY --chown=solr:solr ./server/config/solr/data/packrat/ /var/solr/data/packrat/
