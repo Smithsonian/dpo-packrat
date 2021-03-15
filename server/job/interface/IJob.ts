7b6db1782ab64c529591d6f716ad301ea94b74d2
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-03-14 17:06:16 -0700

    Cook Integration WIP: * Added node-schedule package and related types * Began implementation of job execution

--- server/job/interface/IJob.ts
+++ server/job/interface/IJob.ts
@@ -1,11 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types */
-import { eJobRunStatus } from '../../db';
-
 export interface IJob {
-    start(): Promise<boolean>;
-    pause(): Promise<boolean>;
-    resume(): Promise<boolean>;
-    terminate(): Promise<boolean>;
-    getStatus(): Promise<eJobRunStatus>;
-    getConfiguration(): Promise<any>;
+    name(): string;
+    jobCallback(fireDate: Date): void;
+    getConfiguration(): any;
 }
