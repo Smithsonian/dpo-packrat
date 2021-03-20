99eafd80e768dd4ef40f97938d243f6c3fa15232
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-03-20 16:18:26 -0700

    Job Execution & Cook Integration:  testing WIP

--- server/tests/jobs/impl/JobsNS.test.ts
+++ server/tests/jobs/impl/JobsNS.test.ts
@@ -4,18 +4,18 @@ import { IJob, IJobEngine, JobFactory } from '../../../job/interface';
 import { JOB_TYPE } from '../../../config';
 import { eVocabularyID } from '../../../cache';
 
+import * as COOK from '../../../job/impl/Cook';
 import * as DBAPI from '../../../db';
 import * as H from '../../../utils/helpers';
 import * as LOG from '../../../utils/logger';
 import * as CACHE from '../../../cache';
-import * as MTS from '../../db/composite/Model.setup';
+import * as TESTMODEL from '../../db/composite/Model.setup';
 
 let jobEngine: IJobEngine | null = null;
 let modelTestAvailable: boolean | null = null;
-let modelTestSetup: MTS.ModelTestSetup | null = null;
+let MTS: TESTMODEL.ModelTestSetup | null = null;
 
 afterAll(async done => {
-    jest.setTimeout(5000);
     await H.Helpers.sleep(2000);
     done();
 });
@@ -26,8 +26,8 @@ describe('Jobs NS Init', () => {
         jobEngine = await JobFactory.getInstance(JOB_TYPE.NODE_SCHEDULE);
         expect(jobEngine).toBeTruthy();
 
-        modelTestSetup = new MTS.ModelTestSetup();
-        modelTestAvailable = await modelTestSetup.initialize();
+        MTS = new TESTMODEL.ModelTestSetup();
+        modelTestAvailable = await MTS.initialize();
         expect(modelTestAvailable === null || modelTestAvailable).toBeTruthy(); // null means that model test files were not available, which is ok
     });
 });
@@ -39,18 +39,22 @@ describe('Jobs NS Lifecycle', () => {
     // confirm DB job and jobrun are created and in correct state
     // test job cancellation
 
-    test('IJob.create', async () => {
+    test('IJob.Cook si-packrat-inspect', async () => {
         const idVJobType: number | undefined = await computeVocabularyDBID(eVocabularyID.eJobJobTypeCookSIPackratInspect);
         const dbJobs: DBAPI.Job[] | null = await DBAPI.Job.fetchByType(idVJobType || 0);
         expect(dbJobs).toBeTruthy();
         if (!dbJobs || dbJobs.length != 0)
             return;
+        const idJob: number = dbJobs[0].idJob;
 
-        await testCreateByID(dbJobs[0].idJob, null, null, null, true);
-        await testCreateByType(eVocabularyID.eJobJobTypeCookSIPackratInspect, null, null, null, true);
+        await testCook('fbx-stand-alone', eVocabularyID.eJobJobTypeCookSIPackratInspect);
 
-        await testCreateByID(0, null, null, null, false);
-        await testCreateByType(eVocabularyID.eNone, null, null, null, false);
+        await testCreateByID(idJob, undefined, null, null, true);
+        await testCreateByType(eVocabularyID.eJobJobTypeCookSIPackratInspect, undefined, null, null, true);
+
+        // expected failures:
+        await testCreateByID(0, undefined, null, null, false);
+        await testCreateByType(eVocabularyID.eNone, undefined, null, null, false);
     });
 
     test('Navigation Children', async () => {
@@ -60,11 +64,11 @@ describe('Jobs NS Lifecycle', () => {
 
 });
 
-async function testCreateByID(idJob: number, idAssetVersions: number[] | null, parameters: any,
+async function testCreateByID(idJob: number, idAssetVersions: number[] | undefined, parameters: any,
     schedule: string | null, expectSuccess: boolean = true): Promise<IJob | null> {
     expect(jobEngine).toBeTruthy(); if (!jobEngine) return null;
 
-    const job: IJob | null = await jobEngine.createByID(idJob, idAssetVersions, parameters, schedule);
+    const job: IJob | null = await jobEngine.createByID(idJob, idAssetVersions || null, parameters, schedule);
     if (expectSuccess)
         expect(job).toBeTruthy();
     else
@@ -72,11 +76,11 @@ async function testCreateByID(idJob: number, idAssetVersions: number[] | null, p
     return job;
 }
 
-async function testCreateByType(eJobType: eVocabularyID, idAssetVersions: number[] | null, parameters: any,
+async function testCreateByType(eJobType: eVocabularyID, idAssetVersions: number[] | undefined, parameters: any,
     schedule: string | null, expectSuccess: boolean = true): Promise<IJob | null> {
     expect(jobEngine).toBeTruthy(); if (!jobEngine) return null;
 
-    const job: IJob | null = await jobEngine.createByType(eJobType, idAssetVersions, parameters, schedule);
+    const job: IJob | null = await jobEngine.createByType(eJobType, idAssetVersions || null, parameters, schedule);
     if (expectSuccess)
         expect(job).toBeTruthy();
     else
@@ -89,4 +93,30 @@ async function computeVocabularyDBID(eJobType: eVocabularyID): Promise<number | 
     if (!idVJobType)
         LOG.logger.error(`computeVocabularyDBID unable to fetch Job type from ${CACHE.eVocabularyID[eJobType]}`);
     return idVJobType;
+}
+
+async function testCook(testCase: string, eJobType: eVocabularyID): Promise<boolean> {
+    const assetVersionIDs: number[] | undefined = MTS?.getTestCase(testCase)?.assetVersionIDs;
+    expect(assetVersionIDs).toBeTruthy();
+    const job: IJob | null = await testCreateByType(eJobType, assetVersionIDs, computeJobParameters(testCase, eJobType), null, true);
+    expect(job).toBeTruthy();
+    if (!job)
+        return false;
+
+    const res: H.IOResults = await job.startJob(new Date());
+    expect(res.success).toBeTruthy();
+    return res.success;
+}
+
+function computeJobParameters(testCase: string, eJobType: eVocabularyID): any {
+    let modelName: string | undefined = MTS?.getTestCase(testCase)?.modelName;
+    expect(modelName).toBeTruthy();
+    modelName = modelName || '';
+
+    switch (eJobType) {
+        case eVocabularyID.eJobJobTypeCookSIPackratInspect: return new COOK.JobCookSIPackratInspectParameters(modelName);
+        default:
+            LOG.logger.error(`JobsNS.test computeJobParameters: unexpected job type ${eVocabularyID[eJobType]}`);
+            expect(false).toBeTruthy();
+    }
 }
