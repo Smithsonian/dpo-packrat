7f8f089c8431e5f8b5aab1afee2e4a71b6c273da
Hsin tung <hsintung@Hsins-MacBook-Pro-2.local>
2021-03-23 22:37:15 -0700

    delete excess file. nice catch Jon!

--- conf/scripts/devbox/network 2.sh
+++
@@ -1,8 +0,0 @@
-# Creates a docker network and connect devbox and db
-
-docker network create packrat-devbox-network
-docker network connect packrat-devbox-network packrat-devbox
-docker network connect packrat-devbox-network packrat-devbox-db
-docker network connect packrat-devbox-network packrat-devbox-solr
-
-echo "Done"
