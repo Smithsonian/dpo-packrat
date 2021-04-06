660ce059c28b2172bc6cb0a1a21762bc0d59b9d9
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-04-05 23:10:20 -0700

    Cook: * Implemented JobCookSIPackratInspectOutput.persist() to write cook-extracted information to the database * Added test logic for said code
    GraphQL:
    * Added other fields of interest to getModelConstellation query template ... though I'm not really sure what this is for!
    
    Test:
    * Added asset versions (instead of just IDs) to ModelTestCase
    * Test persistence of JobCookSIPackratInspectOutput

--- server/graphql/api/queries/model/getModelConstellation.ts
+++ server/graphql/api/queries/model/getModelConstellation.ts
@@ -6,22 +6,102 @@ const getModelConstellation = gql`
             ModelConstellation {
                 Model {
                     idModel
+                    Name
+                    DateCreated
+                    Master
+                    Authoritative
+                    VCreationMethod {
+                        Term
+                    }
+                    VModality {
+                        Term
+                    }
+                    VPurpose {
+                        Term
+                    }
+                    VUnits {
+                        Term
+                    }
+                    VFileType {
+                        Term
+                    }
+                    idAssetThumbnail
+                    CountAnimations
+                    CountCameras
+                    CountFaces
+                    CountLights
+                    CountMaterials
+                    CountMeshes
+                    CountVertices
+                    CountEmbeddedTextures
+                    CountLinkedTextures
+                    FileEncoding
                 }
                 ModelObjects {
                     idModelObject
+                    idModel
+                    BoundingBoxP1X
+                    BoundingBoxP1Y
+                    BoundingBoxP1Z
+                    BoundingBoxP2X
+                    BoundingBoxP2Y
+                    BoundingBoxP2Z
+                    CountPoint
+                    CountFace
+                    CountColorChannel
+                    CountTextureCoorinateChannel
+                    HasBones
+                    HasFaceNormals
+                    HasTangents
+                    HasTextureCoordinates
+                    HasVertexNormals
+                    HasVertexColor
+                    IsTwoManifoldUnbounded
+                    IsTwoManifoldBounded
+                    IsWatertight
+                    SelfIntersecting
                 }
                 ModelMaterials {
                     idModelMaterial
+                    Name
                 }
                 ModelMaterialChannels {
                     idModelMaterialChannel
+                    idModelMaterial
+                    Type
+                    Source
+                    Value
+                    VMaterialType {
+                        Term
+                    }
+                    MaterialTypeOther
+                    idModelMaterialUVMap
+                    ChannelPosition
+                    ChannelWidth
+                    Scalar1
+                    Scalar2
+                    Scalar3
+                    Scalar4
+                    AdditionalAttributes
                 }
                 ModelMaterialUVMaps {
                     idModelMaterialUVMap
+                    idModel
+                    idAsset
+                    UVMapEdgeLength
+                }
+                ModelObjectModelMaterialXref {
+                    idModelObject
+                    idModelMaterial
                 }
                 ModelAssets {
                     AssetName
                     AssetType
+                    AssetVersion {
+                        idAsset
+                        idAssetVersion
+                        FileName
+                    }
                 }
             }
         }
