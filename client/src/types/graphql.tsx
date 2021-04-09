515b1aa958d70351cb507b5b8647cb774caebe14
Hsin tung <hsintung@Hsins-MacBook-Pro-2.local>
2021-04-08 17:11:53 -0700

    removed all instances of createModel

--- client/src/types/graphql.tsx
+++ client/src/types/graphql.tsx
@@ -310,7 +310,6 @@ export type Mutation = {
   createCaptureData: CreateCaptureDataResult;
   createCaptureDataPhoto: CreateCaptureDataPhotoResult;
   createItem: CreateItemResult;
-  createModel: CreateModelResult;
   createProject: CreateProjectResult;
   createScene: CreateSceneResult;
   createSubject: CreateSubjectResult;
@@ -338,11 +337,6 @@ export type MutationCreateCaptureDataPhotoArgs = {
 
 export type MutationCreateItemArgs = {
   input: CreateItemInput;
-};
-
-
-export type MutationCreateModelArgs = {
-  input: CreateModelInput;
 };
 
 
@@ -934,33 +928,6 @@ export type LicenseAssignment = {
   License?: Maybe<License>;
   SystemObject?: Maybe<SystemObject>;
   UserCreator?: Maybe<User>;
-};
-
-export type CreateModelInput = {
-  Name: Scalars['String'];
-  Authoritative: Scalars['Boolean'];
-  idVCreationMethod: Scalars['Int'];
-  idVModality: Scalars['Int'];
-  idVPurpose: Scalars['Int'];
-  idVUnits: Scalars['Int'];
-  idVFileType: Scalars['Int'];
-  Master: Scalars['Boolean'];
-  idAssetThumbnail?: Maybe<Scalars['Int']>;
-  CountAnimations?: Maybe<Scalars['Int']>;
-  CountCameras?: Maybe<Scalars['Int']>;
-  CountFaces?: Maybe<Scalars['Int']>;
-  CountLights?: Maybe<Scalars['Int']>;
-  CountMaterials?: Maybe<Scalars['Int']>;
-  CountMeshes?: Maybe<Scalars['Int']>;
-  CountVertices?: Maybe<Scalars['Int']>;
-  CountEmbeddedTextures?: Maybe<Scalars['Int']>;
-  CountLinkedTextures?: Maybe<Scalars['Int']>;
-  FileEncoding?: Maybe<Scalars['String']>;
-};
-
-export type CreateModelResult = {
-  __typename?: 'CreateModelResult';
-  Model?: Maybe<Model>;
 };
 
 export type GetModelInput = {
@@ -2303,22 +2270,6 @@ export type IngestDataMutation = (
   ) }
 );
 
-export type CreateModelMutationVariables = Exact<{
-  input: CreateModelInput;
-}>;
-
-
-export type CreateModelMutation = (
-  { __typename?: 'Mutation' }
-  & { createModel: (
-    { __typename?: 'CreateModelResult' }
-    & { Model?: Maybe<(
-      { __typename?: 'Model' }
-      & Pick<Model, 'idModel'>
-    )> }
-  ) }
-);
-
 export type CreateSceneMutationVariables = Exact<{
   input: CreateSceneInput;
 }>;
@@ -3493,41 +3444,6 @@ export function useIngestDataMutation(baseOptions?: Apollo.MutationHookOptions<I
 export type IngestDataMutationHookResult = ReturnType<typeof useIngestDataMutation>;
 export type IngestDataMutationResult = Apollo.MutationResult<IngestDataMutation>;
 export type IngestDataMutationOptions = Apollo.BaseMutationOptions<IngestDataMutation, IngestDataMutationVariables>;
-export const CreateModelDocument = gql`
-    mutation createModel($input: CreateModelInput!) {
-  createModel(input: $input) {
-    Model {
-      idModel
-    }
-  }
-}
-    `;
-export type CreateModelMutationFn = Apollo.MutationFunction<CreateModelMutation, CreateModelMutationVariables>;
-
-/**
- * __useCreateModelMutation__
- *
- * To run a mutation, you first call `useCreateModelMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useCreateModelMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [createModelMutation, { data, loading, error }] = useCreateModelMutation({
- *   variables: {
- *      input: // value for 'input'
- *   },
- * });
- */
-export function useCreateModelMutation(baseOptions?: Apollo.MutationHookOptions<CreateModelMutation, CreateModelMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<CreateModelMutation, CreateModelMutationVariables>(CreateModelDocument, options);
-      }
-export type CreateModelMutationHookResult = ReturnType<typeof useCreateModelMutation>;
-export type CreateModelMutationResult = Apollo.MutationResult<CreateModelMutation>;
-export type CreateModelMutationOptions = Apollo.BaseMutationOptions<CreateModelMutation, CreateModelMutationVariables>;
 export const CreateSceneDocument = gql`
     mutation createScene($input: CreateSceneInput!) {
   createScene(input: $input) {
--- server/types/graphql.ts
+++ server/types/graphql.ts
@@ -307,7 +307,6 @@ export type Mutation = {
   createCaptureData: CreateCaptureDataResult;
   createCaptureDataPhoto: CreateCaptureDataPhotoResult;
   createItem: CreateItemResult;
-  createModel: CreateModelResult;
   createProject: CreateProjectResult;
   createScene: CreateSceneResult;
   createSubject: CreateSubjectResult;
@@ -335,11 +334,6 @@ export type MutationCreateCaptureDataPhotoArgs = {
 
 export type MutationCreateItemArgs = {
   input: CreateItemInput;
-};
-
-
-export type MutationCreateModelArgs = {
-  input: CreateModelInput;
 };
 
 
@@ -931,33 +925,6 @@ export type LicenseAssignment = {
   License?: Maybe<License>;
   SystemObject?: Maybe<SystemObject>;
   UserCreator?: Maybe<User>;
-};
-
-export type CreateModelInput = {
-  Name: Scalars['String'];
-  Authoritative: Scalars['Boolean'];
-  idVCreationMethod: Scalars['Int'];
-  idVModality: Scalars['Int'];
-  idVPurpose: Scalars['Int'];
-  idVUnits: Scalars['Int'];
-  idVFileType: Scalars['Int'];
-  Master: Scalars['Boolean'];
-  idAssetThumbnail?: Maybe<Scalars['Int']>;
-  CountAnimations?: Maybe<Scalars['Int']>;
-  CountCameras?: Maybe<Scalars['Int']>;
-  CountFaces?: Maybe<Scalars['Int']>;
-  CountLights?: Maybe<Scalars['Int']>;
-  CountMaterials?: Maybe<Scalars['Int']>;
-  CountMeshes?: Maybe<Scalars['Int']>;
-  CountVertices?: Maybe<Scalars['Int']>;
-  CountEmbeddedTextures?: Maybe<Scalars['Int']>;
-  CountLinkedTextures?: Maybe<Scalars['Int']>;
-  FileEncoding?: Maybe<Scalars['String']>;
-};
-
-export type CreateModelResult = {
-  __typename?: 'CreateModelResult';
-  Model?: Maybe<Model>;
 };
 
 export type GetModelInput = {
