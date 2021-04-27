e87fda1029c3cd774b26a0aa13e5fea080e2426c
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-04-27 01:48:41 -0700

    Client: * Attempted, better handling of "Created From" and "Created To" date handlers * Only update filter values if the specified date is a legitimate date * Attempt to handle parsing and generating URLs with dates -- detect a date, handle is specially, and remove the default handling from query_string's method output
    Solr:
    * Continued work on date filter queries
    * Adding logging of ObjectGraph indexing
    * Added exception handling to HTTP route, fullIndex() entrypoint

--- server/http/routes/solrindex.ts
+++ server/http/routes/solrindex.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
 import { IndexSolr } from '../../navigation/impl/NavigationSolr/IndexSolr';
+import * as LOG from '../../utils/logger';
 
 export async function solrindex(_: Request, response: Response): Promise<void> {
     const indexer: IndexSolr = new IndexSolr();
-    const success: boolean = await indexer.fullIndex();
-    response.send(`Solr Indexing Completed: ${success ? 'Success' : 'Failure'}`);
+    try {
+        const success: boolean = await indexer.fullIndex();
+        response.send(`Solr Indexing Completed: ${success ? 'Success' : 'Failure'}`);
+    } catch (error) {
+        LOG.error('solrindex', LOG.LS.eHTTP, error);
+    }
 }
 
 export async function solrindexprofiled(_: Request, response: Response): Promise<void> {
