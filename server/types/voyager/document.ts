f0b3451ff33e621a71f6d39f1574e1670e108d1e
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-04-29 15:23:10 -0700

    Utils: * Implementation of SvxReader, powered by code taken from the Voyager source * Addition of test code and mock files for SvxReader

---
+++ server/types/voyager/document.ts
@@ -0,0 +1,161 @@
+/* eslint-disable quotes, @typescript-eslint/brace-style */
+/**
+ * NOTE: this file is part of the definition of a Voyager scene, found in a .svx.json file.
+ * This was imported from Voyager's source/client/schema on 4/29/2021. It was then modified,
+ * minimally, to allow for use by Packrat. Ideally, in the future, we will extract out the
+ * definition of this shared file format for use by both projects.
+ */
+
+/**
+ * 3D Foundation Project
+ * Copyright 2019 Smithsonian Institution
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// import { Index } from "@ff/core/types";
+type Index = number;
+
+// import { EUnitType, TUnitType, Vector3, Quaternion, Matrix4, ColorRGB } from "./common";
+import { TUnitType, Vector3, Quaternion, Matrix4, ColorRGB } from "./common";
+import { IMeta } from "./meta";
+import { IModel } from "./model";
+import { ISetup } from "./setup";
+
+////////////////////////////////////////////////////////////////////////////////
+
+// export { EUnitType, TUnitType, Vector3, Quaternion, Matrix4, ColorRGB };
+
+export type TCameraType = "perspective" | "orthographic";
+export type TLightType = "ambient" | "directional" | "point" | "spot" | "hemisphere";
+
+/**
+ * Encapsulates a node tree representing a renderable scene.
+ */
+export interface IDocument
+{
+    asset: IDocumentAsset;
+    scene?: Index;
+    scenes?: IScene[];
+    nodes?: INode[];
+    cameras?: ICamera[];
+    lights?: ILight[];
+    metas?: IMeta[];
+    models?: IModel[];
+    setups?: ISetup[];
+}
+
+/**
+ * Information about the document, such as its type, version, copyright and generator.
+ */
+export interface IDocumentAsset
+{
+    type: "application/si-dpo-3d.document+json";
+    version: string;
+    copyright?: string;
+    generator?: string;
+}
+
+export interface IScene
+{
+    name?: string;
+    nodes?: Index[];
+    setup?: Index;
+    meta?: Index;
+    units: TUnitType;
+}
+
+/**
+ * Node in scene hierarchy.
+ */
+export interface INode
+{
+    name?: string;
+    children?: Index[];
+
+    matrix?: Matrix4;
+    translation?: Vector3;
+    rotation?: Quaternion;
+    scale?: Vector3;
+
+    camera?: Index;
+    light?: Index;
+    model?: Index;
+    meta?: Index;
+}
+
+/**
+ * Properties shared by all camera types.
+ */
+export interface ICamera
+{
+    type: TCameraType;
+    perspective?: IPerspectiveCameraProps;
+    orthographic?: IOrthographicCameraProps;
+}
+
+/**
+ * Properties for perspective cameras.
+ */
+export interface IPerspectiveCameraProps
+{
+    yfov: number;
+    znear?: number;
+    zfar?: number;
+}
+
+/**
+ * Properties for orthographic cameras.
+ */
+export interface IOrthographicCameraProps
+{
+    ymag: number;
+    znear?: number;
+    zfar?: number;
+}
+
+/**
+ * Properties shared by all light types.
+ */
+export interface ILight
+{
+    type: TLightType;
+    color?: ColorRGB;
+    intensity?: number;
+
+    shadowEnabled?: boolean;
+    shadowSize?: number;
+    shadowResolution?: string;
+    shadowBlur?: number;
+
+    point?: IPointLightProps;
+    spot?: ISpotLightProps;
+}
+
+/**
+ * Properties for point lights.
+ */
+export interface IPointLightProps
+{
+    distance: number;
+    decay: number;
+}
+
+/**
+ * Properties for spot lights.
+ */
+export interface ISpotLightProps extends IPointLightProps
+{
+    angle: number;
+    penumbra: number;
+}
