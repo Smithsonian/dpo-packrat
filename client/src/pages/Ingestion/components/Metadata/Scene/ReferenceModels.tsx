27d6d043de7657557adcdb5524c83f02c3418530
Hsin tung <hsintung@Hsins-MacBook-Pro-2.local>
2021-05-02 16:25:34 -0700

    WIP scene ingestion data grid

--- client/src/pages/Ingestion/components/Metadata/Scene/ReferenceModels.tsx
+++ client/src/pages/Ingestion/components/Metadata/Scene/ReferenceModels.tsx
@@ -8,7 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import React from 'react';
 import { NewTabLink } from '../../../../../components';
-import { ReferenceModelAction, StateReferenceModel } from '../../../../../store';
+import { ReferenceModelAction /*, StateReferenceModel*/ } from '../../../../../store';
 import { getDetailsUrlForObject } from '../../../../../utils/repository';
 import { formatBytes } from '../../../../../utils/upload';
 
@@ -40,6 +40,9 @@ const useStyles = makeStyles(({ palette }) => ({
         textDecoration: 'underline',
         cursor: 'pointer'
     },
+    labelItalics: {
+        fontStyle: 'italic'
+    },
     empty: {
         textAlign: 'center',
         margin: '15px 0px',
@@ -48,11 +51,13 @@ const useStyles = makeStyles(({ palette }) => ({
     }
 }));
 
-const mockReferenceModels: StateReferenceModel[] = [
+const mockReferenceModels = [
     {
         idSystemObject: 1,
         name: 'Armstrong1.obj (mock)',
-        fileSize: 1.27e+7,
+        usage: 'Web3D',
+        quality: 'Thumb',
+        fileSize: 1.27e7,
         resolution: 2000,
         boundingBoxP1X: 1.0,
         boundingBoxP1Y: 1.0,
@@ -60,12 +65,15 @@ const mockReferenceModels: StateReferenceModel[] = [
         boundingBoxP2X: 10.0,
         boundingBoxP2Y: 10.0,
         boundingBoxP2Z: 10.0,
-        action: ReferenceModelAction.Update
+        action: ReferenceModelAction.Update,
+        existingModel: true
     },
     {
         idSystemObject: 1,
         name: 'Armstrong2.obj (mock)',
-        fileSize: 1.27e+7,
+        usage: 'Web3D',
+        quality: 'High',
+        fileSize: 1.27e7,
         resolution: 2000,
         boundingBoxP1X: 1.0,
         boundingBoxP1Y: 1.0,
@@ -73,7 +81,8 @@ const mockReferenceModels: StateReferenceModel[] = [
         boundingBoxP2X: 10.0,
         boundingBoxP2Y: 10.0,
         boundingBoxP2Z: 10.0,
-        action: ReferenceModelAction.Update
+        action: ReferenceModelAction.Update,
+        existingModel: false
     }
 ];
 
@@ -87,7 +96,9 @@ function ReferenceModels(): React.ReactElement {
             {!hasModels && <Empty />}
             {hasModels && (
                 <Box className={classes.list}>
-                    {mockReferenceModels.map((referenceModel: StateReferenceModel, index: number) => <Item key={index} referenceModel={referenceModel} />)}
+                    {mockReferenceModels.map((referenceModel, index: number) => (
+                        <Item key={index} referenceModel={referenceModel} />
+                    ))}
                 </Box>
             )}
         </Box>
@@ -98,18 +109,18 @@ function Header(): React.ReactElement {
     const classes = useStyles();
 
     return (
-        <Box
-            display='flex'
-            flex={1}
-            flexDirection='row'
-            px={1}
-            marginBottom={1}
-        >
-            <Box display='flex' flex={2}>
+        <Box display='flex' flex={1} flexDirection='row' px={1} marginBottom={1}>
+            <Box display='flex' flex={3}>
                 <Typography className={classes.header}>Reference Models(s)</Typography>
             </Box>
             <Box display='flex' flex={1}>
-                <Typography className={classes.header}>Geometry File Size</Typography>
+                <Typography className={classes.header}>Usage</Typography>
+            </Box>
+            <Box display='flex' flex={1}>
+                <Typography className={classes.header}>Quality</Typography>
+            </Box>
+            <Box display='flex' flex={1}>
+                <Typography className={classes.header}>File Size</Typography>
             </Box>
             <Box display='flex' flex={1} justifyContent='center'>
                 <Typography className={classes.header}>UV Resolution</Typography>
@@ -124,13 +135,8 @@ function Header(): React.ReactElement {
     );
 }
 
-interface ItemProps {
-    referenceModel: StateReferenceModel;
-}
-
-function Item(props: ItemProps): React.ReactElement {
-    const { referenceModel } = props;
-    const { idSystemObject, name, fileSize, resolution, action } = referenceModel;
+function Item({ referenceModel }): React.ReactElement {
+    const { idSystemObject, name, fileSize, resolution, action, quality, usage, existingModel } = referenceModel;
     const { boundingBoxP1X, boundingBoxP1Y, boundingBoxP1Z, boundingBoxP2X, boundingBoxP2Y, boundingBoxP2Z } = referenceModel;
     const classes = useStyles();
 
@@ -139,20 +145,25 @@ function Item(props: ItemProps): React.ReactElement {
     };
 
     const boundingBox: string = `(${boundingBoxP1X}, ${boundingBoxP1Y}, ${boundingBoxP1Z}) - (${boundingBoxP2X}, ${boundingBoxP2Y}, ${boundingBoxP2Z})`;
-
     return (
-        <Box
-            display='flex'
-            flex={1}
-            flexDirection='row'
-            px={1}
-            marginBottom={1}
-        >
-            <Box display='flex' flex={2}>
-                <NewTabLink to={getDetailsUrlForObject(idSystemObject)}>
-                    <Typography className={clsx(classes.label, classes.labelUnderline)}>{name}</Typography>
-                </NewTabLink>
+        <Box display='flex' flex={1} flexDirection='row' px={1} marginBottom={1}>
+            <Box display='flex' flex={2.5}>
+                {existingModel ? (
+                    <NewTabLink to={getDetailsUrlForObject(idSystemObject)}>
+                        <Typography className={clsx(classes.label, classes.labelUnderline)}>{name}</Typography>
+                    </NewTabLink>
+                ) : (
+                    <Typography className={clsx(classes.label, classes.labelUnderline, classes.labelItalics)}>{name}</Typography>
+                )}
             </Box>
+
+            <Box display='flex' flex={1} justifyContent='center'>
+                <Typography className={classes.label}>{usage}</Typography>
+            </Box>
+            <Box display='flex' flex={1} justifyContent='center'>
+                <Typography className={classes.label}>{quality}</Typography>
+            </Box>
+
             <Box display='flex' flex={1} justifyContent='center'>
                 <Typography className={classes.label}>{formatBytes(fileSize)}</Typography>
             </Box>
@@ -163,7 +174,9 @@ function Item(props: ItemProps): React.ReactElement {
                 <Typography className={classes.label}>{boundingBox}</Typography>
             </Box>
             <Box display='flex' flex={0.5} justifyContent='center'>
-                <Typography onClick={onAction} className={clsx(classes.label, classes.labelUnderline)}>{action.toString()}</Typography>
+                <Typography onClick={onAction} className={clsx(classes.label, classes.labelUnderline)}>
+                    {action.toString()}
+                </Typography>
             </Box>
         </Box>
     );
