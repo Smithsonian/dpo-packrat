cf5dfced9a838b26f15fa0d99ce76e8978cd6fee
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-05-22 21:29:54 -0700

    WIP

--- server/job/impl/Cook/JobCookSIGenerateDownloads.ts
+++ server/job/impl/Cook/JobCookSIGenerateDownloads.ts
@@ -9,47 +9,39 @@ import * as DBAPI from '../../../db';
 import * as CACHE from '../../../cache';
 import * as STORE from '../../../storage/interface';
 import * as H from '../../../utils/helpers';
-import { SvxReader } from '../../../utils/parser';
 import { ASL, LocalStore } from '../../../utils/localStore';
 
 import * as path from 'path';
-// import cloneable from 'cloneable-readable';
-// import { Readable } from 'stream';
 
-export class JobCookSIVoyagerSceneParameters {
+export class JobCookSIGenerateDownloadsParameters {
     constructor(idModel: number | undefined,
         sourceMeshFile: string,
-        units: string,
+        svxFile: string,
         sourceDiffuseMapFile: string | undefined = undefined,
-        svxFile: string | undefined = undefined,
-        metaDataFile: string | undefined = undefined,
         outputFileBaseName: string | undefined = undefined) {
         this.idModel = idModel;
         this.sourceMeshFile = path.basename(sourceMeshFile);
-        this.units = units;
+        this.svxFile = path.basename(svxFile);
         this.sourceDiffuseMapFile = sourceDiffuseMapFile ? path.basename(sourceDiffuseMapFile) : undefined;
-        this.svxFile = svxFile ? path.basename(svxFile) : undefined;
-        this.metaDataFile = metaDataFile ? path.basename(metaDataFile) : undefined;
         this.outputFileBaseName = outputFileBaseName ? path.basename(outputFileBaseName) : undefined;
     }
     idModel: number | undefined;
-    sourceMeshFile: string;
-    units: string;
+    sourceMeshFile: string;             // required
+    svxFile: string;                    // required
     sourceDiffuseMapFile?: string | undefined;
-    svxFile?: string | undefined;
-    metaDataFile?: string | undefined;
+    sourceMTLFile?: string | undefined;
     outputFileBaseName?: string | undefined;
 }
 
-export class JobCookSIVoyagerScene extends JobCook<JobCookSIVoyagerSceneParameters> {
-    private parameters: JobCookSIVoyagerSceneParameters;
+export class JobCookSIGenerateDownloads extends JobCook<JobCookSIGenerateDownloadsParameters> {
+    private parameters: JobCookSIGenerateDownloadsParameters;
     private idModel: number | null;
     private cleanupCalled: boolean = false;
 
     constructor(jobEngine: JOB.IJobEngine, idAssetVersions: number[] | null,
-        parameters: JobCookSIVoyagerSceneParameters, dbJobRun: DBAPI.JobRun) {
-        super(jobEngine, Config.job.cookClientId, 'si-vogager-scene',
-            CookRecipe.getCookRecipeID('si-vogager-scene', '512211e5-f2e8-4723-93e9-e30116c88ab0'),
+        parameters: JobCookSIGenerateDownloadsParameters, dbJobRun: DBAPI.JobRun) {
+        super(jobEngine, Config.job.cookClientId, 'si-generate-downloads',
+            CookRecipe.getCookRecipeID('si-generate-downloads', 'fcef7b5c-2df5-4a63-8fe9-365dd1a5e39c'),
             null, idAssetVersions, dbJobRun);
         if (parameters.idModel) {
             this.idModel = parameters.idModel ?? null;
@@ -68,7 +60,7 @@ export class JobCookSIVoyagerScene extends JobCook<JobCookSIVoyagerSceneParamete
             this.cleanupCalled = true;
             return await this.createSystemObjects();
         } catch (error) {
-            LOG.error('JobCookSIVoyagerScene.cleanupJob', LOG.LS.eJOB, error);
+            LOG.error('JobCookSIGenerateDownloads.cleanupJob', LOG.LS.eJOB, error);
             return { success: false, error: JSON.stringify(error) };
         }
     }
@@ -76,67 +68,18 @@ export class JobCookSIVoyagerScene extends JobCook<JobCookSIVoyagerSceneParamete
     private async createSystemObjects(): Promise<H.IOResults> {
         const modelSource: DBAPI.Model | null = this.idModel ? await DBAPI.Model.fetch(this.idModel) : null;
         if (!modelSource) {
-            const error: string = `JobCookSIVoyagerScene.createSystemObjects unable to compute source model from id ${this.idModel}`;
+            const error: string = `JobCookSIGenerateDownloads.createSystemObjects unable to compute source model from id ${this.idModel}`;
             LOG.error(error, LOG.LS.eJOB);
             return { success: false, error };
         }
 
-        const svxFile: string = this.parameters.svxFile ?? 'scene.svx.json';
-        const vScene: DBAPI.Vocabulary | undefined = await CACHE.VocabularyCache.vocabularyByEnum(CACHE.eVocabularyID.eAssetAssetTypeScene);
+        const svxFile: string = this.parameters.svxFile;
         const vModel: DBAPI.Vocabulary | undefined = await CACHE.VocabularyCache.vocabularyByEnum(CACHE.eVocabularyID.eAssetAssetTypeModelGeometryFile);
-        if (!vScene || !vModel) {
-            const error: string = `JobCookSIVoyagerScene.createSystemObjects unable to calculate vocabulary needed to ingest scene file ${svxFile}`;
+        if (!vModel) {
+            const error: string = `JobCookSIGenerateDownloads.createSystemObjects unable to calculate vocabulary needed to ingest scene file ${svxFile}`;
             LOG.error(error, LOG.LS.eJOB);
             return { success: false, error };
         }
-
-        // Retrieve svx.json
-        let RSR: STORE.ReadStreamResult = await this.fetchFile(svxFile);
-        if (!RSR.success || !RSR.readStream) {
-            LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to fetch stream for scene file ${svxFile}: ${RSR.error}`, LOG.LS.eJOB);
-            return { success: false, error: RSR.error };
-        }
-        LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] retrieve svx.json`, LOG.LS.eJOB);
-
-        // // create cloneable stream wrapper, so that we can load the scene into memory for processing, and also stream it out to storage as an ingested asset
-        // const clone = cloneable(new Readable().wrap(RSR.readStream));
-        // LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] create cloneable stream wrapper`, LOG.LS.eJOB);
-        // const cloneStream = clone.clone();
-
-        // Parse Scene
-        const svx: SvxReader = new SvxReader();
-        const res: H.IOResults = await svx.loadFromStream(RSR.readStream);
-        if (!res.success || !svx.SvxExtraction) {
-            LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to parse scene file ${svxFile}: ${res.error}`, LOG.LS.eJOB);
-            return { success: false, error: res.error };
-        }
-        LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] parse scene`, LOG.LS.eJOB);
-        // LOG.info(`JobCookSIVoyagerScene.createSystemObjects fetched scene:\n${JSON.stringify(svx.SvxExtraction, H.Helpers.stringifyMapsAndBigints)}`, LOG.LS.eJOB);
-
-        // Create Scene
-        const scene: DBAPI.Scene = svx.SvxExtraction.extractScene();
-        if (!await scene.create()) {
-            LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to create Scene file ${svxFile}: database error`, LOG.LS.eJOB);
-            return { success: false, error: res.error };
-        }
-        LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] create scene`, LOG.LS.eJOB);
-
-        // wire ModelSource to Scene
-        const SOX: DBAPI.SystemObjectXref | null = await DBAPI.SystemObjectXref.wireObjectsIfNeeded(modelSource, scene);
-        if (!SOX) {
-            LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to wire Model Source ${JSON.stringify(modelSource, H.Helpers.stringifyMapsAndBigints)} to Scene ${JSON.stringify(scene, H.Helpers.stringifyMapsAndBigints)}: database error`, LOG.LS.eJOB);
-            return { success: false, error: res.error };
-        }
-        // LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] wire ModelSource to Scene: ${JSON.stringify(SOX, H.Helpers.stringifyMapsAndBigints)}`, LOG.LS.eJOB);
-
-        // Scene owns this ingested asset of the SVX File
-        // Read file a second time ... cloneStream isn't available
-        RSR = await this.fetchFile(svxFile);
-        if (!RSR.success || !RSR.readStream) {
-            LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to fetch stream for scene file ${svxFile}: ${RSR.error}`, LOG.LS.eJOB);
-            return { success: false, error: RSR.error };
-        }
-        // LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] retrieve svx.json 2`, LOG.LS.eJOB);
 
         const LS: LocalStore | undefined = ASL.getStore();
         const idUserCreator: number = LS?.idUser ?? 0;
@@ -146,128 +89,22 @@ export class JobCookSIVoyagerScene extends JobCook<JobCookSIVoyagerSceneParamete
             FileName: svxFile,
             FilePath: '',
             idAssetGroup: 0,
-            idVAssetType: vScene.idVocabulary,
+            idVAssetType: vModel.idVocabulary,
             idUserCreator,
             SOBased: scene,
         };
         let ISR: STORE.IngestStreamOrFileResult = await STORE.AssetStorageAdapter.ingestStreamOrFile(ISI);
         if (!ISR.success) {
-            LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to ingest scene file ${svxFile}: ${ISR.error}`, LOG.LS.eJOB);
+            LOG.error(`JobCookSIGenerateDownloads.createSystemObjects unable to ingest scene file ${svxFile}: ${ISR.error}`, LOG.LS.eJOB);
             return { success: false, error: ISR.error };
         }
-        // LOG.info(`JobCookSIVoyagerScene.createSystemObjects[${svxFile}] wire ingestStreamOrFile: ${JSON.stringify(ISI, H.Helpers.stringifyMapsAndBigints)}`, LOG.LS.eJOB);
-
-        // Now extract (just) the models; per Jamie Cope 5/3/2021, each model has all textures embedded
-        // How do we create the Model objects here? We can ingest the assets, as above, but we need to connect them to the right objects
-        if (svx.SvxExtraction.modelDetails) {
-            for (const MSX of svx.SvxExtraction.modelDetails) {
-                if (MSX.Name) {
-                    const model: DBAPI.Model = await this.transformModelSceneXrefIntoModel(MSX, modelSource);
-                    if (!await model.create()) {
-                        const error: string = `JobCookSIVoyagerScene.createSystemObjects unable to create Model from referenced model ${MSX.Name}: database error`;
-                        LOG.error(error, LOG.LS.eJOB);
-                        return { success: false, error };
-                    }
-
-                    // wire ModelSource to Model
-                    const SOX1: DBAPI.SystemObjectXref | null = await DBAPI.SystemObjectXref.wireObjectsIfNeeded(modelSource, model);
-                    if (!SOX1) {
-                        LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to wire Model Source ${JSON.stringify(modelSource, H.Helpers.stringifyMapsAndBigints)} to Model ${JSON.stringify(model, H.Helpers.stringifyMapsAndBigints)}: database error`, LOG.LS.eJOB);
-                        return { success: false, error: res.error };
-                    }
-
-                    // Create ModelSceneXref for new model and parent scene
-                    /* istanbul ignore else */
-                    if (!MSX.idModelSceneXref) { // should always be true
-                        MSX.idModel = model.idModel;
-                        MSX.idScene = scene.idScene;
-                        if (!await MSX.create()) {
-                            const error: string = `JobCookSIVoyagerScene.createSystemObjects unable to create ModelSceneXref for model xref ${JSON.stringify(MSX)}: database error`;
-                            LOG.error(error, LOG.LS.eJOB);
-                            return { success: false, error };
-                        }
-                    } else
-                        LOG.error(`JobCookSIVoyagerScene.createSystemObjects unexpected non-null ModelSceneXref for model xref ${JSON.stringify(MSX)}: database error`, LOG.LS.eJOB);
-
-                    const SOX2: DBAPI.SystemObjectXref | null = await DBAPI.SystemObjectXref.wireObjectsIfNeeded(scene, model);
-                    if (!SOX2) {
-                        const error: string = `JobCookSIVoyagerScene.createSystemObjects unable to wire Scene ${JSON.stringify(scene, H.Helpers.stringifyMapsAndBigints)} and Model ${JSON.stringify(model, H.Helpers.stringifyMapsAndBigints)} together: database error`;
-                        LOG.error(error, LOG.LS.eJOB);
-                        return { success: false, error };
-                    }
-
-                    const RSRModel: STORE.ReadStreamResult = await this.fetchFile(MSX.Name);
-                    if (!RSRModel.success || !RSRModel.readStream) {
-                        LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to fetch stream for model file ${MSX.Name}: ${RSRModel.error}`, LOG.LS.eJOB);
-                        return { success: false, error: RSRModel.error };
-                    }
-
-                    const FileName: string = path.basename(MSX.Name);
-                    const FilePath: string = path.dirname(MSX.Name);
-                    const ISIModel: STORE.IngestStreamOrFileInput = {
-                        ReadStream: RSRModel.readStream,
-                        LocalFilePath: null,
-                        FileName,
-                        FilePath,
-                        idAssetGroup: 0,
-                        idVAssetType: vModel.idVocabulary,
-                        idUserCreator,
-                        SOBased: model,
-                    };
-                    ISR = await STORE.AssetStorageAdapter.ingestStreamOrFile(ISIModel);
-                    if (!ISR.success) {
-                        LOG.error(`JobCookSIVoyagerScene.createSystemObjects unable to ingest model file ${MSX.Name}: ${ISR.error}`, LOG.LS.eJOB);
-                        return { success: false, error: ISR.error };
-                    }
-                } else
-                    LOG.error(`JobCookSIVoyagerScene.createSystemObjects skipping unnamed model ${JSON.stringify(MSX)}`, LOG.LS.eJOB);
-            }
-        }
+        // LOG.info(`JobCookSIGenerateDownloads.createSystemObjects[${svxFile}] wire ingestStreamOrFile: ${JSON.stringify(ISI, H.Helpers.stringifyMapsAndBigints)}`, LOG.LS.eJOB);
 
         return { success: true, error: '' };
     }
 
-    protected async getParameters(): Promise<JobCookSIVoyagerSceneParameters> {
+    protected async getParameters(): Promise<JobCookSIGenerateDownloadsParameters> {
         return this.parameters;
-    }
-
-    protected async transformModelSceneXrefIntoModel(MSX: DBAPI.ModelSceneXref, source?: DBAPI.Model | undefined): Promise<DBAPI.Model> {
-        const Name: string = MSX.Name ?? '';
-        const vFileType: DBAPI.Vocabulary | undefined = await CACHE.VocabularyCache.mapModelFileByExtension(Name);
-        return new DBAPI.Model({
-            idModel: 0,
-            Name,
-            DateCreated: new Date(),
-            Authoritative: false,
-            idVCreationMethod: source?.idVCreationMethod ?? 0,
-            idVModality: source?.idVModality ?? 0,
-            idVPurpose: source?.idVPurpose ?? 0,
-            idVUnits: source?.idVUnits ?? 0,
-            idVFileType: vFileType ? vFileType.idVocabulary : 0,
-            idAssetThumbnail: null, CountAnimations: null, CountCameras: null, CountFaces: null, CountLights: null,CountMaterials: null,
-            CountMeshes: null, CountVertices: null, CountEmbeddedTextures: null, CountLinkedTextures: null, FileEncoding: null
-        });
-    }
-
-    static async convertModelUnitsVocabToCookUnits(idVUnits: number): Promise<string | undefined> {
-        // acceptable units for Cook's si-voyager-scene, as of 5/3/2021:  "mm", "cm", "m", "in", "ft", "yd"
-        const eModelUnits: CACHE.eVocabularyID | undefined = await CACHE.VocabularyCache.vocabularyIdToEnum(idVUnits);
-        switch (eModelUnits) {
-            case CACHE.eVocabularyID.eModelUnitsMillimeter: return 'mm';
-            case CACHE.eVocabularyID.eModelUnitsCentimeter: return 'cm';
-            case CACHE.eVocabularyID.eModelUnitsMeter: return 'm';
-            case CACHE.eVocabularyID.eModelUnitsInch: return 'in';
-            case CACHE.eVocabularyID.eModelUnitsFoot: return 'ft';
-            case CACHE.eVocabularyID.eModelUnitsYard: return 'yd';
-
-            // not supported by Cook as of 5/3/2021:
-            case CACHE.eVocabularyID.eModelUnitsMicrometer:
-            case CACHE.eVocabularyID.eModelUnitsKilometer:
-            case CACHE.eVocabularyID.eModelUnitsMile:
-            case CACHE.eVocabularyID.eModelUnitsAstronomicalUnit:
-            default:
-                return undefined;
-        }
     }
 }
 
