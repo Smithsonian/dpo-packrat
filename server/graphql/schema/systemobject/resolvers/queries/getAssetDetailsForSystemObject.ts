f3a2c8692f8abd367c5b18262628eeacb27ba0e2
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-06-03 17:03:18 -0700

    GraphQL: * Sort output from getAssetDetailsForSystemObject, placing the "preferred" asset first in the results.  This preferred asset has an asset type that relates to the owning object. allowing the client to detect the asset type needed for adding a new "version" of a complex object, like a "Capture Data" set, Model, or Scene.

--- server/graphql/schema/systemobject/resolvers/queries/getAssetDetailsForSystemObject.ts
+++ server/graphql/schema/systemobject/resolvers/queries/getAssetDetailsForSystemObject.ts
@@ -15,6 +15,13 @@ export default async function getAssetDetailsForSystemObject(_: Parent, args: Qu
         return { assetDetails };
     }
 
+    const SO: DBAPI.SystemObject | null = await DBAPI.SystemObject.fetch(idSystemObject);
+    if (!SO) {
+        LOG.info(`getAssetDetailsForSystemObject unable to retrieve system object for ${idSystemObject}`, LOG.LS.eGQL);
+        return { assetDetails };
+    }
+
+    let assetDetailPreferred: AssetDetail | null = null;
     for (const assetVersion of assetVersions) {
         // We need the idSystemObject for the asset
         const oID: DBAPI.ObjectIDAndType = { idObject: assetVersion.idAsset, eObjectType: DBAPI.eSystemObjectType.eAsset };
@@ -42,8 +49,66 @@ export default async function getAssetDetailsForSystemObject(_: Parent, args: Qu
             size: assetVersion.StorageSize
         };
 
+        // If we haven't yet identified a preferred assetDetail record, examine this asset's asset type,
+        // and compare that to the system object that we've fetched.
+        if (!assetDetailPreferred) {
+            switch (await CACHE.VocabularyCache.vocabularyIdToEnum(asset.idVAssetType)) {
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetPhotogrammetry:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetDiconde:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetDicom:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetLaserLine:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetSphericalLaser:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetStructuredLight:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataSetOther:
+                    if (SO.idCaptureData) {
+                        assetDetailPreferred = assetDetail;
+                        continue;
+                    }
+                    break;
+
+                case CACHE.eVocabularyID.eAssetAssetTypeModel:
+                case CACHE.eVocabularyID.eAssetAssetTypeModelGeometryFile:
+                    if (SO.idModel) {
+                        assetDetailPreferred = assetDetail;
+                        continue;
+                    }
+                    break;
+
+                case CACHE.eVocabularyID.eAssetAssetTypeScene:
+                    if (SO.idScene) {
+                        assetDetailPreferred = assetDetail;
+                        continue;
+                    }
+                    break;
+
+                case CACHE.eVocabularyID.eAssetAssetTypeProjectDocumentation:
+                    if (SO.idProjectDocumentation) {
+                        assetDetailPreferred = assetDetail;
+                        continue;
+                    }
+                    break;
+
+                case CACHE.eVocabularyID.eAssetAssetTypeIntermediaryFile:
+                    if (SO.idIntermediaryFile) {
+                        assetDetailPreferred = assetDetail;
+                        continue;
+                    }
+                    break;
+
+                case CACHE.eVocabularyID.eAssetAssetTypeBulkIngestion:
+                case CACHE.eVocabularyID.eAssetAssetTypeCaptureDataFile:
+                case CACHE.eVocabularyID.eAssetAssetTypeModelUVMapFile:
+                case CACHE.eVocabularyID.eAssetAssetTypeOther:
+                case undefined:
+                    break;
+            }
+        }
+
         assetDetails.push(assetDetail);
     }
+
+    if (assetDetailPreferred)
+        assetDetails.unshift(assetDetailPreferred);
 
     return { assetDetails };
 }
