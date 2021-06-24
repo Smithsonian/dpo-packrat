98daf709aef261d6e106e649aff0346bb11a615d
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-06-24 11:18:00 -0700

    HTTP: * Added server route for /download/*, allowing us to download specific assets of system objects by using the route /download/idSystemObject-ID/FOO/BAR
    This will download the asset connected to system object with idSystemObject=ID, where the string '/' +Asset.FilePath + '/' + AssetVersion.FileName matches /FOO/BAR.
    * Improved /download error handling
    * Addressed a browser error that occurred when attempting to download content with commas in the name

--- server/http/routes/download.ts
+++ server/http/routes/download.ts
@@ -7,14 +7,17 @@ import * as STORE from '../../storage/interface';
 import { isAuthenticated } from '../auth';
 
 import { Request, Response } from 'express';
+import { ParsedQs } from 'qs';
 import mime from 'mime'; // const mime = require('mime-types'); // can't seem to make this work using "import * as mime from 'mime'"; subsequent calls to mime.lookup freeze!
 import path from 'path';
 
-/** Used to provide download access to assets. Access with one of the following URL query parameters set:
- * ?idAssetVersion=ID:          Downloads the specified version of the specified asset
- * ?idAsset=ID:                 Downloads the most recent asset version of the specified asset
- * ?idSystemObject=ID:          Computes the assets attached to this system object.  If just one, downloads it alone.  If multiple, computes a zip and downloads that zip.
- * ?idSystemObjectVersion=ID:   Computes the assets attached to this system object version.  If just one, downloads it alone.  If multiple, computes a zip and downloads that zip.
+/** Used to provide download access to assets. Access with one of the following URL query parameters set, or path
+ * /download?idAssetVersion=ID:         Downloads the specified version of the specified asset
+ * /download?idAsset=ID:                Downloads the most recent asset version of the specified asset
+ * /download?idSystemObject=ID:         Computes the assets attached to system object with idSystemObject = ID. If just one, downloads it alone.  If multiple, computes a zip and downloads that zip.
+ * /download/idSystemObject-ID:         Computes the assets attached to system object with idSystemObject = ID. If just one, downloads it alone.  If multiple, computes a zip and downloads that zip.
+ * /download/idSystemObject-ID/FOO/BAR: Computes the asset  attached to system object with idSystemObject = ID, found at the path /FOO/BAR.
+ * /download?idSystemObjectVersion=ID:  Computes the assets attached to system object version with idSystemObjectVersion = ID. If just one, downloads it alone.  If multiple, computes a zip and downloads that zip.
  */
 export async function download(request: Request, response: Response): Promise<boolean> {
     const DL: Downloader = new Downloader(request, response);
@@ -39,6 +42,10 @@ class Downloader {
     private idSystemObject: number | null = null;
     private idSystemObjectVersion: number | null = null;
 
+    private systemObjectPath: string | null = null;         // path of asset (e.g. /FOO/BAR) to be downloaded when accessed via e.g. /download/idSystemObject-ID/FOO/BAR
+
+    private static regexDownload: RegExp = new RegExp('/download/idSystemObject-(\\d*)(/.*)?', 'i');
+
     constructor(request: Request, response: Response) {
         this.request = request;
         this.response = response;
@@ -47,22 +54,18 @@ class Downloader {
     async execute(): Promise<boolean> {
         if (!isAuthenticated(this.request)) {
             LOG.error('/download not authenticated', LOG.LS.eHTTP);
-            this.response.status(403);
-            return false;
+            return this.sendError(403);
         }
 
-        if (!this.parseArguments()) {
-            this.response.status(404);
-            return false;
-        }
+        if (!this.parseArguments())
+            return this.sendError(404);
 
         switch (this.eMode) {
             case eDownloadMode.eAssetVersion: {
                 const assetVersion: DBAPI.AssetVersion | null = await DBAPI.AssetVersion.fetch(this.idAssetVersion!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
                 if (!assetVersion) {
                     LOG.error(`/download?idAssetVersion=${this.idAssetVersion} invalid parameter`, LOG.LS.eHTTP);
-                    this.response.status(404);
-                    return false;
+                    return this.sendError(404);
                 }
                 return this.emitDownload(assetVersion);
             }
@@ -71,8 +74,7 @@ class Downloader {
                 const assetVersion: DBAPI.AssetVersion | null = await DBAPI.AssetVersion.fetchLatestFromAsset(this.idAsset!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
                 if (!assetVersion) {
                     LOG.error(`/download?idAsset=${this.idAsset} unable to fetch asset version`, LOG.LS.eHTTP);
-                    this.response.status(404);
-                    return false;
+                    return this.sendError(404);
                 }
                 return await this.emitDownload(assetVersion);
             }
@@ -80,23 +82,40 @@ class Downloader {
             case eDownloadMode.eSystemObject: {
                 const assetVersions: DBAPI.AssetVersion[] | null = await DBAPI.AssetVersion.fetchLatestFromSystemObject(this.idSystemObject!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
                 if (!assetVersions) {
-                    LOG.error(`/download?idSystemObject=${this.idSystemObject} unable to fetch asset versions`, LOG.LS.eHTTP);
-                    this.response.status(404);
-                    return false;
+                    LOG.error(`${this.reconstructSystemObjectLink()} unable to fetch asset versions`, LOG.LS.eHTTP);
+                    return this.sendError(404);
                 }
                 if (assetVersions.length == 0) {
                     this.response.send(`No Assets are connected to idSystemObject ${this.idSystemObject}`);
                     return true;
                 }
-                return await this.emitDownloadZip(assetVersions);
+
+                // if we don't have a system object path, return the zip of all assets
+                if (!this.systemObjectPath || this.systemObjectPath === '/')
+                    return await this.emitDownloadZip(assetVersions);
+
+                // otherwise, find the specified asset by path
+                const pathToMatch: string = this.systemObjectPath.toLowerCase();
+                for (const assetVersion of assetVersions) {
+                    const asset: DBAPI.Asset | null = await DBAPI.Asset.fetch(assetVersion.idAsset);
+                    if (!asset) {
+                        LOG.error(`${this.reconstructSystemObjectLink()} unable to fetch asset from assetVersion ${JSON.stringify(assetVersion, H.Helpers.saferStringify)}`, LOG.LS.eHTTP);
+                        return this.sendError(404);
+                    }
+                    const pathAssetVersion: string = (`/${asset.FilePath}/${assetVersion.FileName}`).toLowerCase();
+                    if (pathToMatch === pathAssetVersion)
+                        return this.emitDownload(assetVersion);
+                }
+
+                LOG.error(`${this.reconstructSystemObjectLink()} unable to find assetVersion with path ${pathToMatch} from ${JSON.stringify(assetVersions, H.Helpers.saferStringify)}`, LOG.LS.eHTTP);
+                return this.sendError(404);
             }
 
             case eDownloadMode.eSystemObjectVersion: {
                 const assetVersions: DBAPI.AssetVersion[] | null = await DBAPI.AssetVersion.fetchFromSystemObjectVersion(this.idSystemObjectVersion!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
                 if (!assetVersions) {
                     LOG.error(`/download?idSystemObjectVersion=${this.idSystemObjectVersion} unable to fetch asset versions`, LOG.LS.eHTTP);
-                    this.response.status(404);
-                    return false;
+                    return this.sendError(404);
                 }
                 if (assetVersions.length == 0) {
                     this.response.send(`No Assets are connected to idSystemObjectVersion ${this.idSystemObjectVersion}`);
@@ -105,29 +124,38 @@ class Downloader {
                 return await this.emitDownloadZip(assetVersions);
             }
         }
-        this.response.status(404);
-        return false;
+        return this.sendError(404);
     }
 
     /** Returns false if arguments are invalid */
     private parseArguments(): boolean {
-        const idSystemObjectU = this.request.query.idSystemObject;
+        // /download/idSystemObject-ID:         Computes the assets attached to this system object.  If just one, downloads it alone.  If multiple, computes a zip and downloads that zip.
+        // /download/idSystemObject-ID/FOO/BAR: Computes the asset attached to this system object, found at the path /FOO/BAR.
+        let idSystemObjectU: string | string[] | ParsedQs | ParsedQs[] | undefined = undefined;
+
+        const downloadMatch: RegExpMatchArray | null = this.request.path.match(Downloader.regexDownload);
+        if (downloadMatch && downloadMatch.length >= 2) {
+            idSystemObjectU = downloadMatch[1];
+            if (downloadMatch.length >= 3)
+                this.systemObjectPath = downloadMatch[2];
+        }
+
+        if (!idSystemObjectU)
+            idSystemObjectU = this.request.query.idSystemObject;
         const idSystemObjectVersionU = this.request.query.idSystemObjectVersion;
         const idAssetU = this.request.query.idAsset;
         const idAssetVersionU = this.request.query.idAssetVersion;
         const urlParamCount: number = (idSystemObjectU ? 1 : 0) + (idSystemObjectVersionU ? 1 : 0) + (idAssetU ? 1 : 0) + (idAssetVersionU ? 1 : 0);
         if (urlParamCount != 1) {
             LOG.error(`/download called with ${urlParamCount} parameters, expected 1`, LOG.LS.eHTTP);
-            this.response.status(404);
-            return false;
+            return this.sendError(404);
         }
 
         if (idAssetVersionU) {
             this.idAssetVersion = H.Helpers.safeNumber(idAssetVersionU);
             if (!this.idAssetVersion) {
                 LOG.error(`/download?idAssetVersion=${idAssetVersionU}, invalid parameter`, LOG.LS.eHTTP);
-                this.response.status(404);
-                return false;
+                return this.sendError(404);
             }
             this.eMode = eDownloadMode.eAssetVersion;
         }
@@ -136,8 +164,7 @@ class Downloader {
             this.idAsset = H.Helpers.safeNumber(idAssetU);
             if (!this.idAsset) {
                 LOG.error(`/download?idAsset=${idAssetU}, invalid parameter`, LOG.LS.eHTTP);
-                this.response.status(404);
-                return false;
+                return this.sendError(404);
             }
             this.eMode = eDownloadMode.eAsset;
         }
@@ -146,8 +173,7 @@ class Downloader {
             this.idSystemObject = H.Helpers.safeNumber(idSystemObjectU);
             if (!this.idSystemObject) {
                 LOG.error(`/download?idSystemObject=${idSystemObjectU} invalid parameter`, LOG.LS.eHTTP);
-                this.response.status(404);
-                return false;
+                return this.sendError(404);
             }
             this.eMode = eDownloadMode.eSystemObject;
         }
@@ -156,8 +182,7 @@ class Downloader {
             this.idSystemObjectVersion = H.Helpers.safeNumber(idSystemObjectVersionU);
             if (!this.idSystemObjectVersion) {
                 LOG.error(`/download?idSystemObjectVersion=${idSystemObjectVersionU} invalid parameter`, LOG.LS.eHTTP);
-                this.response.status(404);
-                return false;
+                return this.sendError(404);
             }
             this.eMode = eDownloadMode.eSystemObjectVersion;
         }
@@ -169,12 +194,15 @@ class Downloader {
         const res: STORE.ReadStreamResult = await STORE.AssetStorageAdapter.readAssetVersion(assetVersion);
         if (!res.success || !res.readStream) {
             LOG.error(`download idAssetVersion=${idAssetVersion} unable to read from storage: ${res.error}`, LOG.LS.eHTTP);
-            this.response.status(500);
-            return false;
+            return this.sendError(500);
         }
         const fileName: string = assetVersion.FileName;
         const mimeType: string = mime.lookup(fileName) || 'application/octet-stream';
         return this.emitDownloadFromStream(res.readStream, fileName, mimeType);
+    }
+
+    private reconstructSystemObjectLink(): string {
+        return (this.systemObjectPath) ? `/download/idSystemObject-${this.idSystemObject}${this.systemObjectPath}` :`/download?idSystemObject=${this.idSystemObject}`;
     }
 
     private async emitDownloadZip(assetVersions: DBAPI.AssetVersion[]): Promise<boolean> {
@@ -182,7 +210,7 @@ class Downloader {
         let idSystemObject: number = this.idSystemObject ?? 0;
 
         if (idSystemObject)
-            errorMsgBase = `/download?idSystemObject=${this.idSystemObject}`;
+            errorMsgBase = this.reconstructSystemObjectLink();
         else if (this.idSystemObjectVersion) {
             errorMsgBase = `/download?idSystemObjectVersion=${this.idSystemObjectVersion}`;
             const SOV: DBAPI.SystemObjectVersion | null = await DBAPI.SystemObjectVersion.fetch(this.idSystemObjectVersion);
@@ -202,31 +230,27 @@ class Downloader {
             const asset: DBAPI.Asset | null = await DBAPI.Asset.fetch(assetVersion.idAsset);
             if (!asset) {
                 LOG.error(`${errorMsgBase} failed to identify asset by id ${assetVersion.idAsset}`, LOG.LS.eHTTP);
-                this.response.status(500);
-                return false;
+                return this.sendError(500);
             }
 
             const RSR: STORE.ReadStreamResult = await STORE.AssetStorageAdapter.readAsset(asset, assetVersion);
             if (!RSR.success || !RSR.readStream) {
                 LOG.error(`${errorMsgBase} failed to extract stream for asset version ${assetVersion.idAssetVersion}`, LOG.LS.eHTTP);
-                this.response.status(500);
-                return false;
+                return this.sendError(500);
             }
 
             const fileNameAndPath: string = path.join(asset.FilePath, assetVersion.FileName);
             const res: H.IOResults = await zip.add(fileNameAndPath, RSR.readStream);
             if (!res.success) {
                 LOG.error(`${errorMsgBase} failed to add asset version ${assetVersion.idAssetVersion} to zip: ${res.error}`, LOG.LS.eHTTP);
-                this.response.status(500);
-                return false;
+                return this.sendError(500);
             }
         }
 
         const zipStream: NodeJS.ReadableStream | null = await zip.streamContent(null);
         if (!zipStream) {
             LOG.error(`${errorMsgBase} failed to extract stream from zip`, LOG.LS.eHTTP);
-            this.response.status(500);
-            return false;
+            return this.sendError(500);
         }
 
         const fileName: string = (await CACHE.SystemObjectCache.getObjectNameByID(idSystemObject) || `SystemObject${idSystemObject}`) + '.zip';
@@ -234,7 +258,8 @@ class Downloader {
         return this.emitDownloadFromStream(zipStream, fileName, mimeType);
     }
 
-    private async emitDownloadFromStream(readStream: NodeJS.ReadableStream, fileName: string, mimeType: string | undefined): Promise<boolean> {
+    private async emitDownloadFromStream(readStream: NodeJS.ReadableStream, fileNameIn: string, mimeType: string | undefined): Promise<boolean> {
+        const fileName: string = fileNameIn.replace(/,/g, '_'); // replace commas with underscores to avoid ERR_RESPONSE_HEADERS_MULTIPLE_CONTENT_DISPOSITION browser error
         if (!mimeType)
             mimeType = mime.lookup(fileName) || 'application/octet-stream';
         LOG.info(`/download emitDownloadFromStream filename=${fileName}, mimetype=${mimeType}`, LOG.LS.eHTTP);
@@ -244,5 +269,11 @@ class Downloader {
             this.response.setHeader('Content-type', mimeType);
         readStream.pipe(this.response);
         return true;
+    }
+
+    private sendError(statusCode: number, message?: string | undefined): boolean {
+        this.response.status(statusCode);
+        this.response.send(message ?? '');
+        return false;
     }
 }
