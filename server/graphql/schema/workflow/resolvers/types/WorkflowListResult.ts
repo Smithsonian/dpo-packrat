287be978fe87ab0460c0da0e342dcb19ea9aa678
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-07-27 10:48:31 -0700

    DBAPI & GraphQL: * Bug fixes as a result of WorkflowListResult.search() testing * Make WorkflowListResult type have mostly nullable fields * Added WorkflowReport and JobRun resolvable types to WorkflowListResult

--- server/graphql/schema/workflow/resolvers/types/WorkflowListResult.ts
+++ server/graphql/schema/workflow/resolvers/types/WorkflowListResult.ts
@@ -7,25 +7,31 @@ import { RouteBuilder, eHrefMode } from '../../../../../http/routes/routeBuilder
 
 const WorkflowListResult = {
     UserInitiator: async (parent: Parent): Promise<DBAPI.User | null> => {
-        return await DBAPI.User.fetch(parent.idUserInitiator);
+        return parent.idUserInitiator ? await DBAPI.User.fetch(parent.idUserInitiator) : null;
     },
     Owner: async (parent: Parent): Promise<DBAPI.User | null> => {
-        return await DBAPI.User.fetch(parent.idOwner);
+        return parent.idOwner ? await DBAPI.User.fetch(parent.idOwner) : null;
     },
     Workflow: async (parent: Parent): Promise<DBAPI.Workflow | null> => {
-        return await DBAPI.Workflow.fetch(parent.idWorkflow);
+        return parent.idWorkflow ? await DBAPI.Workflow.fetch(parent.idWorkflow) : null;
+    },
+    WorkflowReport: async (parent: Parent): Promise<DBAPI.WorkflowReport | null> => {
+        return parent.idWorkflowReport ? await DBAPI.WorkflowReport.fetch(parent.idWorkflowReport) : null;
     },
     WorkflowSet: async (parent: Parent): Promise<DBAPI.WorkflowSet | null> => {
-        return await DBAPI.WorkflowSet.fetch(parent.idWorkflowSet);
+        return parent.idWorkflowSet ? await DBAPI.WorkflowSet.fetch(parent.idWorkflowSet) : null;
+    },
+    JobRun: async (parent: Parent): Promise<DBAPI.JobRun | null> => {
+        return parent.idJobRun ? await DBAPI.JobRun.fetch(parent.idJobRun) : null;
     },
     HyperlinkReport: async (parent: Parent): Promise<string | null> => {
-        return RouteBuilder.DownloadWorkflowReport(parent.idWorkflowReport, eHrefMode.ePrependServerURL);
+        return parent.idWorkflowReport ? RouteBuilder.DownloadWorkflowReport(parent.idWorkflowReport, eHrefMode.ePrependServerURL) : null;
     },
     HyperlinkSet: async (parent: Parent): Promise<string | null> => {
-        return RouteBuilder.DownloadWorkflowSet(parent.idWorkflowSet, eHrefMode.ePrependServerURL);
+        return parent.idWorkflowSet ? RouteBuilder.DownloadWorkflowSet(parent.idWorkflowSet, eHrefMode.ePrependServerURL) : null;
     },
     HyperlinkJob: async (parent: Parent): Promise<string | null> => {
-        return RouteBuilder.DownloadJobRun(parent.idJobRun, eHrefMode.ePrependServerURL);
+        return parent.idJobRun ? RouteBuilder.DownloadJobRun(parent.idJobRun, eHrefMode.ePrependServerURL) : null;
     },
 };
 
