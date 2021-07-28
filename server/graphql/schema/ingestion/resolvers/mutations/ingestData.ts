1aa8cc5f6d691fa6fe7c1af37454c49dad481013
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-07-28 15:59:01 -0700

    GraphQL: * Updated ingestion to send post-ingestion workflow events for each ingested object (which may have multiple files, such as a zip for a model or scene)
    Job:
    * Change nature of Cook integration for job creation to handle 500 errors that we're experiencing -- improve logging, increase request count (for limiting retries) upon failures
    * Detect more quickly when file staging completely fails without retrying 100 times...

--- server/graphql/schema/ingestion/resolvers/mutations/ingestData.ts
+++ server/graphql/schema/ingestion/resolvers/mutations/ingestData.ts
@@ -859,7 +859,7 @@ class IngestDataWorker extends ResolverBase {
                         const hrefObject: string = H.Helpers.computeHref(pathObject, assetVersion.FileName);
                         const pathDownload: string = RouteBuilder.DownloadAssetVersion(assetVersion.idAssetVersion, eHrefMode.ePrependServerURL);
                         const hrefDownload: string = H.Helpers.computeHref(pathDownload, 'Download');
-                        await this.appendToWFReport(`Ingested ${hrefObject}: ${hrefDownload}`);
+                        await this.appendToWFReport(`Ingested ${hrefObject}: ${hrefDownload}, size ${assetVersion.StorageSize}, hash ${assetVersion.StorageHash}`);
                     }
                 }
                 if (ISR.assets) {
@@ -889,25 +889,53 @@ class IngestDataWorker extends ResolverBase {
 
         const user: User = this.user!; // eslint-disable-line @typescript-eslint/no-non-null-assertion
 
+        // prepare to wire together ingestion workflow step with output asset versions (in systemObjectSet)
+        const WFC: DBAPI.WorkflowConstellation | null = (this.workflowHelper && this.workflowHelper.workflow)
+            ? await this.workflowHelper.workflow.workflowConstellation() : null;
+        const workflowSteps: DBAPI.WorkflowStep[] | null = WFC ? WFC.workflowStep : null;
+        const workflowStep: DBAPI.WorkflowStep | null = (workflowSteps && workflowSteps.length > 0) ? workflowSteps[workflowSteps.length - 1] : null;
+
         // compute set of unique asset versions ingested:
         let ret: boolean = true;
-        const idSystemObject: number[] = [];
         for (const IAR of ingestResMap.values()) {
-            if (IAR && IAR.assetVersions) {
-                for (const assetVersion of IAR.assetVersions) {
-                    const oID: DBAPI.ObjectIDAndType = { idObject: assetVersion.idAssetVersion, eObjectType: DBAPI.eSystemObjectType.eAssetVersion };
-                    const sysInfo: DBAPI.SystemObjectInfo | undefined = await CACHE.SystemObjectCache.getSystemFromObjectID(oID);
-                    if (!sysInfo) {
-                        LOG.error(`ingestData sendWorkflowIngestionEvent could not find system object for ${JSON.stringify(oID)}`, LOG.LS.eGQL);
-                        ret = false;
-                        continue;
-                    }
-                    idSystemObject.push(sysInfo.idSystemObject);
+            if (!IAR || !IAR.assetVersions)
+                continue;
+
+            const systemObjectSet: Set<number> = new Set<number>();
+            for (const assetVersion of IAR.assetVersions) {
+                const oID: DBAPI.ObjectIDAndType = { idObject: assetVersion.idAssetVersion, eObjectType: DBAPI.eSystemObjectType.eAssetVersion };
+                const sysInfo: DBAPI.SystemObjectInfo | undefined = await CACHE.SystemObjectCache.getSystemFromObjectID(oID);
+                if (!sysInfo) {
+                    LOG.error(`ingestData sendWorkflowIngestionEvent could not find system object for ${JSON.stringify(oID)}`, LOG.LS.eGQL);
+                    ret = false;
+                    continue;
+                }
+                systemObjectSet.add(sysInfo.idSystemObject);
+            }
+
+            const idSystemObject: number[] = [];
+            for (const idSystemObjectDistinct of systemObjectSet.values()) {
+                idSystemObject.push(idSystemObjectDistinct);
+                if (workflowStep) {
+                    const WSSOX: DBAPI.WorkflowStepSystemObjectXref = new DBAPI.WorkflowStepSystemObjectXref({
+                        idWorkflowStep: workflowStep.idWorkflowStep,
+                        idSystemObject: idSystemObjectDistinct,
+                        Input: false,
+                        idWorkflowStepSystemObjectXref: 0
+                    });
+                    if (!await WSSOX.create())
+                        LOG.error(`ingestData sendWorkflowIngestionEvent failed to create WorkflowStepSystemObjectXref ${JSON.stringify(WSSOX, H.Helpers.saferStringify)}`, LOG.LS.eGQL);
                 }
             }
-        }
 
-        if (idSystemObject.length > 0) {
+            let message: string = 'Sending WorkflowEngine IngestObject event';
+            if (IAR.systemObjectVersion?.idSystemObject) {
+                const pathObject: string = RouteBuilder.RepositoryDetails(IAR.systemObjectVersion.idSystemObject, eHrefMode.ePrependClientURL);
+                const hrefObject: string = H.Helpers.computeHref(pathObject, `System Object ${IAR.systemObjectVersion.idSystemObject}`);
+                message += ` for ${hrefObject}`;
+            }
+            await this.appendToWFReport(message);
+
             const workflowParams: WF.WorkflowParameters = {
                 eWorkflowType: null,
                 idSystemObject,
@@ -915,8 +943,6 @@ class IngestDataWorker extends ResolverBase {
                 idUserInitiator: user.idUser,
                 parameters: modelTransformUpdated ? { modelTransformUpdated } : null
             };
-
-            await this.appendToWFReport('Sending WorkflowEngine IngestObject event');
 
             // send workflow engine event, but don't wait for results
             workflowEngine.event(CACHE.eVocabularyID.eWorkflowEventIngestionIngestObject, workflowParams);
