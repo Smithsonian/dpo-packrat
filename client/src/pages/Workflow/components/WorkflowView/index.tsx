3a741bf28806a7c1a49395e8000583d9888a7121
Hsin tung <hsintung@Hsins-MacBook-Pro-2.local>
2021-08-18 10:20:39 -0700

    fixed undesirable row height behavior when displaying workflow error and addressed lint

--- client/src/pages/Workflow/components/WorkflowView/index.tsx
+++ client/src/pages/Workflow/components/WorkflowView/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import WorkflowFilter from './WorkflowFilter';
 import WorkflowList from './WorkflowList';
