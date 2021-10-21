929227c8fb042867ada6b0c3860e31f89d3536a8
Hsin Tung <shintung00@gmail.com>
2021-10-21 12:41:13 -0700

    refactored client to reduce excess styling function calls

--- client/src/components/controls/RepositoryIcon.tsx
+++ client/src/components/controls/RepositoryIcon.tsx
@@ -3,48 +3,26 @@
  *
  * This component renders the icons for the repository tree view item.
  */
-import React from 'react';
-import { Box, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { eSystemObjectType } from '../../types/server';
-import { getTermForSystemObjectType } from '../../utils/repository';
-
-const useStyles = makeStyles(({ typography, breakpoints }) => ({
-    container: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 18,
-        width: 18,
-        borderRadius: 2.5,
-        backgroundColor: ({ backgroundColor }: RepositoryIconProps) => backgroundColor,
-        [breakpoints.down('lg')]: {
-            height: 15,
-            width: 15,
-        },
-    },
-    initial: {
-        fontSize: 10,
-        fontWeight: typography.fontWeightMedium,
-        color: ({ textColor }: RepositoryIconProps) => textColor,
-    }
-}));
-
-export interface RepositoryIconProps {
-    objectType: eSystemObjectType;
-    backgroundColor: string;
-    textColor: string;
-    overrideText?: string | undefined;
-}
-
-export function RepositoryIcon(props: RepositoryIconProps): React.ReactElement {
-    const { objectType, overrideText } = props;
-    const classes = useStyles(props);
-    const initial = !overrideText ? getTermForSystemObjectType(objectType).toUpperCase().slice(0, 1) : overrideText;
-
-    return (
-        <Box className={classes.container}>
-            <Typography className={classes.initial}>{initial}</Typography>
-        </Box>
-    );
-}
+ import React from 'react';
+ import { eSystemObjectType } from '../../types/server';
+ import { getTermForSystemObjectType } from '../../utils/repository';
+ 
+ export interface RepositoryIconProps {
+     objectType: eSystemObjectType;
+     backgroundColor: string;
+     textColor: string;
+     overrideText?: string | undefined;
+     makeStyles?: { [key: string]: string };
+ }
+ 
+ export function RepositoryIcon(props: RepositoryIconProps): React.ReactElement {
+     const { objectType, overrideText, makeStyles } = props;
+     const initial = !overrideText ? getTermForSystemObjectType(objectType).toUpperCase().slice(0, 1) : overrideText;
+ 
+     return (
+         <div className={makeStyles?.container} style={{ backgroundColor: makeStyles?.backgroundColor }}>
+             <p className={makeStyles?.initial} style={{ color: makeStyles?.color }}>{initial}</p>
+         </div>
+     );
+ }
+ 
