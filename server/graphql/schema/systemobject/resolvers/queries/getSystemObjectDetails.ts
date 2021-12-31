7f381181688c3ce92cdc76de300dfca429deb182
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2021-12-30 21:38:32 -0800

    DBAPI: * Relocated logic for computing object ancestors from getSystemObjectDetails to db/api/composite/ObjectAncestors.ts * Changed ancestor computation to combine capture data, models, and scenes into a single level of display * Extract the object of interest and display it at the end of the list * Extract asset owners and display them in a list before the assets * Avoid including an object multiple times (if the item is part of a cycle in the object graph)

--- server/graphql/schema/systemobject/resolvers/queries/getSystemObjectDetails.ts
+++ server/graphql/schema/systemobject/resolvers/queries/getSystemObjectDetails.ts
@@ -11,13 +11,14 @@ import {
 } from '../../../../../types/graphql';
 import { Parent } from '../../../../../types/resolvers';
 import * as LOG from '../../../../../utils/logger';
-import * as H from '../../../../../utils/helpers';
 
 type PublishedStateInfo = {
     publishedState: string;
     publishedEnum: DBAPI.ePublishedState;
     publishable: boolean;
 };
+
+const UNKNOWN_NAME: string = '<UNKNOWN>';
 
 export default async function getSystemObjectDetails(_: Parent, args: QueryGetSystemObjectDetailsArgs): Promise<GetSystemObjectDetailsResult> {
     const { input } = args;
@@ -27,16 +28,19 @@ export default async function getSystemObjectDetails(_: Parent, args: QueryGetSy
     const oID: DBAPI.ObjectIDAndType | undefined = await CACHE.SystemObjectCache.getObjectFromSystem(idSystemObject);
     // LOG.info('getSystemObjectDetails 1', LOG.LS.eGQL);
 
-    const OGD: DBAPI.ObjectGraphDatabase = new DBAPI.ObjectGraphDatabase();
-    const OG: DBAPI.ObjectGraph = new DBAPI.ObjectGraph(idSystemObject, DBAPI.eObjectGraphMode.eAncestors, 32, OGD);
-    const { unit, project, subject, item, objectAncestors } = await getObjectAncestors(OG);
+    const { success, error, unit, project, subject, item, objectAncestors, OGDB } = await getObjectAncestors(idSystemObject);
+    if (!success) {
+        const message: string = `Failed to compute ancestors: ${error}`;
+        LOG.error(message, LOG.LS.eGQL);
+        throw new Error(message);
+    }
     // LOG.info('getSystemObjectDetails 2', LOG.LS.eGQL);
 
     const systemObject: SystemObject | null = await DBAPI.SystemObject.fetch(idSystemObject);
     const sourceObjects: RelatedObject[] = await getRelatedObjects(idSystemObject, RelatedObjectType.Source);
     const derivedObjects: RelatedObject[] = await getRelatedObjects(idSystemObject, RelatedObjectType.Derived);
     const objectVersions: DBAPI.SystemObjectVersion[] | null = await DBAPI.SystemObjectVersion.fetchFromSystemObject(idSystemObject);
-    const LR: DBAPI.LicenseResolver | undefined = await CACHE.LicenseCache.getLicenseResolver(idSystemObject, OGD);
+    const LR: DBAPI.LicenseResolver | undefined = await CACHE.LicenseCache.getLicenseResolver(idSystemObject, OGDB);
     const publishedStateInfo: PublishedStateInfo = await getPublishedState(idSystemObject, oID, LR);
     const identifiers = await getIngestIdentifiers(idSystemObject);
 
@@ -177,149 +181,27 @@ async function getIngestIdentifiers(idSystemObject: number): Promise<IngestIdent
 }
 
 type GetObjectAncestorsResult = {
-    unit: RepositoryPath | null;
-    project: RepositoryPath | null;
-    subject: RepositoryPath | null;
-    item: RepositoryPath | null;
+    success: boolean;
+    error?: string;
+    unit?: RepositoryPath | null;
+    project?: RepositoryPath | null;
+    subject?: RepositoryPath | null;
+    item?: RepositoryPath | null;
     objectAncestors: RepositoryPath[][];
+    OGDB?: DBAPI.ObjectGraphDatabase;
 };
 
-async function getObjectAncestors(OG: DBAPI.ObjectGraph): Promise<GetObjectAncestorsResult> {
-    let unit: RepositoryPath | null = null;
-    let project: RepositoryPath | null = null;
-    let subject: RepositoryPath | null = null;
-    let item: RepositoryPath | null = null;
+async function getObjectAncestors(idSystemObject: number): Promise<GetObjectAncestorsResult> {
+    const OA: DBAPI.ObjectAncestors = new DBAPI.ObjectAncestors(idSystemObject, UNKNOWN_NAME);
+    if (!await OA.compute())
+        return { success: false, objectAncestors: [], error: 'getSystemObjectDetails failed to compute object graph' };
 
-    if (!(await OG.fetch())) {
-        return {
-            unit,
-            project,
-            subject,
-            item,
-            objectAncestors: []
-        };
-    }
-    // LOG.info('getSystemObjectDetails 1a-OG Fetch', LOG.LS.eGQL);
-
-    const objectAncestors: RepositoryPath[][] = [];
-
-    if (OG.unit) {
-        const objectAncestor: RepositoryPath[] = await objectToRepositoryPath(OG.unit, DBAPI.eSystemObjectType.eUnit);
-        unit = objectAncestor[0];
-        objectAncestors.push(objectAncestor);
-    }
-
-    if (OG.project) {
-        const objectAncestor: RepositoryPath[] = await objectToRepositoryPath(OG.project, DBAPI.eSystemObjectType.eProject);
-        project = objectAncestor[0];
-        objectAncestors.push(objectAncestor);
-    }
-
-    if (OG.subject) {
-        const objectAncestor: RepositoryPath[] = await objectToRepositoryPath(OG.subject, DBAPI.eSystemObjectType.eSubject);
-        subject = objectAncestor[0];
-        objectAncestors.push(objectAncestor);
-    }
-
-    if (OG.item) {
-        const objectAncestor: RepositoryPath[] = await objectToRepositoryPath(OG.item, DBAPI.eSystemObjectType.eItem);
-        item = objectAncestor[0];
-        objectAncestors.push(objectAncestor);
-    }
-
-    if (OG.captureData) objectAncestors.push(await objectToRepositoryPath(OG.captureData, DBAPI.eSystemObjectType.eCaptureData));
-    if (OG.model) objectAncestors.push(await objectToRepositoryPath(OG.model, DBAPI.eSystemObjectType.eModel));
-    if (OG.scene) objectAncestors.push(await objectToRepositoryPath(OG.scene, DBAPI.eSystemObjectType.eScene));
-    if (OG.intermediaryFile) objectAncestors.push(await objectToRepositoryPath(OG.intermediaryFile, DBAPI.eSystemObjectType.eIntermediaryFile));
-    if (OG.projectDocumentation) objectAncestors.push(await objectToRepositoryPath(OG.projectDocumentation, DBAPI.eSystemObjectType.eProjectDocumentation));
-    if (OG.asset) objectAncestors.push(await objectToRepositoryPath(OG.asset, DBAPI.eSystemObjectType.eAsset));
-    if (OG.assetVersion) objectAncestors.push(await objectToRepositoryPath(OG.assetVersion, DBAPI.eSystemObjectType.eAssetVersion));
-    if (OG.actor) objectAncestors.push(await objectToRepositoryPath(OG.actor, DBAPI.eSystemObjectType.eActor));
-    if (OG.stakeholder) objectAncestors.push(await objectToRepositoryPath(OG.stakeholder, DBAPI.eSystemObjectType.eStakeholder));
-
-    // LOG.info('getSystemObjectDetails 1b', LOG.LS.eGQL);
-    return {
-        unit,
-        project,
-        subject,
-        item,
-        objectAncestors
-    };
-}
-
-const unknownName: string = '<UNKNOWN>';
-
-type Objects =
-    | DBAPI.Unit[]
-    | DBAPI.Project[]
-    | DBAPI.Subject[]
-    | DBAPI.Item[]
-    | DBAPI.CaptureData[]
-    | DBAPI.Model[]
-    | DBAPI.Scene[]
-    | DBAPI.IntermediaryFile[]
-    | DBAPI.ProjectDocumentation[]
-    | DBAPI.Asset[]
-    | DBAPI.AssetVersion[]
-    | DBAPI.Actor[]
-    | DBAPI.Stakeholder[];
-
-async function objectToRepositoryPath(objects: Objects, objectType: DBAPI.eSystemObjectType): Promise<RepositoryPath[]> {
-    const paths: RepositoryPath[] = [];
-    for (const object of objects) {
-        let idObject: number | null = null;
-
-        if (object instanceof DBAPI.Unit)
-            idObject = object.idUnit;
-        else if (object instanceof DBAPI.Project)
-            idObject = object.idProject;
-        else if (object instanceof DBAPI.Subject)
-            idObject = object.idSubject;
-        else if (object instanceof DBAPI.Item)
-            idObject = object.idItem;
-        else if (object instanceof DBAPI.CaptureData)
-            idObject = object.idCaptureData;
-        else if (object instanceof DBAPI.Model)
-            idObject = object.idModel;
-        else if (object instanceof DBAPI.Scene)
-            idObject = object.idScene;
-        else if (object instanceof DBAPI.IntermediaryFile)
-            idObject = object.idIntermediaryFile;
-        else if (object instanceof DBAPI.ProjectDocumentation)
-            idObject = object.idProjectDocumentation;
-        else if (object instanceof DBAPI.Asset)
-            idObject = object.idAsset;
-        else if (object instanceof DBAPI.AssetVersion)
-            idObject = object.idAssetVersion;
-        else if (object instanceof DBAPI.Actor)
-            idObject = object.idActor;
-        else if (object instanceof DBAPI.Stakeholder)
-            idObject = object.idStakeholder;
-        else {
-            LOG.error(`getSystemObjectDetails unable to determine type and id from ${JSON.stringify(object, H.Helpers.saferStringify)}`, LOG.LS.eGQL);
-            continue;
-        }
-
-        const oID: DBAPI.ObjectIDAndType | undefined = { idObject, eObjectType: objectType };
-        const SOI: DBAPI.SystemObjectInfo | undefined = await CACHE.SystemObjectCache.getSystemFromObjectID(oID);
-        if (SOI) {
-            const path: RepositoryPath = {
-                idSystemObject: SOI.idSystemObject,
-                name: await resolveNameForObject(SOI.idSystemObject),
-                objectType
-            };
-            paths.push(path);
-        } else
-            LOG.error(`getSystemObjectDetails could not compute system object info from ${JSON.stringify(oID)}`, LOG.LS.eGQL);
-    }
-
-    // LOG.info(`getSystemObjectDetails 1b-${DBAPI.eSystemObjectType[objectType]} ${objects.length}`, LOG.LS.eGQL);
-    return paths;
+    return { success: true, unit: OA.unit, project: OA.project, subject: OA.subject, item: OA.item, objectAncestors: OA.ancestorStack, OGDB: OA.OGDB };
 }
 
 async function resolveNameForObject(idSystemObject: number): Promise<string> {
     const name: string | undefined = await CACHE.SystemObjectCache.getObjectNameByID(idSystemObject);
-    return name ?? unknownName;
+    return name ?? UNKNOWN_NAME;
 }
 
 async function computeAssetAndOwner(oID: DBAPI.ObjectIDAndType): Promise<{ owner: RepositoryPath | undefined, asset: RepositoryPath | undefined }> {
