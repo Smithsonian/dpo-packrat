81af37c48d850aa4d7afee1f16bd3745c034ef7b
Hsin Tung <shintung00@gmail.com>
2022-02-24 20:05:15 -0800

    WIP performance of resizing for larger lists

--- client/src/pages/Repository/components/RepositoryTreeView/RepositoryTreeHeader.tsx
+++ client/src/pages/Repository/components/RepositoryTreeView/RepositoryTreeHeader.tsx
@@ -5,16 +5,17 @@
  */
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { eMetadata } from '@dpo-packrat/common';
 import { getTreeViewColumns } from '../../../../utils/repository';
 import MetadataView from './MetadataView';
+import ResizeObserver from 'resize-observer-polyfill';
+import { useTreeColumnsStore } from '../../../../store';
 
 const useStyles = makeStyles(({ palette, typography, breakpoints }) => ({
     container: {
         display: 'flex',
         alignItems: 'center',
-        minHeight: 50,
         height: 'fit-content',
         backgroundColor: palette.primary.light,
         borderRadius: 5,
@@ -58,9 +59,9 @@ const useStyles = makeStyles(({ palette, typography, breakpoints }) => ({
         fontSize: typography.pxToRem(14),
         color: palette.primary.dark,
         fontWeight: typography.fontWeightRegular,
-        overflow: 'hidden',
-        textOverflow: 'ellipsis'
-
+        overflow: 'auto',
+        textOverflow: 'ellipsis',
+        resize: 'horizontal'
     },
     text: {
         fontSize: '0.9em',
@@ -75,8 +76,28 @@ interface RepositoryTreeHeaderProps {
 function RepositoryTreeHeader(props: RepositoryTreeHeaderProps): React.ReactElement {
     const { metadataColumns } = props;
     const classes = useStyles();
+    const [updateWidth] = useTreeColumnsStore(state => [state.updateWidth]);
 
     const treeColumns = getTreeViewColumns(metadataColumns, true);
+
+    // this useEffect block observes changes made to header column widths
+    // and cleans it up when we unmount the component
+    useEffect(() => {
+        const columnSet = new Set<ResizeObserver>();
+
+        treeColumns.forEach((col) => {
+            const target = document.getElementById(`header-${col.label}`);
+            if (target) {
+                const columnObersver = new ResizeObserver((e) => updateWidth(col.metadataColumn as number, String(e[0].contentRect.width)));
+                columnObersver.observe(target);
+                columnSet.add(columnObersver);
+            }
+        });
+
+        return () => {
+            columnSet.forEach((col) => col.unobserve);
+        }
+    }, []);
 
     return (
         <Box className={classes.container}>
