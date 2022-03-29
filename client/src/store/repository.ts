59700b46fb88b1975e15642a99389e46f57ea010
Hsin Tung <shintung00@gmail.com>
2022-03-29 11:51:09 -0700

    remove outdated comment

--- client/src/store/repository.ts
+++ client/src/store/repository.ts
@@ -424,7 +424,6 @@ export const useRepositoryStore = create<RepositoryStore>((set: SetState<Reposit
     },
     setDefaultIngestionFilters: async (systemObjectType: eSystemObjectType, idRoot: number | undefined): Promise<void> => {
         const { resetKeywordSearch, resetRepositoryFilter, getChildrenForIngestion } = get();
-        // allows for scene ingestion to search without being rooted
         if (idRoot !== undefined && systemObjectType) {
             const { data: { getSystemObjectDetails: { name, objectType } } } = await apolloClient.query({
                 query: GetSystemObjectDetailsDocument,
