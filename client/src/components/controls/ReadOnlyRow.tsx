c4be94fee958cc494d86ca56129cef093856ac10
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2022-04-11 10:39:09 -0700

    Client: * Added valueProps to ReadOnlyRowProps * Added columnGap to ObjectMeshTable * Added unique keys to ObjectMeshTable's mapped UI elements * Adeed bold fontweight to ObjectMeshTable's material names

--- client/src/components/controls/ReadOnlyRow.tsx
+++ client/src/components/controls/ReadOnlyRow.tsx
@@ -17,6 +17,7 @@ interface ReadOnlyRowProps extends ViewableProps {
     gridTemplate?: string;
     width?: string;
     labelProps?: any;
+    valueProps?: any;
     valueLeftAligned?: boolean;
     gridValue?: number;
     gridLabel?: number;
@@ -26,7 +27,7 @@ interface ReadOnlyRowProps extends ViewableProps {
 }
 
 function ReadOnlyRow(props: ReadOnlyRowProps): React.ReactElement {
-    const { label, value, padding, gridTemplate, width, labelProps, valueLeftAligned, gridValue, gridLabel, paddingString, containerStyle, required = false } = props;
+    const { label, value, padding, gridTemplate, width, labelProps, valueProps, valueLeftAligned, gridValue, gridLabel, paddingString, containerStyle, required = false } = props;
 
     const rowFieldProps = { alignItems: 'baseline', justifyContent: 'space-between', style: { borderRadius: 0, ...containerStyle } };
     if (width) {
@@ -40,7 +41,7 @@ function ReadOnlyRow(props: ReadOnlyRowProps): React.ReactElement {
     return (
         <FieldType label={label} direction='row' containerProps={rowFieldProps} labelProps={{ style: { wordBreak: 'keep-all' }, ...labelProps  }} valueLeftAligned={valueLeftAligned} gridValue={gridValue} gridLabel={gridLabel} padding={paddingString} required={required}>
             <Box width='fit-content' textAlign='right' display='flex'>
-                <Typography variant='caption' style={{ fontFamily: 'Roboto, Helvetical, Arial, sans-serif', overflowWrap: 'break-word', wordBreak: 'break-all', padding }}>
+                <Typography variant='caption' style={{ fontFamily: 'Roboto, Helvetical, Arial, sans-serif', overflowWrap: 'break-word', wordBreak: 'break-all', padding }} {...valueProps}>
                     {value}
                 </Typography>
             </Box>
