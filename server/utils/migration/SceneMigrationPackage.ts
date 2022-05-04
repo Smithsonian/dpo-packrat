f8b42ba40aa2dd49762314b25f8c3f362b390bf8
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2022-05-03 23:17:08 -0700

    Migration: * Name updates

--- server/utils/migration/SceneMigrationPackage.ts
+++ server/utils/migration/SceneMigrationPackage.ts
@@ -1,7 +1,7 @@
-export class SceneMigrationEntry {
+export class SceneMigrationPackage {
     EdanUUID: string;                           // EDAN 3d_package UUID
     PackageName?: string;                       // Name of zip containing scene; undefined means fetch from EDAN
-    PackagePath?: string;                       // Path to zip containing scene; undefined means fetch from EDAN
+    PackagePath?: string;                       // Path to zip containing scene; undefined defaults to ../../tests/mock/scenes relative to this folder (or fetch from EDAN, if PackageName is undefined)
     PosedAndQCd?: boolean;
     ApprovedForPublication?: boolean;
     idSystemObjectItem?: number;                // idSystemObject of item that owns this scene.
