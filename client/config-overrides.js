cf76a482553882bf69e871d56192fd267baf448a
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2022-09-21 11:29:00 -0700

    Build: * Ensure client is able to load source from common

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -8,21 +8,6 @@ const path = require('path');
 const { override, addExternalBabelPlugin, babelInclude } = require('customize-cra');
 const { addReactRefresh } = require('customize-cra-react-refresh');
 
-/*
-function overrideIncludes(config) {
-    // See https://stackoverflow.com/questions/65893787/create-react-app-with-typescript-and-npm-link-enums-causing-module-parse-failed.
-    config.module.rules[1].oneOf[2].include = [
-        path.join(__dirname, './src'),
-        // path.join(__dirname, '../common/src')
-        path.join(__dirname, '../common')
-    ];
-    return config;
-}
-*/
-
-// module.exports = override(overrideIncludes(config), addExternalBabelPlugin('react-activation/babel'), addReactRefresh(), disableEsLint());
-// module.exports = override(overrideIncludes, addExternalBabelPlugin('react-activation/babel'), addReactRefresh());
-// module.exports = override(addExternalBabelPlugin('react-activation/babel'), addReactRefresh(), addExternalBabelPlugin(path.join(__dirname, '../common')));
 module.exports = override(addExternalBabelPlugin('react-activation/babel'), addReactRefresh(), babelInclude([
     path.resolve('src'), // make sure you link your own source
     path.resolve('../common'),
