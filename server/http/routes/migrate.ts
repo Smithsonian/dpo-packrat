4c585deec328be0024e1d9df6e0e13846c3b85e0
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2022-11-09 17:52:14 -0800

    Migration: * Wire item to model when migrating models in "extract" mode

--- server/http/routes/migrate.ts
+++ server/http/routes/migrate.ts
@@ -235,7 +235,7 @@ class Migrator {
                 MMR = await MM.migrateModel(modelFile.uniqueID, user.idUser, modelFileSet, true);
             } else {
                 const filePath: string = ModelMigrationFile.computeFilePath(modelFile);
-                const MDE: ModelDataExtraction = new ModelDataExtraction(modelFile.uniqueID, path.basename(filePath), filePath);
+                const MDE: ModelDataExtraction = new ModelDataExtraction(modelFile.uniqueID, modelFile.idSystemObjectItem, path.basename(filePath), filePath);
                 MMR = await MDE.fetchAndExtractInfo();
             }
             if (!MMR.success)
