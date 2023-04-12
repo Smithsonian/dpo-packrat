830da6c6965a8893c3af5086958f53b627fe10b8
Jon Tyson <6943745+jahjedtieson@users.noreply.github.com>
2023-04-11 22:17:14 -0700

    Test: * For now, disable jest globalTeardown's call to prisma.$disconnect().

--- server/tests/teardown.ts
+++ server/tests/teardown.ts
@@ -1,9 +1,9 @@
 import * as LOG from '../utils/logger';
 import * as H from '../utils/helpers';
-import * as DBC from '../db/connection';
+// import * as DBC from '../db/connection';
 
 export async function teardown(): Promise<void> {
-    await DBC.DBConnection.disconnect();
+    // await DBC.DBConnection.disconnect();
     LOG.end();
     await H.Helpers.sleep(1000);
 }
