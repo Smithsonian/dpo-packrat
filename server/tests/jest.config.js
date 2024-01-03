0df44d86384015fd4fda252948bb88f52bb91c10
Eric Maslowski <emaslowski@quotient-inc.com>
2024-01-03 15:06:46 -0500

    (fix) restored default testing configuration

--- server/tests/jest.config.js
+++ server/tests/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // collectCoverage: true,
     testMatch: [
         // The complete test suite, on one line, to aid in quick commenting out
-        // '**/tests/auth/**', '**/tests/cache/cache.test.ts', '**/tests/collections/*.test.ts', '**/tests/db/**/*.test.ts', '**/tests/graphql/graphql.test.ts', '**/tests/metadata/*.test.ts', '**/tests/job/**/*.test.ts', '**/tests/navigation/**/*.test.ts', '**/tests/storage/**/*.test.ts', '**/tests/utils/**/*.test.ts',
+        '**/tests/auth/**', '**/tests/cache/cache.test.ts', '**/tests/collections/*.test.ts', '**/tests/db/**/*.test.ts', '**/tests/graphql/graphql.test.ts', '**/tests/metadata/*.test.ts', '**/tests/job/**/*.test.ts', '**/tests/navigation/**/*.test.ts', '**/tests/storage/**/*.test.ts', '**/tests/utils/**/*.test.ts',
         // '**/tests/db/dbcreation.test.ts',
 
         // Larger test collections, left here to aid in quick, focused testing; these are the elements on the line above:
@@ -42,10 +42,10 @@ module.exports = {
         // '**/tests/utils/helpers.test.ts',
         // '**/tests/utils/parser/bagitReader.test.ts',
         // '**/tests/utils/parser/bulkIngestReader.test.ts',
-        '**/tests/utils/parser/csvParser.test.ts',
+        // '**/tests/utils/parser/csvParser.test.ts',
         // '**/tests/utils/parser/svxReader.test.ts',
-        '**/tests/utils/zipFile.test.ts',
-        '**/tests/utils/zipStream.test.ts',
+        // '**/tests/utils/zipFile.test.ts',
+        // '**/tests/utils/zipStream.test.ts',
     ],
     testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
     setupFiles: ['<rootDir>/tests/setEnvVars.ts'],
