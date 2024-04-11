59f14e892485c236c339d9541c25e25fe6a3624a
EMaslowskiQ <118929649+EMaslowskiQ@users.noreply.github.com>
2024-04-11 12:08:46 -0400

    Update to Voyager Schema for v0.39.0 (#581)
    (new) support for splash screens and auto near/far plane calculations

--- server/types/voyager/meta.ts
+++ server/types/voyager/meta.ts
@@ -70,6 +70,7 @@ export interface IArticle
     leads?: Dictionary<string>;
     tags?: string[];
     taglist?: Dictionary<string[]>;
+    intros?: Dictionary<string[]>;
 
     mimeType?: string;
     thumbnailUri?: string;
