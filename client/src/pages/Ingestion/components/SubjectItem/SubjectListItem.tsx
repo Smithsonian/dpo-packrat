dc8ba33b8d61f0720a24865f1fdfd6f8f95aa731
Eric Maslowski <emaslowski@quotient-inc.com>
2024-05-17 16:46:10 -0400

    (fix) cleanout legacy files from merge (fix) move '+' button to left side for Subject search list

--- client/src/pages/Ingestion/components/SubjectItem/SubjectListItem.tsx
+++ client/src/pages/Ingestion/components/SubjectItem/SubjectListItem.tsx
@@ -57,6 +57,11 @@ function SubjectListItem(props: SubjectListItemProps): React.ReactElement {
 
     return (
         <TableRow>
+            <TableCell style={cellStyle} align='left'>
+                <Box className={classes.options}>
+                    {selected ? <MdRemoveCircleOutline className={classes.option} onClick={remove} size={20} /> : <MdAddCircleOutline className={classes.option} onClick={add} size={20} />}
+                </Box>
+            </TableCell>
             <TableCell style={cellStyle}>
                 <Typography className={classes.label} variant='caption'>{arkId}</Typography>
             </TableCell>
@@ -71,11 +76,6 @@ function SubjectListItem(props: SubjectListItemProps): React.ReactElement {
             </TableCell>
             <TableCell style={cellStyle} align='left'>
                 <Typography className={classes.label} variant='caption'>{collectionId}</Typography>
-            </TableCell>
-            <TableCell style={cellStyle} align='left'>
-                <Box className={classes.options}>
-                    {selected ? <MdRemoveCircleOutline className={classes.option} onClick={remove} size={20} /> : <MdAddCircleOutline className={classes.option} onClick={add} size={20} />}
-                </Box>
             </TableCell>
         </TableRow>
     );
