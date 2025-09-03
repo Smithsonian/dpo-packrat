89d35fd84b0e078c3a597e7e02f37f0e2f753f2d
EMaslowskiQ <emaslowski@quotient-inc.com>
2025-09-03 10:33:37 -0400

    ObjectProperty Type SQL Fix

--- server/db/sql/scripts/Packrat.SCHEMA.sql
+++ server/db/sql/scripts/Packrat.SCHEMA.sql
@@ -455,7 +455,7 @@ CREATE TABLE IF NOT EXISTS `ObjectProperty` (
   `Rationale` text DEFAULT NULL,
   `idContact` int(11) DEFAULT NULL,
   PRIMARY KEY (`idObjectProperty`),
-  UNIQUE KEY `ObjectProperty_idSystemObject_Type` (`idSystemObject`,`Type`),
+  UNIQUE KEY `ObjectProperty_idSystemObject_PropertyType` (`idSystemObject`,`PropertyType`),
   KEY `ObjectProperty_idSystemObject` (`idSystemObject`),
   KEY `ObjectProperty_idContact` (`idContact`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
